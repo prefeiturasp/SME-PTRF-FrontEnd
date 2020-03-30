52a703ad6c3dc7ea79372b730a4e0728d32033d2
Alessandro Fernandes <alcfernandes@yahoo.com>
2020-03-30 17:40:28 -0300

    Cria lista despesas da escola

--- src/componentes/Despesas/ListaDeDespesas/index.jsx
+++ src/componentes/Despesas/ListaDeDespesas/index.jsx
@@ -3,7 +3,7 @@ import { DataTable } from 'primereact/datatable'
 import { Column } from 'primereact/column'
 import { Button } from 'primereact/button'
 import { Row, Col } from 'reactstrap'
-//import { getModeloAtesteLookup } from "../../../service/ModeloAteste.service";
+import { getListaRateiosDespesas } from '../../../services/RateiosDespesas.service'
 import { redirect } from '../../../utils/redirect.js'
 
 export class ListaDeDespesas extends Component {
@@ -14,42 +14,49 @@ export class ListaDeDespesas extends Component {
     }
   }
 
-  getListaRateiosDespesas() {
-    const despesas = {
-      count: 1,
-      next: null,
-      previous: null,
-      results: [
-        {
-          uuid: 'ed678d57-0fb1-40bc-b854-812ee929f94f',
-          despesa: 'cc204c00-c9dd-4f22-8ce6-f7377d289c38',
-          numero_documento: '634767',
-          status_despesa: 'COMPLETO',
-          especificacao_material_servico: {
-            id: 1,
-            descricao: 'Material Elétrico',
-            aplicacao_recurso: 'CUSTEIO',
-            tipo_custeio: 1
-          },
-          data_documento: '2020-03-10',
-          aplicacao_recurso: 'CUSTEIO',
-          acao_associacao: {
-            uuid: '721dcff8-293e-4d7d-bb44-6203a62080de',
-            nome: 'PTRF'
-          },
-          valor_total: 11000.5
-        }
-      ]
-    }
-    return despesas
-  }
   buscaRateiosDespesas = async () => {
-    const rateiosDespesas = await this.getListaRateiosDespesas()
+    const rateiosDespesas = await getListaRateiosDespesas()
     this.setState({ rateiosDespesas })
   }
 
   componentDidMount() {
     this.buscaRateiosDespesas()
+  }
+
+  numeroDocumentoStatusTemplate(rowData, column) {
+    const statusColor =
+      rowData['status_despesa'] === 'COMPLETO'
+        ? 'ptrf-despesa-status-ativo'
+        : 'ptrf-despesa-status-inativo'
+    const statusText =
+      rowData['status_despesa'] === 'COMPLETO'
+        ? 'Status: Completo'
+        : 'Status: Incompleto'
+    return (
+      <div>
+        <span>{rowData['numero_documento']}</span>
+        <br></br>
+        <span className={statusColor}>{statusText}</span>
+      </div>
+    )
+  }
+
+  especificacaoDataTemplate(rowData, column) {
+    return (
+      <div>
+        <span>{rowData['especificacao_material_servico'].descricao}</span>
+        <br></br>
+        <span>Data: {rowData['data_documento']}</span>
+      </div>
+    )
+  }
+
+  valorTotalTemplate(rowData, column) {
+    const valorFormatado = rowData['valor_total'].toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    })
+    return <span>{valorFormatado}</span>
   }
 
   render() {
@@ -81,16 +88,30 @@ export class ListaDeDespesas extends Component {
         </Row>
         <DataTable
           value={rateiosDespesas}
-          className="datatable-strapd-coad"
+          className="mt-3 datatable-footer-coad"
           paginator={rateiosDespesas.length > rowsPerPage}
           rows={rowsPerPage}
           paginatorTemplate="PrevPageLink PageLinks NextPageLink"
+          autoLayout={true}
+          selectionMode="single"
         >
-          <Column field="numero_documento" header="Número do documento" />
           <Column
-            field="especificacao_material_servico"
+            field="numero_documento"
+            header="Número do documento"
+            body={this.numeroDocumentoStatusTemplate}
+          />
+          <Column
+            field="especificacao_material_servico.descricao"
             header="Especificação do material ou serviço"
-            style={{ width: '10em' }}
+            body={this.especificacaoDataTemplate}
+          />
+          <Column field="aplicacao_recurso" header="Aplicação" />
+          <Column field="acao_associacao.nome" header="Tipo de ação" />
+          <Column
+            field="valor_total"
+            header="Valor"
+            body={this.valorTotalTemplate}
+            style={{ textAlign: 'right' }}
           />
         </DataTable>
       </div>
