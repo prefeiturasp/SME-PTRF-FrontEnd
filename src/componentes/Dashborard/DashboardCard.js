b1f7783a2156f6b3991e36bea1363bc00b90155d
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-04-20 14:36:55 -0300

    Diagramando o painel de acordo com o protótipo - 02

--- src/componentes/Dashborard/DashboardCard.js
+++ src/componentes/Dashborard/DashboardCard.js
@@ -13,19 +13,25 @@ export const DashboardCard = ({acoesAssociacao}) => {
                             <span><strong>{acao.acao_associacao_nome}</strong> </span>
                         </div>
                         <div className="card-body">
-                            {/*<h5 className="card-title">Card title</h5>*/}
                             <div className='row'>
-                                <div className="col-12 col-md-5">
-                                    <div className="col-12 align-self-center container-lado-esquerdo">
+                                <div className="col-12 col-md-5 align-self-center">
+                                    <div className="col-12 container-lado-esquerdo pt-1 pb-1">
+
                                         <p className="pt-1 mb-1" >Custeio: <strong>{acao.saldo_atual_custeio}</strong></p>
                                         <p className="pt-1 mb-1">Capital: <strong>{acao.saldo_atual_capital}</strong></p>
-                                        <p className="pt-1 pb-1 mb-0">Capital: <strong>{acao.saldo_atual_capital}</strong></p>
+                                        <p className="pt-1 pb-1 mb-0">Total: <strong>{acao.saldo_atual_total}</strong></p>
                                     </div>
 
                                 </div>
 
-                                <div className="col-12 col-md-7 align-self-center ">
-                                    <p className="card-text">This is a longer card with supporting.</p>
+                                <div className="col-12 col-md-7 container-lado-direito align-self-center ">
+                                    <p className="pt-1 mb-1" >Saldo reprogramado: <strong>{acao.saldo_reprogramado}</strong></p>
+                                    <p className="pt-1 mb-1">Repasses no período: <strong>{acao.repasses_no_periodo}</strong></p>
+                                    <p className="pt-1 pb-1 mb-0">Despesa declarada: <strong>{acao.despesas_no_periodo}</strong></p>
+                                    {acao.acao_associacao_nome === "PTRF" ? (
+                                        <p className="pt-1 pb-1 mb-0">Próx. repasse a partir de: <strong>{acoesAssociacao.data_prevista_repasse}</strong></p>
+                                    ) : null}
+
                                 </div>
                             </div>
 
