176b605113ebfa95e2bef03b3c7cbbb05ab536d9
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-04-20 14:01:55 -0300

    Diagramando o painel de acordo com o protótipo

--- src/componentes/Dashborard/DashboardCard.js
+++ src/componentes/Dashborard/DashboardCard.js
@@ -10,17 +10,25 @@ export const DashboardCard = ({acoesAssociacao}) => {
                 <div key={index} className="col mb-4 container-dashboard-card">
                     <div className="card h-100">
                         <div className="card-header bg-white">
-                            <span><strong>{acao.acao_associacao_nome}</strong></span>
+                            <span><strong>{acao.acao_associacao_nome}</strong> </span>
                         </div>
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a longer card with supporting text below as a natural
-                                lead-in
-                                to additional content. This content is a little bit longer.
-                                This is a longer card with supporting text below as a natural lead-in to additional
-                                content.
-                                This content is a little bit longer.
-                            </p>
+                            {/*<h5 className="card-title">Card title</h5>*/}
+                            <div className='row'>
+                                <div className="col-12 col-md-5">
+                                    <div className="col-12 align-self-center container-lado-esquerdo">
+                                        <p className="pt-1 mb-1" >Custeio: <strong>{acao.saldo_atual_custeio}</strong></p>
+                                        <p className="pt-1 mb-1">Capital: <strong>{acao.saldo_atual_capital}</strong></p>
+                                        <p className="pt-1 pb-1 mb-0">Capital: <strong>{acao.saldo_atual_capital}</strong></p>
+                                    </div>
+
+                                </div>
+
+                                <div className="col-12 col-md-7 align-self-center ">
+                                    <p className="card-text">This is a longer card with supporting.</p>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
