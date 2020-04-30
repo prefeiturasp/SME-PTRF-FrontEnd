757e3ecd4b5edc140d3fcf456fd783e458e11651
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-04-30 15:59:11 -0300

    Diagramando Selects superior - 02

--- src/componentes/PrestacaoDeContas/index.js
+++ src/componentes/PrestacaoDeContas/index.js
@@ -8,25 +8,34 @@ export const PrestacaoDeContas = () => {
 
 
             <div className="row">
-                <div className="col-sm-12 col-md-6 col-lg-4">
+                <div className="col-md-12 col-lg-7 col-xl-5 mb-md-2">
                     <div className="row">
-                    <div className="col-12 col-md-3 mt-2 pr-0">
-                        <label htmlFor="inputPassword" className="">Período:</label>
-                    </div>
-                    <div className="col-12 col-md-9 pl-0">
-                        <select id="inputState" className="form-control">
-                        <option>Choose...</option>
-                        <option>...</option>
-                        </select>
-                    </div>
+                        <div className="col-12 col-sm-5 col-md-3 mt-2 pr-0">
+                            <label htmlFor="inputPassword" className="">Período:</label>
+                        </div>
+                        <div className="col-12 col-sm-7 col-md-9 pl-0">
+                            <select id="inputState" className="form-control">
+                            <option>Choose...</option>
+                            <option>...</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
-                <div className="col-12 col-md-5 col-lg-4 offset-md-2 offset-md-4">
-                    <label htmlFor="inputPassword" className="col-12 col-md-3 col-form-label pr-0 label-select-superior">Período:</label>
-                    <select id="inputState" className="form-control w-75 float-md-right">
-                    <option>Choose...</option>
-                    <option>...</option>
-                </select>
+
+                <div className="col-md-12 col-lg-5 col-xl-4 offset-xl-3">
+
+                    <div className="row">
+                        <div className="col-12 col-sm-5 col-md-3 mt-2 pr-0">
+                            <label htmlFor="inputPassword" className="">Período:</label>
+                        </div>
+                        <div className="col-12 col-sm-7 col-md-9 pl-0">
+                            <select id="inputState" className="form-control">
+                                <option>Choose...</option>
+                                <option>...</option>
+                            </select>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </form>
