92f8200cdfac26e6d54076ab0c82a8bb79a2fe90
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-05-04 17:46:01 -0300

    Estilizando componenteTabelaValoresPendentesPorAcao.js

--- src/componentes/PrestacaoDeContas/DetalheDasPrestacoes/TabelaValoresPendentesPorAcao/index.js
+++ src/componentes/PrestacaoDeContas/DetalheDasPrestacoes/TabelaValoresPendentesPorAcao/index.js
@@ -17,24 +17,35 @@ export const TabelaValoresPendentesPorAcao = () => {
         {acao: 'Educom - Imprensa Jovem', totalReceitas: 'R$ 765,43', conciliadoReceitas: 'R$ 765,43', aconciliarReceitas: 'R$ 421,13', totalDespesas: 'R$ 150,05', conciliadoDespesas: '$43,342', aconciliarDespesas: 'R$ 54,406.00'},
         {acao: 'Mais Escola', totalReceitas: 'R$ 10,54', conciliadoReceitas: 'R$ 10,54', aconciliarReceitas: 'R$ 10,54', totalDespesas: 'R$ 100,21', conciliadoDespesas: 'R$ 43,34', aconciliarDespesas: 'R$ 54,406.00'},
         {acao: 'Formação', totalReceitas: 'R$ 4,54', conciliadoReceitas: 'R$ 0,00', aconciliarReceitas: 'R$ 4,54', totalDespesas: 'R$ 296,23', conciliadoDespesas: 'R$ 43,32', aconciliarDespesas: 'R$ 54,406.00'},
-        {acao: 'Total', totalReceitas: 'R$ 3.651,74', conciliadoReceitas: 'R$ 2.718,51', aconciliarReceitas: 'R$ 810,10', totalDespesas: 'R$ 70.454,86', conciliadoDespesas: 'R$ 61.165,93', aconciliarDespesas: 'R$ 9.388,93'},
     ];
 
     let headerGroup = <ColumnGroup>
         <Row className="detalhe-das-prestacoes-tabela-fundo-azul-claro">
-            <Column className="detalhe-das-prestacoes-tabela-fundo-azul-claro" header="Ação" rowSpan={3} />
+            <Column style={{background: 'red'}} className="detalhe-das-prestacoes-tabela-fundo-azul-claro" header="Ação" rowSpan={3} />
         </Row>
         <Row>
-            <Column className="detalhe-das-prestacoes-align-center" header="Receitas" colSpan={3} />
-            <Column className="detalhe-das-prestacoes-align-center" header="Despesas" colSpan={3} />
+            <Column className="detalhe-das-prestacoes-align-center detalhe-das-prestacoes-tabela-th-fundo-branco" header="Receitas" colSpan={3} />
+            <Column className="detalhe-das-prestacoes-align-center detalhe-das-prestacoes-tabela-th-fundo-branco" header="Despesas" colSpan={3} />
         </Row>
         <Row>
-            <Column header="Total" />
-            <Column header="Conciliado" />
-            <Column header="À conciliar" />
-            <Column header="Total" />
-            <Column header="Conciliado" />
-            <Column header="À conciliar" />
+            <Column className="detalhe-das-prestacoes-tabela-th-fundo-branco" header="Total" />
+            <Column className="detalhe-das-prestacoes-tabela-th-fundo-branco" header="Conciliado" />
+            <Column className="detalhe-das-prestacoes-tabela-th-fundo-branco" header="À conciliar" />
+            <Column className="detalhe-das-prestacoes-tabela-th-fundo-branco" header="Total" />
+            <Column className="detalhe-das-prestacoes-tabela-th-fundo-branco" header="Conciliado" />
+            <Column className="detalhe-das-prestacoes-tabela-th-fundo-branco" header="À conciliar" />
+        </Row>
+    </ColumnGroup>;
+
+    let footerGroup = <ColumnGroup>
+        <Row>
+            <Column className="detalhe-das-prestacoes-tabela-fundo-azul-claro" footer="Total"/>
+            <Column className='detalhe-das-prestacoes-tabela-th-fundo-branco fonte-normal' footer="R$ 3.651,74" />
+            <Column className='detalhe-das-prestacoes-tabela-th-fundo-branco fonte-normal' footer="R$ 2.718,51" />
+            <Column className='detalhe-das-prestacoes-tabela-th-fundo-branco fonte-normal' footer="R$ 810,10" />
+            <Column className='detalhe-das-prestacoes-tabela-th-fundo-branco fonte-normal' footer="R$ 70.454,86" />
+            <Column className='detalhe-das-prestacoes-tabela-th-fundo-branco fonte-normal' footer="R$ 61.165,93" />
+            <Column className='detalhe-das-prestacoes-tabela-th-fundo-branco fonte-normal' footer="R$ 9.388,93" />
         </Row>
     </ColumnGroup>;
 
@@ -45,8 +56,13 @@ export const TabelaValoresPendentesPorAcao = () => {
                 <p className="detalhe-das-prestacoes-titulo-lancamentos">Valores pendentes de conciliação por ação</p>
 
                 <div className="content-section implementation">
-                    <DataTable value={sales} headerColumnGroup={headerGroup}>
-                        <Column field="acao" />
+                    <DataTable
+                        value={sales}
+                        headerColumnGroup={headerGroup}
+                        footerColumnGroup={footerGroup}
+                        className="detalhe-das-prestacoes-tabela"
+                    >
+                        <Column className="detalhe-das-prestacoes-tabela-fundo-azul-claro" field="acao" />
                         <Column field="totalReceitas" />
                         <Column field="conciliadoReceitas" />
                         <Column field="aconciliarReceitas" />
