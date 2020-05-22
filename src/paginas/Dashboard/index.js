0c5052c7876315cda735459afda85f265165cbae
Alessandro Fernandes <alcfernandes@yahoo.com>
2020-05-22 11:26:27 -0300

    Add referencia e status período no painel de ações

--- src/paginas/Dashboard/index.js
+++ src/paginas/Dashboard/index.js
@@ -1,31 +1,49 @@
-import React, {useEffect, useState} from "react";
-import {PaginasContainer} from "../PaginasContainer";
-import {Dashboard} from "../../componentes/Dashborard";
-import {getAcoesAssociacao} from "../../services/Dashboard.service";
-import {exibeDataPT_BR} from "../../utils/ValidacoesAdicionaisFormularios"
+import React, { useEffect, useState } from 'react'
+import { PaginasContainer } from '../PaginasContainer'
+import { Dashboard } from '../../componentes/Dashborard'
+import { getAcoesAssociacao } from '../../services/Dashboard.service'
+import {
+  exibeDataPT_BR,
+  getCorStatusPeriodo,
+  getTextoStatusPeriodo,
+} from '../../utils/ValidacoesAdicionaisFormularios'
+import { BarraDeStatusPeriodoAssociacao } from '../../componentes/Dashborard/BarraDeStatusPeriodoAssociacao'
 
 export const DashboardPage = () => {
+  const [acoesAssociacao, setAcoesAssociacao] = useState({})
 
-    const [acoesAssociacao, setAcoesAssociacao] = useState({})
+  useEffect(() => {
+    buscaListaAcoesAssociacao()
+  }, [])
 
-    useEffect(() => {
-        buscaListaAcoesAssociacao();
-    }, [])
+  const buscaListaAcoesAssociacao = async () => {
+    const listaAcoes = await getAcoesAssociacao()
+    setAcoesAssociacao(listaAcoes)
+  }
 
-    const buscaListaAcoesAssociacao = async () => {
-        const listaAcoes = await getAcoesAssociacao()
-        setAcoesAssociacao(listaAcoes)
-    }
-
-    return (
-        <PaginasContainer>
-            <h1 className="titulo-itens-painel mt-5">Painel</h1>
-            <h2 className="subtitulo-itens-painel-out">Ações recebidas no período: {exibeDataPT_BR(acoesAssociacao.data_inicio_realizacao_despesas)} até {exibeDataPT_BR(acoesAssociacao.data_fim_realizacao_despesas)}</h2>
-            <div className="page-content-inner bg-transparent p-0">
-                <Dashboard
-                    acoesAssociacao={acoesAssociacao}
-                />
-            </div>
-        </PaginasContainer>
-    );
+  return (
+    <PaginasContainer>
+      <h1 className="titulo-itens-painel mt-5">Painel</h1>
+      <h2 className="subtitulo-itens-painel-out">
+        {'Período: '}
+        {acoesAssociacao.periodo_referencia} {' - '}
+        {exibeDataPT_BR(acoesAssociacao.data_inicio_realizacao_despesas)}
+        {' até '}
+        {exibeDataPT_BR(acoesAssociacao.data_fim_realizacao_despesas)}
+        {'.'}
+      </h2>
+      <BarraDeStatusPeriodoAssociacao
+        statusPeriodoAssociacao={acoesAssociacao.periodo_status}
+        corBarraDeStatusPeriodoAssociacao={getCorStatusPeriodo(
+          acoesAssociacao.periodo_status
+        )}
+        textoBarraDeStatusPeriodoAssociacao={getTextoStatusPeriodo(
+          acoesAssociacao.periodo_status
+        )}
+      />
+      <div className="page-content-inner bg-transparent p-0">
+        <Dashboard acoesAssociacao={acoesAssociacao} />
+      </div>
+    </PaginasContainer>
+  )
 }
