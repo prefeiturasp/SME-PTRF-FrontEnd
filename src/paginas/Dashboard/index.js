977034d8ad2203414ce46e9e89c12bfa14bf710d
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-05-26 16:41:48 -0300

    Iniciando permitir navegar entre periodos

--- src/paginas/Dashboard/index.js
+++ src/paginas/Dashboard/index.js
@@ -1,49 +1,92 @@
-import React, { useEffect, useState } from 'react'
-import { PaginasContainer } from '../PaginasContainer'
-import { Dashboard } from '../../componentes/Dashborard'
-import { getAcoesAssociacao } from '../../services/Dashboard.service'
+import React, {useEffect, useState} from 'react'
+import {PaginasContainer} from '../PaginasContainer'
+import {Dashboard} from '../../componentes/Dashborard'
+import {getAcoesAssociacao,getAcoesAssociacaoPorPeriodo} from '../../services/Dashboard.service'
 import {
-  exibeDataPT_BR,
-  getCorStatusPeriodo,
-  getTextoStatusPeriodo,
+    exibeDataPT_BR,
+    getCorStatusPeriodo,
+    getTextoStatusPeriodo,
 } from '../../utils/ValidacoesAdicionaisFormularios'
-import { BarraDeStatusPeriodoAssociacao } from '../../componentes/Dashborard/BarraDeStatusPeriodoAssociacao'
+import {BarraDeStatusPeriodoAssociacao} from '../../componentes/Dashborard/BarraDeStatusPeriodoAssociacao'
+import {getPeriodos} from "../../services/PrestacaoDeContas.service";
 
 export const DashboardPage = () => {
-  const [acoesAssociacao, setAcoesAssociacao] = useState({})
+    const [acoesAssociacao, setAcoesAssociacao] = useState({})
+    const [periodosAssociacao, setPeriodosAssociacao] = useState(false);
 
-  useEffect(() => {
-    buscaListaAcoesAssociacao()
-  }, [])
 
-  const buscaListaAcoesAssociacao = async () => {
-    const listaAcoes = await getAcoesAssociacao()
-    setAcoesAssociacao(listaAcoes)
-  }
+    useEffect(() => {
+        buscaListaAcoesAssociacao()
+    }, [])
 
-  return (
-    <PaginasContainer>
-      <h1 className="titulo-itens-painel mt-5">Painel</h1>
-      <h2 className="subtitulo-itens-painel-out">
-        {'Período: '}
-        {acoesAssociacao.periodo_referencia} {' - '}
-        {exibeDataPT_BR(acoesAssociacao.data_inicio_realizacao_despesas)}
-        {' até '}
-        {exibeDataPT_BR(acoesAssociacao.data_fim_realizacao_despesas)}
-        {'.'}
-      </h2>
-      <BarraDeStatusPeriodoAssociacao
-        statusPeriodoAssociacao={acoesAssociacao.periodo_status}
-        corBarraDeStatusPeriodoAssociacao={getCorStatusPeriodo(
-          acoesAssociacao.periodo_status
-        )}
-        textoBarraDeStatusPeriodoAssociacao={getTextoStatusPeriodo(
-          acoesAssociacao.periodo_status
-        )}
-      />
-      <div className="page-content-inner bg-transparent p-0">
-        <Dashboard acoesAssociacao={acoesAssociacao} />
-      </div>
-    </PaginasContainer>
-  )
+    const buscaListaAcoesAssociacao = async () => {
+        let periodos = await getPeriodos();
+        //console.log("carregaPeriodos ", periodos)
+
+        setPeriodosAssociacao(periodos);
+        const listaAcoes = await getAcoesAssociacao();
+        console.log("buscaListaAcoesAssociacao ", listaAcoes)
+        setAcoesAssociacao(listaAcoes)
+
+    }
+
+    const handleChangePeriodo = async (value) => {
+
+        console.log("handleChangePeriodo ", value)
+
+        let acoesPorPeriodo = await getAcoesAssociacaoPorPeriodo(value)
+        console.log("acoesPorPeriodo ", acoesPorPeriodo)
+        setAcoesAssociacao(acoesPorPeriodo)
+
+
+    }
+
+    return (
+        <PaginasContainer>
+            <h1 className="titulo-itens-painel mt-5">Painel</h1>
+            <div className="form-row align-items-center mb-3">
+                <div className="col-auto my-1">
+                    <h2 className="subtitulo-itens-painel-out mb-0">Ações recebidas no período:</h2>
+                </div>
+                <div className="col-auto my-1">
+                    <select
+                        value={periodosAssociacao.uuid}
+                        onChange={(e) => handleChangePeriodo(e.target.value)}
+                        name="periodo"
+                        id="periodo"
+                        className="form-control"
+                    >
+                        <option value="">Escolha um período</option>
+                        {periodosAssociacao && periodosAssociacao.map((periodo)=>
+                            <option key={periodo.uuid} value={periodo.uuid}>{`${periodo.referencia} - ${periodo.data_inicio_realizacao_despesas ? exibeDataPT_BR(periodo.data_inicio_realizacao_despesas) : "-"} até ${periodo.data_fim_realizacao_despesas ? exibeDataPT_BR(periodo.data_fim_realizacao_despesas) : "-"}`}</option>
+                        )}
+                    </select>
+                </div>
+
+            </div>
+
+
+
+            {/*<h2 className="subtitulo-itens-painel-out">
+                {'Período: '}
+                {acoesAssociacao.periodo_referencia} {' - '}
+                {exibeDataPT_BR(acoesAssociacao.data_inicio_realizacao_despesas)}
+                {' até '}
+                {exibeDataPT_BR(acoesAssociacao.data_fim_realizacao_despesas)}
+                {'.'}
+            </h2>*/}
+            <BarraDeStatusPeriodoAssociacao
+                statusPeriodoAssociacao={acoesAssociacao.periodo_status}
+                corBarraDeStatusPeriodoAssociacao={getCorStatusPeriodo(
+                    acoesAssociacao.periodo_status
+                )}
+                textoBarraDeStatusPeriodoAssociacao={getTextoStatusPeriodo(
+                    acoesAssociacao.periodo_status
+                )}
+            />
+            <div className="page-content-inner bg-transparent p-0">
+                <Dashboard acoesAssociacao={acoesAssociacao}/>
+            </div>
+        </PaginasContainer>
+    )
 }
