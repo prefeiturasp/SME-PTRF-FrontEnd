7dac1f4323ad807e7b7e54b141b8ad65fddcae22
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-05-28 16:47:28 -0300

    Corrigindo textos errados

--- src/componentes/PrestacaoDeContas/DetalheDasPrestacoes/TabelaDeLancamentosReceitas/index.js
+++ src/componentes/PrestacaoDeContas/DetalheDasPrestacoes/TabelaDeLancamentosReceitas/index.js
@@ -69,7 +69,7 @@ export const TabelaDeLancamentosReceitas = ({conciliados, receitas, checkboxRece
     return (
         <div className="row mt-4">
             <div className="col-12">
-                <p className="detalhe-das-prestacoes-titulo-lancamentos">Laçamentos {conciliados ? "conciliados" : "pendentes de conciliação"}</p>
+                <p className="detalhe-das-prestacoes-titulo-lancamentos">Lançamentos {conciliados ? "conciliados" : "pendentes de conciliação"}</p>
                 <div className="content-section implementation">
                     {receitas && receitas.length > 0 ? (
                         <DataTable
