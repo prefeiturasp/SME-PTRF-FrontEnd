c19ef5eccbd7de2283d0a8922edfbb5343c96df5
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-06-03 13:55:22 -0300

    Criando Componente TextoDinamicoSuperior e colocando sombra no cabecalho conforme protótipo

---
+++ src/componentes/GeracaoDaAta/VisualizacaoDaAta/TextoDinamicoSuperior/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const TextoDinamicoSuperior = () => {
+    return(
+        <>
+            <p>
+                ATA DA REUNIÃO ___ DA Associação “Escola Municipal de Educação Infantil Emílio Ribas” DO(A) Unidade Educacional “Escola Municipal de Educação Infantil Emílio Ribas”
+            </p>
+
+            <p>
+                Aos ___ dias do mês de ___ de ___, reuniram-se os membros da Asociação “Escola Municipal de Educação Infantil Emílio Ribas” do(a) Unidade Educacional “Escola Municipal de Educação Infantil Emílio Ribas no(a) ___ , para tratar da seguinte pauta: Apresentação ao Conselho Fiscal da prestação de contas da verba do PTRF
+                e suas ações agregadas, do período de 20/02/2019  até  20/05/2020, referente ao 1° repasse de 2019. Aberta a reunião em ___ , ___ convocação pelo(a) Senhor(a)                                                ___ , ___ e verificada a existência de número legal de membros presentes, o(a) senhor(a) ___ apresentou os documentos fiscais referentes às despesas realizadas no período para análise dos presentes, conforme segue:
+            </p>
+        </>
+    )
+}
--- src/componentes/GeracaoDaAta/VisualizacaoDaAta/index.js
+++ src/componentes/GeracaoDaAta/VisualizacaoDaAta/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "../geracao-da-ata.scss"
 import {TopoComBotoes} from "./TopoComBotoes";
+import {TextoDinamicoSuperior} from "./TextoDinamicoSuperior";
 
 export const VisualizacaoDaAta = () => {
     return(
         <div className="col-12 container-visualizacao-da-ata">
-            <div className="col-12 mt-4 mb-5">
+            <div className="col-12 mt-4">
                 <TopoComBotoes/>
+            </div>
+            <div id="copiar" className="col-12">
+                <TextoDinamicoSuperior/>
             </div>
         </div>
     )
