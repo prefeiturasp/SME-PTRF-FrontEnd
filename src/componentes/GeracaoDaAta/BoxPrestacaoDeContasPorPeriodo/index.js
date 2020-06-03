5e2d5b1c0bd86d4a77dea44ce978ecaa6eddec67
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-06-03 11:50:19 -0300

    Estilizando componentes

--- src/componentes/GeracaoDaAta/BoxPrestacaoDeContasPorPeriodo/index.js
+++ src/componentes/GeracaoDaAta/BoxPrestacaoDeContasPorPeriodo/index.js
@@ -8,13 +8,13 @@ export const BoxPrestacaoDeContasPorPeriodo = () => {
                 <h1 className="titulo-box-prestacao-de-contas-por-periodo">Ata de apresentação da prestação de contas por período</h1>
                 <div className="col-12">
 
-                    <div className="row mt-3 border">
+                    <div className="row mt-3 border pt-4 pb-4">
                         <div className="col-12 col-md-8">
-                            <p>Ata de apresentação da prestação de contas</p>
-                            <p>Ata de apresentação da prestação de contas</p>
+                            <p className='fonte-14 mb-1'><strong>Ata de apresentação da prestação de contas</strong></p>
+                            <p className="fonte-12 mb-1 status-preenchido">Último preenchimento em 10/05/2020 10:50</p>
                         </div>
-                        <div className="col-12 col-md-4 float-right align-self-center">
-                            <button type="button" className="btn btn btn-outline-success float-right">Cadastrar despesa</button>
+                        <div className="col-12 col-md-4 align-self-center">
+                            <button type="button" className="btn btn-success float-right">Visualizar ata</button>
                         </div>
                     </div>
 
