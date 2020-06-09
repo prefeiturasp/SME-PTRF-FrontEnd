6bd3893ed1ad8340b34675799b628b0f5afe1168
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-06-09 10:17:59 -0300

    Criando AtasAssociacao.service.js

---
+++ src/services/AtasAssociacao.service.js
@@ -0,0 +1,10 @@
+import api from './Api'
+import { TOKEN_ALIAS } from './auth.service.js';
+import {ASSOCIACAO_UUID} from "./auth.service";
+
+const authHeader = {
+    headers: {
+        'Authorization': `JWT ${localStorage.getItem(TOKEN_ALIAS)}`,
+        'Content-Type': 'application/json'
+    }
+}
