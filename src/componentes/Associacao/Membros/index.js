facab38d5230a6af41418e6d34fa3ec9374b6284
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-06-22 09:57:19 -0300

    Criando menu interno, folha de estilo e iniciando Membros.js

---
+++ src/componentes/Associacao/Membros/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Membros = () =>{
+    return(
+
+        <h1>Membros</h1>
+    );
+}
---
+++ src/componentes/Associacao/MenuInterno/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+
+export const MenuInterno = () => {
+    return (
+        <>
+            <nav className="nav mb-4 mt-2 menu-interno">
+                <li className="nav-item"><NavLink to='/dados-da-associacao' activeClassName="active">Dados da associação</NavLink></li>
+                <li className="nav-item" ><NavLink to='/lista-de-despesas' activeClassName="active">Membros</NavLink></li>
+                <li className="nav-item"><NavLink to='/lista-de-receitas' activeClassName="active">Dados das contas</NavLink></li>
+            </nav>
+        </>
+    );
+}
