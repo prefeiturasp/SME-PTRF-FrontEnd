9c95167c82ac1b1e4a789aa2147785b8ffa22423
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-06-22 13:11:30 -0300

    Setando comportamento e estilos aos icones faAngleDown e faAngleUp

--- src/componentes/Associacao/Membros/index.js
+++ src/componentes/Associacao/Membros/index.js
@@ -1,16 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import {MenuInterno} from "../MenuInterno";
 import {TabelaMembros} from "../TabelaMembros";
 
 export const MembrosDaAssociacao = () =>{
+
+    const [clickIconeToogle, setClickIconeToogle] = useState(false)
     return(
         <div className="row">
             <div className="col-12">
                 <MenuInterno/>
-                <TabelaMembros/>
-
+                <TabelaMembros
+                    clickIconeToogle={clickIconeToogle}
+                    setClickIconeToogle={setClickIconeToogle}
+                />
             </div>
         </div>
-
     );
 }
--- src/componentes/Associacao/TabelaMembros/index.js
+++ src/componentes/Associacao/TabelaMembros/index.js
@@ -1,25 +1,57 @@
 import React from "react";
 import "../associacao.scss"
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faAngleDown, faAngleUp, faEdit} from '@fortawesome/free-solid-svg-icons'
 
-export const TabelaMembros = () => {
-    return(
-      <>
-          <table className="table table-bordered tabela-membros">
-              <thead>
-              <tr className="cabecalho">
-                  <th scope="col">Cargo na associação</th>
-                  <th scope="col">Nome completo</th>
-                  <th scope="col">Representação na associação</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-              </tr>
-              </tbody>
-          </table>
-      </>
+export const TabelaMembros = ({clickIconeToogle, setClickIconeToogle}) => {
+    return (
+        <>
+            <p><strong>Diretoria Executiva</strong></p>
+            <table className="table table-bordered tabela-membros">
+                <thead>
+                <tr className="cabecalho">
+                    <th scope="col">Cargo na associação</th>
+                    <th scope="col">Nome completo</th>
+                    <th scope="col">Representação na associação</th>
+                    <th scope="col"></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>
+                        <div className="d-flex justify-content-start">
+                            <a onClick={()=>setClickIconeToogle(!clickIconeToogle)} data-toggle="collapse" href="#collapseExample" role="button"
+                               aria-expanded="false" aria-controls="collapseExample">
+                                <FontAwesomeIcon
+                                    style={{fontSize: '30px', marginRight:'5px'}}
+                                    icon={clickIconeToogle ? faAngleUp : faAngleDown}
+                                />
+                            </a>
+                            <span>Presidente</span>
+                        </div>
+                    </td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>
+                        <div className="d-flex justify-content-center">
+                            <a>
+                                <FontAwesomeIcon
+                                    style={{fontSize: '20px', marginRight:"0"}}
+                                    icon={faEdit}
+                                />
+                            </a>
+                        </div>
+
+                    </td>
+                </tr>
+                <tr className="collapse cabecalho" id="collapseExample">
+                    <td colSpan="4" >
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+                        Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </>
     );
 }
