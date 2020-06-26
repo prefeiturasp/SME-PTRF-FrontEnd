b5b371f70da9bb1ec7845d94d9a511e16b6397c0
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-06-26 14:48:28 -0300

    Criando Service getDespesaCadastrada

--- src/services/Despesas.service.js
+++ src/services/Despesas.service.js
@@ -7,27 +7,27 @@ const authHeader = {
         'Authorization': `JWT ${localStorage.getItem(TOKEN_ALIAS)}`,  
         'Content-Type': 'application/json'
     }
-}
+};
 
 export const deleteDespesa = async uuid => {
     return (await api.delete(`api/despesas/${uuid}/`, authHeader)).data
-}
+};
 
 export const getDespesasTabelas = async () => {
     return (await api.get(`api/despesas/tabelas/`, authHeader)).data
-}
+};
 
 export const getEspecificacoesCapital = async () => {
     return (await api.get(`api/especificacoes/?aplicacao_recurso=CAPITAL`, authHeader)).data
-}
+};
 
 export const getEspecificacoesCusteio = async (id_tipo_custeio) => {
     return (await api.get(`api/especificacoes/?aplicacao_recurso=CUSTEIO&tipo_custeio=${id_tipo_custeio}`, authHeader)).data
-}
+};
 
 export const getDespesa = async (idDespesa) => {
     return (await api.get(`api/despesas/${idDespesa}`, authHeader)).data
-}
+};
 
 export const criarDespesa = async (payload) => {
     return api.post('api/despesas/', payload, authHeader).then(response => {
@@ -35,7 +35,7 @@ export const criarDespesa = async (payload) => {
     }).catch(error => {
         return error.response;
     });
-}
+};
 
 export const alterarDespesa = async (payload, idDespesa) => {
     return api.put(`api/despesas/${idDespesa}/`, payload, authHeader).then(response => {
@@ -43,7 +43,7 @@ export const alterarDespesa = async (payload, idDespesa) => {
     }).catch(error => {
         return error.response;
     });
-}
+};
 
 export const getNomeRazaoSocial = async (cpf_cnpj) => {
     if (cpf_cnpj){
@@ -51,10 +51,14 @@ export const getNomeRazaoSocial = async (cpf_cnpj) => {
     }else {
         return ""
     }
-}
+};
 
 export const getPeriodoFechadoDespesa = async (palavra, aplicacao_recurso, acao_associacao__uuid, despesa__status) => {
     return (await api.get(`api/rateios-despesas/totais/?search=${palavra}&aplicacao_recurso=${aplicacao_recurso}&acao_associacao__uuid=${acao_associacao__uuid}&despesa__status=${despesa__status}&associacao__uuid=${localStorage.getItem(ASSOCIACAO_UUID)}`, authHeader)).data
-}
+};
+
+export const getDespesaCadastrada = async (tipo_documento, numero_documento, cpf_cnpj_fornecedor, despesa_uuid=null) => {
+    return (await api.get(`api/despesas/ja-lancada/?tipo_documento=${tipo_documento}&numero_documento=${numero_documento}&cpf_cnpj_fornecedor=${cpf_cnpj_fornecedor}${despesa_uuid ? '&despesa_uuid='+despesa_uuid : ''}`, authHeader)).data
+};
 
 
