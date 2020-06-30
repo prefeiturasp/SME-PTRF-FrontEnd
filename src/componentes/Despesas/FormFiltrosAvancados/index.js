65292cc4e346f7aad9e4f541eff8e9960a5cecb9
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-06-30 12:18:56 -0300

    Corrigindo formato da data_inicio e data_fim, agora quando datas forem nulas para enviar para a API

--- src/componentes/Despesas/FormFiltrosAvancados/index.js
+++ src/componentes/Despesas/FormFiltrosAvancados/index.js
@@ -38,14 +38,16 @@ export const FormFiltrosAvancados = (props) => {
     };
 
     const handleSubmit = async (event) => {
-
-        console.log("handleSubmit")
         event.preventDefault();
         //setLoading(true);
 
         reusltadoSomaDosTotais(state.filtrar_por_termo, state.aplicacao_recurso, state.acao_associacao, state.despesa_status);
-        let data_inicio = state.data_inicio ? moment(new Date(state.data_inicio), "YYYY-MM-DD").format("YYYY-MM-DD") : "";
-        let data_fim = state.data_fim ? moment(new Date(state.data_fim), "YYYY-MM-DD").format("YYYY-MM-DD") : "";
+        let data_inicio = state.data_inicio ? moment(new Date(state.data_inicio), "YYYY-MM-DD").format("YYYY-MM-DD") : null;
+        let data_fim = state.data_fim ? moment(new Date(state.data_fim), "YYYY-MM-DD").format("YYYY-MM-DD") : null;
+
+        console.log("handleSubmit data_inicio ", data_inicio)
+        console.log("handleSubmit data_fim ", data_fim)
+
         const lista_retorno_api = await filtrosAvancadosRateios(state.filtrar_por_termo, state.aplicacao_recurso, state.acao_associacao, state.despesa_status, state.fornecedor, data_inicio, data_fim);
         setLista(lista_retorno_api)
         setBuscaUtilizandoFiltro(true)
