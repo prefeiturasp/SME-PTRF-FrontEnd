51b6004ceec4f758e72cf3e27111bc3142acb9c9
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-06-30 14:04:00 -0300

    Passando datas para o service filtrosAvancadosReceitas

--- src/services/Receitas.service.js
+++ src/services/Receitas.service.js
@@ -72,7 +72,7 @@ export const filtroPorPalavraReceitas = async (palavra) => {
     return (await api.get(`api/receitas/?search=${palavra}&associacao__uuid=${localStorage.getItem(ASSOCIACAO_UUID)}`, authHeader)).data
 };
 
-export const filtrosAvancadosReceitas = async (palavra, tipo_receita, acao_associacao__uuid, conta_associacao__uuid) => {
+export const filtrosAvancadosReceitas = async (palavra, tipo_receita, acao_associacao__uuid, conta_associacao__uuid, data_inicio, data_fim) => {
     return (await api.get(`api/receitas/?search=${palavra}&associacao__uuid=${localStorage.getItem(ASSOCIACAO_UUID)}&tipo_receita=${tipo_receita}&acao_associacao__uuid=${acao_associacao__uuid}&conta_associacao__uuid=${conta_associacao__uuid}`, authHeader)).data
 };
 
