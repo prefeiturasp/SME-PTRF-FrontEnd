58622c036ff63974192c9c3a659412db95e28f31
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-07-02 10:25:27 -0300

    Criando Pagina DadosDasContasAssociacao e rotas

--- src/paginas/DadosDasContasAssociacao/index.js
+++ src/paginas/DadosDasContasAssociacao/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import {PaginasContainer} from "../PaginasContainer";
-import {MembrosDaAssociacao} from "../../componentes/Associacao/Membros";
+import {DadosDasContas} from "../../componentes/Associacao/DadosDasContas";
 
 
-export const MembrosDaAssociacaoPage = () =>{
+export const DadosDasContasPage = () =>{
     return (
         <PaginasContainer>
             <h1 className="titulo-itens-painel mt-5">Membros</h1>
             <div className="page-content-inner">
-                <MembrosDaAssociacao/>
+                <DadosDasContas/>
             </div>
         </PaginasContainer>
     )
