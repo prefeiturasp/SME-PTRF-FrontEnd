4bfdfc1eaf0f257fdabdeab6d80e43fc15032bab
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-07-02 10:39:56 -0300

    Inserindo loading e menu interno

--- src/componentes/Associacao/DadosDasContas/index.js
+++ src/componentes/Associacao/DadosDasContas/index.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {UrlsMenuInterno} from "../UrlsMenuInterno";
+import Loading from "../../../utils/Loading";
+import {MenuInterno} from "../../MenuInterno";
 
-export const DadosDasContas = () =>{
-    return(
+export const DadosDasContas = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        setLoading(false)
+    }, []);
+
+    return (
         <>
-            <h1>Dados da contas</h1>
+            {loading ? (
+                    <Loading
+                        corGrafico="black"
+                        corFonte="dark"
+                        marginTop="0"
+                        marginBottom="0"
+                    />
+                ) :
+                <div className="row">
+                    <div className="col-12">
+                        <MenuInterno
+                            caminhos_menu_interno = {UrlsMenuInterno}
+                        />
+                        <h1>Dados da contas</h1>
+                    </div>
+                </div>
+            }
         </>
     )
-}
+};
