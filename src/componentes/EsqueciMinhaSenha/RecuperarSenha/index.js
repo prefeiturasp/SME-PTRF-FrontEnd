10f380b8e2713a54741b8ff02c4d8080e56a9769
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-07-13 11:57:26 -0300

    Criando esqueci-minha-senha.scss e iniciando a diagramação de RecuperarSenha.js

--- src/componentes/EsqueciMinhaSenha/RecuperarSenha/index.js
+++ src/componentes/EsqueciMinhaSenha/RecuperarSenha/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-export const RecuperarMinhaSenha = () =>{
-    return(
-        <div className='w-75'>
-        <h1>Componente Recuperar Senha</h1>
-        <button type="button" onClick={()=>window.location.assign('/esqueci-minha-senha/recuperacao-de-email')} className="btn btn-link">Esqueci minha senha</button>
+export const RecuperarMinhaSenha = () => {
+    return (
+        <>
+        <div className="esqueci-minha-senha-inner-texto">
+            <h1 className="titulo-services">Recuperação de Senha</h1>
+            <p>Caso você tenha cadastrado um endereço de e-mail, informe seu usuário ou RF e ao continuar você receberá um e-mail com as orientações para redefinição da sua senha.</p>
+            <p>Se você não tem um e-mail cadastrado ou não tem mais acessoa ao endereço de e-mail cadastrado, procure o responsável pelo SGP na sua unidade</p>
         </div>
+            <div className='col-12'>
+                <button type="button" onClick={() => window.location.assign('/esqueci-minha-senha/recuperacao-de-email/')} className="btn btn-link">Esqueci minha senha</button>
+            </div>
+        </>
     );
 };
--- src/componentes/EsqueciMinhaSenha/index.js
+++ src/componentes/EsqueciMinhaSenha/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import {useParams} from "react-router-dom";
 import {RecuperarMinhaSenha} from "./RecuperarSenha";
 import {RecuperacaoDeEmail} from "./RecuperacaoDeEmail";
+import "./esqueci-minha-senha.scss"
 
 export const EsqueciMinhaSenha = () =>{
     const {service} = useParams();
@@ -9,13 +10,13 @@ export const EsqueciMinhaSenha = () =>{
     console.log("Rota ", service)
 
     return(
-        <>
+        <div className='container-esqueci-minha-senha'>
             {service === 'recuperar-minha-senha' &&
                 <RecuperarMinhaSenha/>
             }
             {service === 'recuperacao-de-email' &&
                 <RecuperacaoDeEmail/>
             }
-        </>
+        </div>
     );
 };
