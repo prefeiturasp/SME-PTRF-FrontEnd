0308a959daf9fb88323f906bb7361ec2a6b237ba
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-07-13 15:48:17 -0300

    Criando componente RedefinirSenha

---
+++ src/componentes/EsqueciMinhaSenha/RedefinirSenha/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const RedefinirSenha = () => {
+    return (
+        <>
+            <div className="esqueci-minha-senha-inner-texto">
+                <h1 className="titulo-services mb-3">Redefinir Senha</h1>
+            </div>
+        </>
+    );
+}
