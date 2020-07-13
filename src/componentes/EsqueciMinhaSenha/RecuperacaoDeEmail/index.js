c8cf828fb9f5f371615dee219785ea7bcbaf03a8
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-07-13 15:23:12 -0300

    Criando e utilizando método mascaraExibicaoEmail

--- src/componentes/EsqueciMinhaSenha/RecuperacaoDeEmail/index.js
+++ src/componentes/EsqueciMinhaSenha/RecuperacaoDeEmail/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 
-export const RecuperacaoDeEmail = ({recuperacaoDeEmail}) =>{
-    console.log("RecuperacaoDeEmail ", recuperacaoDeEmail)
+export const RecuperacaoDeEmail = ({recuperacaoDeEmail, emailComMascara}) =>{
+    console.log("RecuperacaoDeEmail ", recuperacaoDeEmail);
     return (
         <>
             <div className="esqueci-minha-senha-inner-texto">
                 <h1 className="titulo-services">Recuperação de Senha</h1>
                 {recuperacaoDeEmail.encontrado ? (
-                    <p>Seu email é: {recuperacaoDeEmail.usuario}</p>
+                    <>
+                    <p>
+                        Seu link de recuperação de senha foi enviado para: <br/>{emailComMascara}
+                    </p>
+                    <p className="mb-5">Verifique sua caixa de entrada!</p>
+                    <button onClick={()=>window.location.assign("/login")} className="btn btn-success btn-block">Continuar</button>
+                    </>
                 ): <p>Seu email é NÃO FOI ENCONTRADO</p>}
             </div>
         </>
--- src/componentes/EsqueciMinhaSenha/index.js
+++ src/componentes/EsqueciMinhaSenha/index.js
@@ -3,11 +3,13 @@ import {RecuperarMinhaSenha} from "./RecuperarSenha";
 import {RecuperacaoDeEmail} from "./RecuperacaoDeEmail";
 import "./esqueci-minha-senha.scss"
 import {YupSignupSchemaRecuperarSenha} from "../../utils/ValidacoesAdicionaisFormularios";
+import {replace} from "formik";
 
 export const EsqueciMinhaSenha = () =>{
 
     const [service, setService] = useState(false);
     const [recuperacaoDeEmail, setRecuperacaoDeEmail] = useState({});
+    const [emailComMascara, setEmailComMascara] = useState({});
     useEffect(()=>{
         setService('recuperar-minha-senha')
     }, []);
@@ -21,8 +23,18 @@ export const EsqueciMinhaSenha = () =>{
         setRecuperacaoDeEmail({
             usuario:values.usuario,
             encontrado:true,
-        })
+        });
+        setEmailComMascara(mascaraExibicaoEmail(values.usuario));
         setService('recuperacao-de-email')
+    };
+
+    const mascaraExibicaoEmail = (email) =>{
+        let pos_arroba = email.indexOf("@");
+        let iniciais_email = email.substr(0,3);
+        let conteudo_arroba = email.substr(3, pos_arroba -3 );
+        let assinatura_email = email.substr(pos_arroba);
+        let email_com_mascara = iniciais_email + conteudo_arroba.replace(/\w/gim, "*") + assinatura_email;
+        return email_com_mascara
     };
 
     return(
@@ -38,9 +50,9 @@ export const EsqueciMinhaSenha = () =>{
             {service === 'recuperacao-de-email' &&
                 <RecuperacaoDeEmail
                     recuperacaoDeEmail={recuperacaoDeEmail}
+                    emailComMascara={emailComMascara}
                 />
             }
-
         </div>
     );
 };
