64f3cb4f7c42e65ed8d5e3512c2943257727eef6
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-07-14 09:07:53 -0300

    Criando Box com mensagem sobre a força da senha

--- src/componentes/EsqueciMinhaSenha/RedefinirSenha/index.js
+++ src/componentes/EsqueciMinhaSenha/RedefinirSenha/index.js
@@ -159,6 +159,11 @@ export const RedefinirSenha = () => {
                                     <p className='requisitos-seguranca-senha'><span id='caracteres_acentuados' className='pr-4'>Não podem conter caracteres acentuados</span></p>
                                     <p className='requisitos-seguranca-senha'><span id='numero_ou_caracter_especial' className='pr-4'>Um número ou símbolo (caracter especial)</span></p>
                                     <p className='requisitos-seguranca-senha'><span id='entre_oito_ate_doze' className='pr-4'>Deve ter no mínimo 8 e no máximo 12 caracteres</span></p>
+
+                                    {btnOnsubmitReadOnly &&
+                                        <p className="forca-senha-msg mt-3 p-2 text-center">Sua nova senha deve conter letras maiúsculas, minúsculas, números e símbolos. Por favor, digite outra senha</p>
+                                    }
+
                                 </div>
 
                                 <div className="d-flex  justify-content-end pb-3 mt-3">
