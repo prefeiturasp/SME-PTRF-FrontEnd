774903ab2bcdc6ad590ca0f3840487758f7cabe8
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-07-16 17:08:10 -0300

    Alterando senha com sucesso e exibindo mensagens de erro que vem do Back

--- src/componentes/MeusDados/index.js
+++ src/componentes/MeusDados/index.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 import "./meus-dados.scss"
 import {USUARIO_NOME, USUARIO_LOGIN, USUARIO_EMAIL, USUARIO_CPF} from "../../services/auth.service";
-import {AlterarSenhaMeusDados, EditarMembro} from "../../utils/Modais";
+import {AlterarSenhaMeusDados} from "../../utils/Modais";
+import {FormInicialEdiar} from "./formInicialEditar";
 
 export const MeusDados = () => {
     const [showEditarSenha, setShowEditarSenha] = useState(false);
@@ -18,38 +19,10 @@ export const MeusDados = () => {
                 </div>
 
                 <div className="col-12 col-md-8 px-5 py-4 align-self-center container-meus-dados-direita">
-                    <form>
-                        <div className="row">
-                            <div className="col-12 mt-3">
-                                <label htmlFor="email"><strong>Email</strong></label>
-                            </div>
-                            <div className='col-10'>
-                                <div className="form-group">
-                                    <input readOnly={true} name="email" type="email" className="form-control" id="email" placeholder="Clique em editar para inserir um e-mail"/>
-                                </div>
-                            </div>
-                            <div className='col-2'>
-                                <div className="form-group">
-                                    <button type="button" className="btn btn-outline-success">Editar</button>
-                                </div>
-                            </div>
+                    <FormInicialEdiar
+                        setShowEditarSenha={setShowEditarSenha}
+                    />
 
-                            <div className="col-12 mt-5">
-                                <label htmlFor="senha"><strong>Senha</strong></label>
-                            </div>
-
-                            <div className='col-10'>
-                                <div className="form-group">
-                                    <input readOnly={true} name="senha" type="password" className="form-control" id="senha" placeholder="************"/>
-                                </div>
-                            </div>
-                            <div className='col-2'>
-                                <div className="form-group">
-                                    <button onClick={()=>setShowEditarSenha(true)}  type="button" className="btn btn-outline-success">Editar</button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
                 </div>
                 <section>
                     <AlterarSenhaMeusDados
