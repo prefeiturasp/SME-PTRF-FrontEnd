d321525145b5b98bfcbe7350f75905c99f79685c
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-07-27 09:07:30 -0300

    Diagramando e estilizando componente Tags

--- src/componentes/Despesas/Tags/index.js
+++ src/componentes/Despesas/Tags/index.js
@@ -2,15 +2,24 @@ import React from "react";
 import "./tags.scss"
 
 export const Tags = () => {
-    return(
+    return (
+        <div className="container-tags mt-3">
+            <div className="form-row align-items-center box-escolha-tag">
+                <div className="col-auto">
+                    <p className='mb-0 mr-4 font-weight-normal'>Esse gasto possui vínculo a alguma etiqueta?</p>
+                </div>
+                <div className="col-auto">
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="radio" name="tags" id="tag_sim" value="sim"/>
+                        <label className="form-check-label" htmlFor="tag_sim">Sim</label>
+                    </div>
 
-        <div className="container-tags">
-            <div className='box-escolha-tag'>
-                <p>Estou aqui</p>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input" type="radio" name="tags" id="tag_nao" value="nao"/>
+                        <label className="form-check-label" htmlFor="tag_nao">Não</label>
+                    </div>
+                </div>
             </div>
         </div>
-
-
-
     )
-}
+};
