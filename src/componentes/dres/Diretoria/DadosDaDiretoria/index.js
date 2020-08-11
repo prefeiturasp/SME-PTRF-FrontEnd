42bff957d5b6941a7eb8bbfceef0187a338d7c99
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-08-11 05:36:24 -0300

    Trazendo dados da Diretoria

--- src/componentes/dres/Diretoria/DadosDaDiretoria/index.js
+++ src/componentes/dres/Diretoria/DadosDaDiretoria/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {getAssociacao} from "../../../../services/dres/Associacoes.service";
+import {ASSOCIACAO_UUID} from "../../../../services/auth.service";
 
 export const DadosDaDiretoria = () => {
-    return(
-        <h1>Dados da Diretoria Componente</h1>
+    const [dadosDiretoria, setDadosDiretoria] = useState(null);
+
+    useEffect(() => {
+        buscaDiretoria()
+    }, []);
+
+    const buscaDiretoria = async () => {
+        let diretoria = await getAssociacao(localStorage.getItem(ASSOCIACAO_UUID));
+        console.log("Diretoria ", diretoria.unidade.dre.nome)
+        setDadosDiretoria(diretoria.unidade)
+    };
+
+    return (
+        <>
+            {dadosDiretoria ? (
+                <>
+                    <div className="d-flex bd-highlight">
+                        <div className="p-2 flex-grow-1 bd-highlight">
+                            <h1 className="titulo-itens-painel mt-5">Dados da diretoria {dadosDiretoria.dre.nome}</h1>
+                        </div>
+                    </div>
+                    <div className="page-content-inner">
+                        <h4>AQUIAASDFASDF</h4>
+                    </div>
+                </>
+            ) : null}
+        </>
     )
 }
