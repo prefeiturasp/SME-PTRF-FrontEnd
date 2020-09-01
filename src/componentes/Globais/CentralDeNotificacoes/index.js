e119509ef6582c0d89d202659790657481a909ff
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-09-01 12:32:44 -0300

    Criando estilos e botoes seletores para Todas, Não Lidas e Lidas

--- src/componentes/Globais/CentralDeNotificacoes/index.js
+++ src/componentes/Globais/CentralDeNotificacoes/index.js
@@ -1,10 +1,25 @@
 import React from "react";
+import "./central-de-notificacoes.scss"
 
 export const CentralDeNotificacoes = () => {
 
+    const handleClickBtnCategorias = (e) =>{
+        console.log("Cliquei ", e.target.id)
+    };
+
     return(
       <>
-          <h1>CentralDeNotificacoes Componente</h1>
+          <div className="btn-group btn-group-toggle container-btn-broup-categorias" data-toggle="buttons">
+              <label className="btn btn-outline-light active">
+                  <input onClick={(e)=>handleClickBtnCategorias(e)}  onChange={()=>{}}  type="radio" name="options" id="todas" checked={true}/> Todas
+              </label>
+              <label className="btn btn-outline-light">
+                  <input onClick={(e)=>handleClickBtnCategorias(e)}  onChange={()=>{}}  type="radio" name="options" id="nao_lidas"/> Não Lidas
+              </label>
+              <label className="btn btn-outline-light">
+                  <input onClick={(e)=>handleClickBtnCategorias(e)}  onChange={()=>{}}  type="radio" name="options" id="lidas"/> Lidas
+              </label>
+          </div>
       </>
     );
 };
