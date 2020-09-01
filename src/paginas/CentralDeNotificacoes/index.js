44c0db16bcb4995a3de8f0c078b445c9f504ada0
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-09-01 11:07:08 -0300

    Criando rota e página CentralDeNotificacoes

---
+++ src/paginas/CentralDeNotificacoes/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {PaginasContainer} from "../PaginasContainer";
+
+
+export const CentralDeNotificacoesPage = () =>{
+    return (
+        <PaginasContainer>
+            <h1 className="titulo-itens-painel mt-5">Notificações</h1>
+            <div className="page-content-inner">
+                <h1>CentralDeNotificacoes PAGE</h1>
+            </div>
+        </PaginasContainer>
+    )
+};
