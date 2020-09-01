d4071e7331a0496a29f4bfae8dae61d5814ce760
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-09-01 12:43:36 -0300

    Compnetizando Botoes Categorias Notificacoes

--- src/componentes/Globais/CentralDeNotificacoes/index.js
+++ src/componentes/Globais/CentralDeNotificacoes/index.js
@@ -1,25 +1,18 @@
 import React from "react";
 import "./central-de-notificacoes.scss"
+import {BotoesCategoriasNotificacoes} from "./BotoesCategoriasNotificacoes";
 
 export const CentralDeNotificacoes = () => {
 
-    const handleClickBtnCategorias = (e) =>{
+    const handleClickBtnCategorias = (e) => {
         console.log("Cliquei ", e.target.id)
     };
 
-    return(
-      <>
-          <div className="btn-group btn-group-toggle container-btn-broup-categorias" data-toggle="buttons">
-              <label className="btn btn-outline-light active">
-                  <input onClick={(e)=>handleClickBtnCategorias(e)}  onChange={()=>{}}  type="radio" name="options" id="todas" checked={true}/> Todas
-              </label>
-              <label className="btn btn-outline-light">
-                  <input onClick={(e)=>handleClickBtnCategorias(e)}  onChange={()=>{}}  type="radio" name="options" id="nao_lidas"/> Não Lidas
-              </label>
-              <label className="btn btn-outline-light">
-                  <input onClick={(e)=>handleClickBtnCategorias(e)}  onChange={()=>{}}  type="radio" name="options" id="lidas"/> Lidas
-              </label>
-          </div>
-      </>
+    return (
+        <>
+            <BotoesCategoriasNotificacoes
+                handleClickBtnCategorias={handleClickBtnCategorias}
+            />
+        </>
     );
 };
