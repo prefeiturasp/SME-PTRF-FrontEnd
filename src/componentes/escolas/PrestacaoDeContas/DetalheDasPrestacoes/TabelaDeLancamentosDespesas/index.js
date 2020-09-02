09206290ee51a5837d3a0e2adca595d7149e979a
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-09-02 12:21:30 -0300

    Refatorando TabelaDeLancamentosDespesas/index.js

--- src/componentes/escolas/PrestacaoDeContas/DetalheDasPrestacoes/TabelaDeLancamentosDespesas/index.js
+++ src/componentes/escolas/PrestacaoDeContas/DetalheDasPrestacoes/TabelaDeLancamentosDespesas/index.js
@@ -18,27 +18,26 @@ export const TabelaDeLancamentosDespesas = ({conciliados, despesas, checkboxDesp
 
     const onShowModal = () => {
         setShowModal(true);
-    }
+    };
 
     const onHandleClose = () => {
         setShowModal(false);
-    }
+    };
 
     const onCancelarTrue = () => {
         setShowModal(false);
-        const url = '/edicao-de-despesa/' + uuid + '/tabela-de-lancamentos-despesas'
+        const url = '/edicao-de-despesa/' + uuid + '/tabela-de-lancamentos-despesas';
         history.push(url);
-    }
-
+    };
 
     const redirecionaDetalhe = value => {
-        setUuid(value.despesa)
+        setUuid(value.despesa);
         onShowModal();
-    }
+    };
 
     const notificarNaoConciliado = (notificarDiasNaoConferido) => {
         return notificarDiasNaoConferido > 0 ? {color: 'red', fontWeight: 'bold'} : {color: 'black'}
-    }
+    };
 
     const conferidoTemplate = (rowData) => {
         if (rowData.status_despesa === "COMPLETO"){
@@ -66,9 +65,9 @@ export const TabelaDeLancamentosDespesas = ({conciliados, despesas, checkboxDesp
             )
         }
 
-    }
+    };
 
-    const dataDocumentoTemplate = (rowData, column) => {
+    const dataDocumentoTemplate = (rowData) => {
         return (
             <div style={notificarNaoConciliado(rowData['notificar_dias_nao_conferido'])}>
                 {rowData['data_documento']
@@ -76,9 +75,9 @@ export const TabelaDeLancamentosDespesas = ({conciliados, despesas, checkboxDesp
                     : ''}
             </div>
         )
-    }
+    };
 
-    const dataTransacaoTemplate = (rowData, column) => {
+    const dataTransacaoTemplate = (rowData) => {
         return (
             <div style={notificarNaoConciliado(rowData['notificar_dias_nao_conferido'])}>
                 {rowData['data_transacao']
@@ -86,19 +85,19 @@ export const TabelaDeLancamentosDespesas = ({conciliados, despesas, checkboxDesp
                     : ''}
             </div>
         )
-    }
+    };
 
-    const valorTemplate = (rowData, column) => {
+    const valorTemplate = (rowData) => {
         const valorFormatado = rowData['valor_total']
-            ? new Number(rowData['valor_total']).toLocaleString('pt-BR', {
+            ? Number(rowData['valor_total']).toLocaleString('pt-BR', {
                 style: 'currency',
                 currency: 'BRL'
             })
-            : ''
+            : '';
         return (<span style={notificarNaoConciliado(rowData['notificar_dias_nao_conferido'])}>{valorFormatado}</span>)
-    }
+    };
 
-    const cnpjTemplate = (rowData, column) => {
+    const cnpjTemplate = (rowData) => {
         return (
             rowData['notificar_dias_nao_conferido'] > 0 ?
                 <div data-tip={`Não demonstrado por ${rowData['notificar_dias_nao_conferido']} dias.`}>
@@ -118,18 +117,17 @@ export const TabelaDeLancamentosDespesas = ({conciliados, despesas, checkboxDesp
                 </span>
 
         )
-    }
+    };
 
     const genericTemplate = (rowData, column) => {
         return (<span
             style={notificarNaoConciliado(rowData['notificar_dias_nao_conferido'])}>{rowData[column.field]}</span>)
-    }
+    };
 
-    const especificacaoTemplate = (rowData, column) => {
+    const especificacaoTemplate = (rowData) => {
         return (<span
             style={notificarNaoConciliado(rowData['notificar_dias_nao_conferido'])}>{rowData['especificacao_material_servico'].descricao}</span>)
-    }
-
+    };
 
     return (
         <div className="row mt-4">
@@ -181,6 +179,5 @@ export const TabelaDeLancamentosDespesas = ({conciliados, despesas, checkboxDesp
                 <RedirectModalTabelaLancamentos show={showModal} handleClose={onHandleClose} onCancelarTrue={onCancelarTrue}/>
             </section>
         </div>
-
     )
-}
+};
