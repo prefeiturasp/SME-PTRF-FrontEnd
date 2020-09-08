f4f54e5962a03a14b269f9cb1a59f0e5eb404462
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-09-08 17:10:45 -0300

    Criando estilos para botões de contas Associação

--- src/componentes/escolas/PrestacaoDeContas/index.js
+++ src/componentes/escolas/PrestacaoDeContas/index.js
@@ -4,6 +4,8 @@ import {getPeriodosDePrestacaoDeContasDaAssociacao} from "../../../services/esco
 import {getStatusPeriodoPorData} from "../../../services/escolas/PrestacaoDeContas.service";
 import {getTabelasReceita} from "../../../services/escolas/Receitas.service";
 import {BarraDeStatusPrestacaoDeContas} from "./BarraDeStatusPrestacaoDeContas";
+import {getAcoesAssociacaoPorConta} from "../../../services/Dashboard.service";
+import {NavLink} from "react-router-dom";
 
 export const PrestacaoDeContas = () => {
 
@@ -11,6 +13,8 @@ export const PrestacaoDeContas = () => {
     const [periodosAssociacao, setPeriodosAssociacao] = useState(false);
     const [statusPrestacaoDeConta, setStatusPrestacaoDeConta] = useState(false);
     const [contasAssociacao, setContasAssociacao] = useState(false);
+    const [clickBtnEscolheCategoria, setClickBtnEscolheCategoria] = useState({0: true});
+
 
     useEffect(() => {
         getPeriodoPrestacaoDeConta();
@@ -78,9 +82,19 @@ export const PrestacaoDeContas = () => {
         });
     };
 
+    const toggleBtnEscolheCategoria = (id) => {
+        setClickBtnEscolheCategoria({
+            [id]: !clickBtnEscolheCategoria[id]
+        });
+    };
+
+    const getAcoesPorConta = (uuid_conta) =>{
+        console.log("getAcoesPorConta ", uuid_conta)
+    };
+
     return (
         <>
-            {statusPrestacaoDeConta &&
+            {statusPrestacaoDeConta && Object.entries(statusPrestacaoDeConta).length > 0  &&
                 <BarraDeStatusPrestacaoDeContas
                     statusPrestacaoDeConta={statusPrestacaoDeConta}
                 />
@@ -94,13 +108,26 @@ export const PrestacaoDeContas = () => {
                 statusPrestacaoDeConta={statusPrestacaoDeConta}
             />
 
-            {contasAssociacao && contasAssociacao.length > 0 && contasAssociacao.map((conta, index)=>
-                <Fragment key={index}>
-                    <button className="btn btn-primary mr-2">{conta.nome}</button>
-                </Fragment>
+            {statusPrestacaoDeConta && Object.entries(statusPrestacaoDeConta).length > 0  &&
 
-            )}
-
+            <nav className="nav mb-4 mt-2 menu-interno">
+                {contasAssociacao && contasAssociacao.length > 0 && contasAssociacao.map((conta, index) =>
+                    <Fragment key={index}>
+                        <li className="nav-item">
+                            <button
+                                onClick={() => {
+                                    toggleBtnEscolheCategoria(index);
+                                    getAcoesPorConta(conta.uuid);
+                                }}
+                                className={`nav-link btn-escolhe-acao mr-3 ${clickBtnEscolheCategoria[index] ? "btn-escolhe-acao-active" : ""}`}
+                            >
+                                Conta {conta.nome}
+                            </button>
+                        </li>
+                    </Fragment>
+                )}
+            </nav>
+            }
         </>
     )
 };
