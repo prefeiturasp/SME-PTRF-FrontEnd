addca39c36cb5e596ae0764df1bf91a477979ef2
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-09-17 15:03:00 -0300

    Avançando no componente DashboardCard

--- src/componentes/dres/Dashboard/DashboardCard.js
+++ src/componentes/dres/Dashboard/DashboardCard.js
@@ -3,15 +3,20 @@ import React from "react";
 export const DashboardCard = ({ObjetoDashboard}) => {
     return (
         <>
-            <div className="row row-cols-1 row-cols-md-4">
+            <div className="row row-cols-1 row-cols-md-4 mt-3">
             {ObjetoDashboard && ObjetoDashboard.cards && ObjetoDashboard.cards.length > 0 && ObjetoDashboard.cards.map((card, index)=>
-                <div key={index} className="col-12 col-md-3 mb-4 pr-0">
+                <div key={index} className="col-12 col-md-3 mb-4 ">
                     <div className="card h-100 container-cards-dre-dashboard">
+                        <div className="card-header">{card.titulo}</div>
                         <div className="card-body">
-                            <h5 className="card-title borda-bottom ">{card.titulo}</h5>
-                            <p className="card-text card-qtde-associacoes">{card.gtdeAssociacoes}</p>
-                            <p className="card-text">{card.gtdeAssociacoes}</p>
+                            <p className="card-text card-qtde-associacoes  mb-0 pb-2">{card.gtdeAssociacoes}</p>
+                            <p className="text-center">
+                                <a href="#" className="btn  btn-outline-success">Ver as prestações</a>
+                            </p>
+
+
                         </div>
+
                     </div>
                 </div>
             )}
