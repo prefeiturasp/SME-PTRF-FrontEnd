776d09a752f00f4c1f49e00cb9adde4b57288539
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-09-17 11:12:57 -0300

    Criando rota e Página Dre Dashboard

---
+++ src/paginas/DreDashboard/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {PaginasContainer} from '../PaginasContainer'
+
+export const DreDashboardPage = () => {
+    return (
+        <PaginasContainer>
+            <h1 className="titulo-itens-painel mt-5">Resumo dos Recursos</h1>
+            <div className="page-content-inner">
+                <h1>Dre Dashboard Page</h1>
+            </div>
+        </PaginasContainer>
+
+    )
+};
