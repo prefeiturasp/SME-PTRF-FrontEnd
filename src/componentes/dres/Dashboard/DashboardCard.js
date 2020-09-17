0e38600d38440ae11ab445b4ae8934c732c3f822
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-09-17 15:58:37 -0300

    Refinando estilos cards

--- src/componentes/dres/Dashboard/DashboardCard.js
+++ src/componentes/dres/Dashboard/DashboardCard.js
@@ -1,17 +1,21 @@
 import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faChevronUp, faEye} from "@fortawesome/free-solid-svg-icons";
+import {faEye} from "@fortawesome/free-solid-svg-icons";
 
 export const DashboardCard = ({ObjetoDashboard}) => {
     return (
         <>
-            <div className="row mt-3">
+            <div className="row mt-4">
             {ObjetoDashboard && ObjetoDashboard.cards && ObjetoDashboard.cards.length > 0 && ObjetoDashboard.cards.map((card, index)=>
                 <div key={index} className="col-sm-12 col-md-4 col-xl-3 mb-4 ">
                     <div className="card h-100 container-cards-dre-dashboard">
-                        <div className="card-header">{card.titulo}</div>
+                        <div className="card-header">
+                            {card.titulo}
+                        </div>
+                        <hr className="mt-0 mb-0 ml-3 mr-3" />
                         <div className="card-body">
-                            <p className="card-text card-qtde-associacoes  mb-0 pb-2">{card.gtdeAssociacoes}</p>
+
+                            <p className="card-text card-qtde-associacoes  mb-0 pb-3">{card.gtdeAssociacoes}</p>
                             <p className="text-center">
                                 <a href="#" className="btn btn-outline-success btn-ver-prestacoes">
                                     <FontAwesomeIcon
