e90f220ddfa7441afaa15017e58da4b47994736e
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-09-22 10:08:05 -0300

    Trazendo itens do dashboard dinamicamente vindos da API

--- src/componentes/dres/Dashboard/index.js
+++ src/componentes/dres/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {getPeriodos} from "../../../services/dres/Dashboard.service";
+import {getPeriodos, getItensDashboard} from "../../../services/dres/Dashboard.service";
 import {SelectPeriodo} from "./SelectPeriodo";
 import "./dashboard.scss"
 import {BarraDeStatus} from "./BarraDeStatus";
@@ -9,30 +9,49 @@ import {DashboardCard} from "./DashboardCard";
 export const DreDashboard = () => {
 
     const [periodos, setPeriodos] = useState(false);
+    const [periodoEsolhido, setPeriodoEsolhido] = useState(false);
+    const [itensDashboard, setItensDashboard] = useState(false);
 
     useEffect(() => {
         carregaPeriodos();
     }, []);
 
+    useEffect(() => {
+        carregaItensDashboard();
+    }, [periodoEsolhido]);
+
     const carregaPeriodos = async () => {
         let periodos = await getPeriodos();
         setPeriodos(periodos);
+        if (periodos && periodos.length > 0){
+            setPeriodoEsolhido(periodos[0].uuid)
+        }
     };
 
-    const handleChangePeriodos = async (value) => {
+    const carregaItensDashboard = async () =>{
+        if (periodoEsolhido){
+            let itens = await getItensDashboard(periodoEsolhido);
+            setItensDashboard(itens)
+        }
+
+    }
+
+    const handleChangePeriodos = async (uuid_periodo) => {
+        setPeriodoEsolhido(uuid_periodo)
     };
 
     return (
         <>
             <SelectPeriodo
                 periodos={periodos}
+                periodoEsolhido={periodoEsolhido}
                 handleChangePeriodos={handleChangePeriodos}
            />
            <BarraDeStatus
-               ObjetoDashboard={ObjetoDashboard}
+               itensDashboard={itensDashboard}
            />
            <DashboardCard
-               ObjetoDashboard={ObjetoDashboard}
+               itensDashboard={itensDashboard}
            />
         </>
     )
