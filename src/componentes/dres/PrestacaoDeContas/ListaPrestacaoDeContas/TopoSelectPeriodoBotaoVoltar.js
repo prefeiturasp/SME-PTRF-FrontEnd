b2fa051e609cb99b05633deae98d5f13fe9b8160
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-09-22 14:17:53 -0300

    Refinando topo com select e botão voltar

--- src/componentes/dres/PrestacaoDeContas/ListaPrestacaoDeContas/TopoSelectPeriodoBotaoVoltar.js
+++ src/componentes/dres/PrestacaoDeContas/ListaPrestacaoDeContas/TopoSelectPeriodoBotaoVoltar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import {exibeDataPT_BR} from "../../../../utils/ValidacoesAdicionaisFormularios";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
 
 export const TopoSelectPeriodoBotaoVoltar = ({periodos, periodoEscolhido, handleChangePeriodos}) =>{
     return (
@@ -9,7 +11,7 @@ export const TopoSelectPeriodoBotaoVoltar = ({periodos, periodoEscolhido, handle
                 <div className="p-2 flex-grow-1 bd-highlight">
                     <form id="periodo_conta">
                         <div className="row pt-3">
-                            <div className="col-md-12 col-lg-7 col-xl-5 mb-md-2">
+                            <div className="col-md-12 col-lg-8 col-xl-6 mb-md-2">
                                 <div className="row">
                                     <div className="col-12 col-sm-5 col-md-3 mt-2 pr-0 mr-xl-n3 mr-lg-n2">
                                         <label htmlFor="periodo" className="">Período:</label>
@@ -33,7 +35,16 @@ export const TopoSelectPeriodoBotaoVoltar = ({periodos, periodoEscolhido, handle
                     </form>
                 </div>
                 <div className="p-2 bd-highlight pt-3">
-                    <Link to='/dre-dashboard' className="btn btn-success ml-2">Voltar para painel geral</Link>
+                    <Link
+                        to='/dre-dashboard'
+                        className="btn btn-success ml-2"
+                    >
+                        <FontAwesomeIcon
+                            style={{marginRight: "5px", color: '#fff'}}
+                            icon={faArrowLeft}
+                        />
+                        Voltar para painel geral
+                    </Link>
                 </div>
 
             </div>
