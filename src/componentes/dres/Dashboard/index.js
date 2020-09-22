d418a6e545bbe64649fcc3dce09cc967563ff519
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-09-22 10:17:35 -0300

    Incluindo Loading

--- src/componentes/dres/Dashboard/index.js
+++ src/componentes/dres/Dashboard/index.js
@@ -4,12 +4,14 @@ import {SelectPeriodo} from "./SelectPeriodo";
 import "./dashboard.scss"
 import {BarraDeStatus} from "./BarraDeStatus";
 import {DashboardCard} from "./DashboardCard";
+import Loading from "../../../utils/Loading";
 
 export const DreDashboard = () => {
 
     const [periodos, setPeriodos] = useState(false);
     const [periodoEsolhido, setPeriodoEsolhido] = useState(false);
     const [itensDashboard, setItensDashboard] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         carregaPeriodos();
@@ -19,19 +21,27 @@ export const DreDashboard = () => {
         carregaItensDashboard();
     }, [periodoEsolhido]);
 
+    useEffect(() => {
+        setLoading(false);
+    }, []);
+
     const carregaPeriodos = async () => {
+        setLoading(true);
         let periodos = await getPeriodos();
         setPeriodos(periodos);
         if (periodos && periodos.length > 0){
             setPeriodoEsolhido(periodos[0].uuid)
         }
+        setLoading(false);
     };
 
     const carregaItensDashboard = async () =>{
+        setLoading(true);
         if (periodoEsolhido){
             let itens = await getItensDashboard(periodoEsolhido);
             setItensDashboard(itens)
         }
+        setLoading(false);
     };
 
     const handleChangePeriodos = async (uuid_periodo) => {
@@ -45,12 +55,25 @@ export const DreDashboard = () => {
                 periodoEsolhido={periodoEsolhido}
                 handleChangePeriodos={handleChangePeriodos}
            />
-           <BarraDeStatus
-               itensDashboard={itensDashboard}
-           />
-           <DashboardCard
-               itensDashboard={itensDashboard}
-           />
+
+            {loading ? (
+                    <Loading
+                        corGrafico="black"
+                        corFonte="dark"
+                        marginTop="0"
+                        marginBottom="0"
+                    />
+                ) :
+
+                <>
+                    <BarraDeStatus
+                        itensDashboard={itensDashboard}
+                    />
+                    <DashboardCard
+                        itensDashboard={itensDashboard}
+                    />
+                </>
+            }
         </>
     )
 };
