4b4196a577911e146c8b8f1e309051fbee6fe8f6
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-09-22 13:09:58 -0300

    Criando e chamando getPrestacoesDeContas service

--- src/componentes/dres/PrestacaoDeContas/ListaPrestacaoDeContas/index.js
+++ src/componentes/dres/PrestacaoDeContas/ListaPrestacaoDeContas/index.js
@@ -3,16 +3,25 @@ import { useParams } from "react-router-dom";
 import {PaginasContainer} from "../../../../paginas/PaginasContainer";
 import {getPeriodos} from "../../../../services/dres/Dashboard.service";
 import {TopoSelectPeriodoBotaoVoltar} from "./TopoSelectPeriodoBotaoVoltar";
+import {getPrestacoesDeContas} from "../../../../services/dres/PrestacaoDeContas.service";
 
 export const ListaPrestacaoDeContas= () => {
+
     let {periodo_uuid, status_prestacao} = useParams();
+
     const [periodos, setPeriodos] = useState(false);
     const [periodoEscolhido, setPeriodoEsolhido] = useState(false);
+    const [prestacaoDeContas, setPrestacaoDeContas] = useState(false);
 
     useEffect(() => {
         carregaPeriodos();
     }, []);
 
+
+    useEffect(() => {
+        console.log("Mudei o periodo")
+        carregaPrestacoesDeContas();
+    }, [periodoEscolhido]);
 
     const carregaPeriodos = async () => {
         let periodos = await getPeriodos();
@@ -21,6 +30,15 @@ export const ListaPrestacaoDeContas= () => {
             setPeriodoEsolhido(periodo_uuid)
         }
     };
+
+    const carregaPrestacoesDeContas = async ()=>{
+        if (periodoEscolhido){
+            let prestacoes_de_contas = await getPrestacoesDeContas(periodoEscolhido);
+            console.log("Prestacoes de contas ", prestacoes_de_contas)
+            setPrestacaoDeContas(prestacoes_de_contas)
+        }
+
+    }
 
     const handleChangePeriodos = async (uuid_periodo) => {
         setPeriodoEsolhido(uuid_periodo)
---
+++ src/services/dres/PrestacaoDeContas.service.js
@@ -0,0 +1,14 @@
+import api from '../api'
+import { TOKEN_ALIAS } from '../auth.service.js';
+import {ASSOCIACAO_UUID} from "../auth.service";
+
+const authHeader = {
+    headers: {
+        'Authorization': `JWT ${localStorage.getItem(TOKEN_ALIAS)}`,
+        'Content-Type': 'application/json'
+    }
+};
+
+export const getPrestacoesDeContas = async (periodo_uuid=null) => {
+    return (await api.get(`/api/prestacoes-contas/?associacao__unidade__dre__uuid=${localStorage.getItem(ASSOCIACAO_UUID)}&periodo__uuid=${periodo_uuid}`, authHeader)).data
+};
