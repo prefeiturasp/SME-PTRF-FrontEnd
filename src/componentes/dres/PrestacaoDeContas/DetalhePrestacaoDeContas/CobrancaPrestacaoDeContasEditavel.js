3cdba83aff88d028b5d3bab5f401cf5ccbdb5424
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-09-29 09:13:39 -0300

    Estilizando cobrança

--- src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/CobrancaPrestacaoDeContasEditavel.js
+++ src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/CobrancaPrestacaoDeContasEditavel.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {DatePickerField} from "../../../Globais/DatePickerField";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTrashAlt} from '@fortawesome/free-solid-svg-icons'
 
 export const CobrancaPrestacaoDeContasEditavel = ({listaDeCobrancas, dataCobranca, handleChangeDataCobranca, addCobranca, deleteCobranca}) =>{
 
@@ -10,7 +12,7 @@ export const CobrancaPrestacaoDeContasEditavel = ({listaDeCobrancas, dataCobranc
             <hr className='mt-4 mb-3'/>
             <h4>Cobrança da prestação de contas</h4>
 
-            <form method='post'>
+{/*            <form method='post'>
                 <div className="col">
                     <label htmlFor="data_cobranca">Data de recebimento</label>
                     <DatePickerField
@@ -21,10 +23,36 @@ export const CobrancaPrestacaoDeContasEditavel = ({listaDeCobrancas, dataCobranc
                     />
                 </div>
                 <button type='button' onClick={addCobranca}>Add Cobranca</button>
+            </form>*/}
+
+            <form className="form-inline mt-4 mb-4">
+                <div className="form-group">
+                    <label className='mr-3' htmlFor="data_cobranca">Data de recebimento</label>
+                    <DatePickerField
+                        name="data_cobranca"
+                        id="data_cobranca"
+                        value={dataCobranca ? dataCobranca : ''}
+                        onChange={handleChangeDataCobranca}
+                    />
+                    <button disabled={!dataCobranca} onClick={addCobranca} type="button" className="btn btn-success my-1 ml-3">Adicionar Cobranca</button>
+                </div>
             </form>
 
+
+
             {listaDeCobrancas && listaDeCobrancas.length > 0 && listaDeCobrancas.map((cobrancao, index)=>
-                <p key={index}><button onClick={()=>deleteCobranca(cobrancao.uuid)}>Cobrança Data {cobrancao.data} Excluir</button></p>
+                <p key={index}>
+                    <button
+                        className="btn-excluir-valores-reprogramados"
+                        onClick={()=>deleteCobranca(cobrancao.uuid)}
+                        >
+                        Cobrança Data {cobrancao.data}
+                        <FontAwesomeIcon
+                            style={{fontSize: '20px', marginLeft: "1rem"}}
+                            icon={faTrashAlt}
+                        />
+                    </button>
+                </p>
             )}
 
         </>
