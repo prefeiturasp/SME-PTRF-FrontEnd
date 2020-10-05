239e94a27cfa3411d0fccdaa4ec47187f365b2a2
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-10-05 08:43:03 -0300

    Implementando Saldo Final e ResumoFinanceito TabelaTotais

--- src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/ResumoFinanceiroTabelaTotais.js
+++ src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/ResumoFinanceiroTabelaTotais.js
@@ -21,21 +21,21 @@ export const ResumoFinanceiroTabelaTotais = ({infoAta, valorTemplate}) => {
                     </thead>
                     <tbody>
                     <tr>
-                        <td>Saldo anterior</td>
+                        <td>Saldo inicial (reprogramado do período anterios)</td>
                         <td>{valorTemplate(infoAta.totais.saldo_reprogramado_custeio)}</td>
                         <td>{valorTemplate(infoAta.totais.saldo_reprogramado_capital)}</td>
                         <td>{valorTemplate(infoAta.totais.saldo_reprogramado_livre)}</td>
                         <td>{valorTemplate(infoAta.totais.saldo_reprogramado)}</td>
                     </tr>
                     <tr>
-                        <td>Recebimento</td>
+                        <td>Repasses</td>
                         <td>{valorTemplate(infoAta.totais.repasses_no_periodo_custeio)}</td>
                         <td>{valorTemplate(infoAta.totais.repasses_no_periodo_capital)}</td>
                         <td>{valorTemplate(infoAta.totais.repasses_no_periodo_livre)}</td>
                         <td>{valorTemplate(infoAta.totais.repasses_no_periodo)}</td>
                     </tr>
                     <tr>
-                        <td>Demais créditos (rendimento e outros)</td>
+                        <td>Demais créditos</td>
                         <td>{valorTemplate(infoAta.totais.outras_receitas_no_periodo_custeio)}</td>
                         <td>{valorTemplate(infoAta.totais.outras_receitas_no_periodo_capital)}</td>
                         <td>{valorTemplate(infoAta.totais.outras_receitas_no_periodo_livre)}</td>
@@ -49,39 +49,25 @@ export const ResumoFinanceiroTabelaTotais = ({infoAta, valorTemplate}) => {
                         <td>{valorTemplate(infoAta.totais.despesas_no_periodo)}</td>
                     </tr>
                     <tr>
-                        <td>Saldo atual</td>
-                        <td>{valorTemplate(infoAta.totais.saldo_atual_custeio)}</td>
-                        <td>{valorTemplate(infoAta.totais.saldo_atual_capital)}</td>
+                        <td>Saldo final - Saldo atual</td>
+                        <td>{valorTemplate(infoAta.totais.saldo_atual_custeio + infoAta.totais.despesas_nao_conciliadas_custeio)}</td>
+                        <td>{valorTemplate(infoAta.totais.saldo_atual_capital + infoAta.totais.despesas_nao_conciliadas_capital)}</td>
                         <td>{valorTemplate(infoAta.totais.saldo_atual_livre)}</td>
-                        <td>{valorTemplate(infoAta.totais.saldo_atual_total)}</td>
+                        <td>{valorTemplate(infoAta.totais.saldo_atual_total + infoAta.totais.despesas_nao_conciliadas)}</td>
                     </tr>
                     <tr>
-                        <td>Pagamentos a compensar</td>
+                        <td>Despesas não demonstradas</td>
                         <td>{valorTemplate(infoAta.totais.despesas_nao_conciliadas_custeio)}</td>
                         <td>{valorTemplate(infoAta.totais.despesas_nao_conciliadas_capital)}</td>
                         <td className="td-livre-aplicacao-ausente">-</td>
                         <td>{valorTemplate(infoAta.totais.despesas_nao_conciliadas)}</td>
                     </tr>
                     <tr>
-                        <td>Crédito não demonstrado</td>
-                        <td>{valorTemplate(infoAta.totais.receitas_nao_conciliadas_custeio)}</td>
-                        <td>{valorTemplate(infoAta.totais.receitas_nao_conciliadas_capital)}</td>
-                        <td>{valorTemplate(infoAta.totais.receitas_nao_conciliadas_livre)}</td>
-                        <td>{valorTemplate(infoAta.totais.receitas_nao_conciliadas)}</td>
-                    </tr>
-                    <tr>
-                        <td>Crédito futuros</td>
-                        <td>{valorTemplate(infoAta.totais.repasses_nao_realizados_custeio)}</td>
-                        <td>{valorTemplate(infoAta.totais.repasses_nao_realizados_capital)}</td>
-                        <td>{valorTemplate(infoAta.totais.repasses_nao_realizados_livre)}</td>
-                        <td>{valorTemplate(infoAta.totais.repasses_nao_realizados_custeio + infoAta.totais.repasses_nao_realizados_capital + infoAta.totais.repasses_nao_realizados_livre)}</td>
-                    </tr>
-                    <tr>
-                        <td>Devolução a conta do PTRF</td>
-                        <td>{valorTemplate(infoAta.totais.receitas_devolucao_no_periodo_custeio)}</td>
-                        <td>{valorTemplate(infoAta.totais.receitas_devolucao_no_periodo_capital)}</td>
-                        <td>{valorTemplate(infoAta.totais.receitas_devolucao_no_periodo_livre)}</td>
-                        <td>{valorTemplate(infoAta.totais.receitas_devolucao_no_periodo_custeio + infoAta.totais.receitas_devolucao_no_periodo_capital + infoAta.totais.receitas_devolucao_no_periodo_livre)}</td>
+                        <td>Saldo reprogramado (para o próximo período)</td>
+                        <td>{valorTemplate(infoAta.totais.saldo_atual_custeio)}</td>
+                        <td>{valorTemplate(infoAta.totais.saldo_atual_capital)}</td>
+                        <td>{valorTemplate(infoAta.totais.saldo_atual_livre)}</td>
+                        <td>{valorTemplate(infoAta.totais.saldo_atual_total)}</td>
                     </tr>
                     </tbody>
                 </table>
