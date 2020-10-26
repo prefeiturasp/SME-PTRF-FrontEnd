797055dfb942a4a31d113b6d0fb0ac51859e312b
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-10-26 12:36:57 -0300

    Criando e estilizando AccordionInfo

--- src/componentes/Globais/GestaoDePerfis/index.js
+++ src/componentes/Globais/GestaoDePerfis/index.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
+import "./gestao-de-perfis.scss"
+import {AccordionInfo} from "./AccordionInfo";
 
 export const GestaoDePerfis = () => {
-    return(
+    const [clickBtnInfo, setClickBtnInfo] = useState(false);
+
+    return (
         <>
             <p>Faça a gestão dos seus usuários e determine seus perfis atrelando-os aos grupos de acesso.</p>
+            <AccordionInfo
+                clickBtnInfo={clickBtnInfo}
+                setClickBtnInfo={setClickBtnInfo}
+            />
+
         </>
     );
 };
