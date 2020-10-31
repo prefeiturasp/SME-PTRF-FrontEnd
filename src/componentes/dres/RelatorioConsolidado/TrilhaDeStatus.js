12f0b0a02fa0b524f88bff121e8bcf4010883f25
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-10-31 10:49:22 -0300

    Criando SelectPeriodo e TrilhaDeStatus

---
+++ src/componentes/dres/RelatorioConsolidado/TrilhaDeStatus.js
@@ -0,0 +1,53 @@
+import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheck} from "@fortawesome/free-solid-svg-icons";
+
+export const TrilhaDeStatus = () => {
+    return (
+        <>
+            <div className='row mt-3 mb-3'>
+                <div className='col-12'>
+                    <h4>Prestações de contas das Associações</h4>
+                </div>
+            </div>
+            <div className='row'>
+                <div className="col-12">
+                    <div id="timeline">&nbsp;</div>
+                    <div className="d-flex justify-content-between mb-3">
+                        <div className='container-circulo'>
+                            <span className='circulo circulo-ativo-passado'>
+                                <FontAwesomeIcon
+                                    style={{color: '#fff'}}
+                                    icon={faCheck}
+                                />
+                            </span>
+                            <p className='mt-2'><strong>Não recebido</strong></p>
+                        </div>
+                        <div className='container-circulo'>
+                            <span className='circulo circulo-ativo-passado'>
+                                <FontAwesomeIcon
+                                    style={{color: '#fff'}}
+                                    icon={faCheck}
+                                />
+                            </span>
+                            <p className='mt-2'><strong>Recebida e <br/> aguardando análise</strong></p>
+                        </div>
+                        <div className='container-circulo'>
+                            <span className='circulo circulo-ativo-passado'>
+                                <FontAwesomeIcon
+                                    style={{color: '#fff'}}
+                                    icon={faCheck}
+                                />
+                            </span>
+                            <p className='mt-2'><strong>Em análise</strong></p>
+                        </div>
+                        <div className='container-circulo'>
+                            <span className='circulo circulo-ativo'>4</span>
+                            <p className='mt-2'><strong>Conclusão <br/> da análise</strong></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+};
--- src/componentes/dres/RelatorioConsolidado/index.js
+++ src/componentes/dres/RelatorioConsolidado/index.js
@@ -1,18 +1,55 @@
 import React, {useEffect, useState} from "react";
 import {getFiqueDeOlho} from "../../../services/dres/RelatorioConsolidado.service";
+import {getItensDashboard, getPeriodos} from "../../../services/dres/Dashboard.service";
+import {SelectPeriodo} from "./SelectPeriodo";
+import {MsgImgCentralizada} from "../../Globais/Mensagens/MsgImgCentralizada";
+import Img404 from "../../../assets/img/img-404.svg";
+import {TrilhaDeStatus} from "./TrilhaDeStatus";
 
 export const RelatorioConsolidado = () => {
     const [fiqueDeOlho, setFiqueDeOlho] = useState("");
+    const [periodos, setPeriodos] = useState(false);
+    const [periodoEscolhido, setPeriodoEsolhido] = useState(false);
+    const [itensDashboard, setItensDashboard] = useState(false);
 
     useEffect(() => {
         buscaFiqueDeOlho();
     }, []);
 
+    useEffect(() => {
+        carregaPeriodos();
+    }, []);
+
+    useEffect(() => {
+        carregaItensDashboard();
+    }, [periodoEscolhido]);
+
+    const carregaPeriodos = async () => {
+        let periodos = await getPeriodos();
+        setPeriodos(periodos);
+        if (periodos && periodos.length > 0){
+            setPeriodoEsolhido(periodos[0].uuid)
+        }
+    };
+
+    const carregaItensDashboard = async () =>{
+        if (periodoEscolhido){
+            let itens = await getItensDashboard(periodoEscolhido);
+            setItensDashboard(itens)
+        }
+    };
+
     const buscaFiqueDeOlho = async () => {
         let fique_de_olho = await getFiqueDeOlho();
-        console.log('Fuca aslnlas ', fique_de_olho)
         setFiqueDeOlho(fique_de_olho.detail);
     };
+
+    const handleChangePeriodos = async (uuid_periodo) => {
+        setPeriodoEsolhido(uuid_periodo)
+    };
+
+    console.log('PERIODO Escolhido ', periodoEscolhido)
+    console.log('itensDashboard ', itensDashboard)
 
     return (
         <>
@@ -21,6 +58,21 @@ export const RelatorioConsolidado = () => {
             </div>
 
             <div className="page-content-inner">
+                <SelectPeriodo
+                    periodos={periodos}
+                    periodoEscolhido={periodoEscolhido}
+                    handleChangePeriodos={handleChangePeriodos}
+                />
+
+                {periodoEscolhido && itensDashboard ? (
+                    <TrilhaDeStatus/>
+                ):
+                    <MsgImgCentralizada
+                        texto='Selecione um período acima para visualizar as ações'
+                        img={Img404}
+                    />
+                }
+
             </div>
         </>
     )
