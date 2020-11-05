43ee654511418d3e8857794f513cf0b5b503ef00
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-11-05 13:38:59 -0300

    Verificando permissões para botão Exportar

--- src/componentes/escolas/Associacao/ExportaDadosAssociacao/index.js
+++ src/componentes/escolas/Associacao/ExportaDadosAssociacao/index.js
@@ -3,6 +3,9 @@ import "../associacao.scss"
 import {exportarDadosAssociacao} from "../../../../services/escolas/Associacao.service";
 import Loading from "../../../../utils/Loading";
 import IconeExportarDados from "../../../../assets/img/icone-exportar-dados.svg";
+import {visoesService} from "../../../../services/visoes.service";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowRight, faDownload} from "@fortawesome/free-solid-svg-icons";
 
 export const ExportaDadosDaAsssociacao = () => {
     const [loading, setLoading] = useState(false);
@@ -24,13 +27,17 @@ export const ExportaDadosDaAsssociacao = () => {
                     />
                 ) :
                 <div className="d-flex  justify-content-end pb-3 mt-3">
-                    <a onClick={exportarDados} className="link-exportar">
-                        <img
-                            src={IconeExportarDados}
-                            alt=""
-                            className="img-fluid"
-                        /> <strong>Exportar</strong>
-                    </a>
+                    <button
+                        disabled={!visoesService.getPermissoes(['change_associacao'])}
+                        onClick={exportarDados}
+                        className={`link-exportar ${!visoesService.getPermissoes(['change_associacao']) ? 'link-exportar-disabled' : ''}`}
+                    >
+                        <FontAwesomeIcon
+                            style={{color: `${!visoesService.getPermissoes(['change_associacao']) ? '#7D7D7D' : '#00585E'}`, marginRight:'3px'}}
+                            icon={faDownload}
+                        />
+                        <strong>Exportar</strong>
+                    </button>
                 </div>
 
 
