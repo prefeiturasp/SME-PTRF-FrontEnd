9df07b032f5df19339d056e4456b96e498bc4cc6
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-11-13 13:13:22 -0300

    Estilizando de acordo com protótipo

--- src/componentes/dres/Associacoes/DadosDasAssociacoes/TopoComBotoes.js
+++ src/componentes/dres/Associacoes/DadosDasAssociacoes/TopoComBotoes.js
@@ -8,31 +8,16 @@ export const TopoComBotoes = ({dadosDaAssociacao}) =>{
             <div className="p-2 flex-grow-1 bd-highlight">
                 <h1 className="titulo-itens-painel mt-5">{dadosDaAssociacao.dados_da_associacao.nome}</h1>
             </div>
-            {/*<div className="p-2 bd-highlight mt-5">
-                <Link to="/dre-situacao-financeira-unidade-educacional" type="button" className="btn btn-outline-success">Ver situação financeira</Link>
-            </div>
-            <div className="p-2 bd-highlight mt-5">
-                <Link to="/dre-regularidade-unidade-educacional" className="btn btn btn-outline-success">Ver regularidade</Link>
-            </div>
-            */}
             <div className="p-2 bd-highlight mt-5">
                 <Link to="/dre-associacoes" className="btn btn btn-success">Voltar</Link>
             </div>
         </div>
 
-        <div className="d-flex justify-content-between bd-highlight mb-3">
-            <div className="p-2 bd-highlight">
-                <Link to="/dre-dados-da-unidade-educacional" className="btn btn-outline-success">Dados da unidade</Link>
-            </div>
-            <div className="p-2 bd-highlight">
-                <Link to="/dre-regularidade-unidade-educacional" className="btn btn btn-outline-success">Regularidade</Link>
-            </div>
-            <div className="p-2 bd-highlight">
-                <Link to="/dre-situacao-financeira-unidade-educacional" type="button" className="btn btn-outline-success">Situação financeira</Link>
-            </div>
-            <div className="p-2 bd-highlight">
-                <Link to="/dre-situacao-financeira-unidade-educacional" type="button" className="btn btn-outline-success">Situação patrimonial</Link>
-            </div>
+        <div className="d-flex justify-content-between p-3 mb-4 mt-2 bg-white container-menu-dados-das-associacoes">
+            <Link to="/dre-dados-da-unidade-educacional" className="">Dados da unidade</Link>
+            <Link to="/dre-regularidade-unidade-educacional" className="">Regularidade</Link>
+            <Link to="/dre-situacao-financeira-unidade-educacional" type="button" className="">Situação financeira</Link>
+            <Link to="/dre-situacao-financeira-unidade-educacional" type="button" className="">Situação patrimonial</Link>
         </div>
 
         </>
