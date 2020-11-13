83018394b845389edb8d782fda2b8b04d5c49345
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-11-13 12:57:24 -0300

    Criando links e utilizando unico componente TopoComBotoes

--- src/componentes/dres/Associacoes/DadosDasAssociacoes/TopoComBotoes.js
+++ src/componentes/dres/Associacoes/DadosDasAssociacoes/TopoComBotoes.js
@@ -19,6 +19,22 @@ export const TopoComBotoes = ({dadosDaAssociacao}) =>{
                 <Link to="/dre-associacoes" className="btn btn btn-success">Voltar</Link>
             </div>
         </div>
+
+        <div className="d-flex justify-content-between bd-highlight mb-3">
+            <div className="p-2 bd-highlight">
+                <Link to="/dre-dados-da-unidade-educacional" className="btn btn-outline-success">Dados da unidade</Link>
+            </div>
+            <div className="p-2 bd-highlight">
+                <Link to="/dre-regularidade-unidade-educacional" className="btn btn btn-outline-success">Regularidade</Link>
+            </div>
+            <div className="p-2 bd-highlight">
+                <Link to="/dre-situacao-financeira-unidade-educacional" type="button" className="btn btn-outline-success">Situação financeira</Link>
+            </div>
+            <div className="p-2 bd-highlight">
+                <Link to="/dre-situacao-financeira-unidade-educacional" type="button" className="btn btn-outline-success">Situação patrimonial</Link>
+            </div>
+        </div>
+
         </>
     );
 };
