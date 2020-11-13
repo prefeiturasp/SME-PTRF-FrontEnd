5f5a9a0cb4e167a04753e62ee352d6d34a4f677a
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-11-13 12:44:11 -0300

    Removendo Botões Ver situação financeira e Ver regularidade

--- src/componentes/dres/Associacoes/DadosDasAssociacoes/TopoComBotoes.js
+++ src/componentes/dres/Associacoes/DadosDasAssociacoes/TopoComBotoes.js
@@ -3,19 +3,22 @@ import {Link} from "react-router-dom";
 
 export const TopoComBotoes = ({dadosDaAssociacao}) =>{
     return(
+        <>
         <div className="d-flex bd-highlight">
             <div className="p-2 flex-grow-1 bd-highlight">
                 <h1 className="titulo-itens-painel mt-5">{dadosDaAssociacao.dados_da_associacao.nome}</h1>
             </div>
-            <div className="p-2 bd-highlight mt-5">
+            {/*<div className="p-2 bd-highlight mt-5">
                 <Link to="/dre-situacao-financeira-unidade-educacional" type="button" className="btn btn-outline-success">Ver situação financeira</Link>
             </div>
             <div className="p-2 bd-highlight mt-5">
                 <Link to="/dre-regularidade-unidade-educacional" className="btn btn btn-outline-success">Ver regularidade</Link>
             </div>
+            */}
             <div className="p-2 bd-highlight mt-5">
                 <Link to="/dre-associacoes" className="btn btn btn-success">Voltar</Link>
             </div>
         </div>
+        </>
     );
 };
