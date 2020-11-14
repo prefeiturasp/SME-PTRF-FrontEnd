768dcb03996facc298602d86ad0500cb74cba9e0
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-11-13 22:00:14 -0300

    Voltando ao estado inicial

--- src/componentes/dres/Associacoes/DadosDasAssociacoes/SituacaoFinanceiraUnidadeEducacional/TopoComBotoes.js
+++ src/componentes/dres/Associacoes/DadosDasAssociacoes/SituacaoFinanceiraUnidadeEducacional/TopoComBotoes.js
@@ -8,11 +8,12 @@ export const TopoComBotoes = ({dadosDaAssociacao}) =>{
                 <h1 className="titulo-itens-painel mt-5">{dadosDaAssociacao.dados_da_associacao.nome}</h1>
             </div>
             <div className="p-2 bd-highlight mt-5">
-                <Link to="/dre-situacao-financeira-unidade-educacional" type="button" className="btn btn-outline-success">Ver situação financeira</Link>
-            </div>
-            <div className="p-2 bd-highlight mt-5">
                 <Link to="/dre-regularidade-unidade-educacional" className="btn btn btn-outline-success">Ver regularidade</Link>
             </div>
+            <div className="p-2 bd-highlight mt-5">
+                <Link to="/dre-dados-da-unidade-educacional" className="btn btn-outline-success">Ver dados unidade</Link>
+            </div>
+
             <div className="p-2 bd-highlight mt-5">
                 <Link to="/dre-associacoes" className="btn btn btn-success">Voltar</Link>
             </div>
