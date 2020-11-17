750ba0b6ef6e67ccd6f1767e9e4e8f70fc4bb06b
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-11-17 09:36:55 -0300

    Substituindo status de DOCS_PENDENTES para NAO_APRESENTADA em Prestacao De Contas

--- src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/FormRecebimentoPelaDiretoria.js
+++ src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/FormRecebimentoPelaDiretoria.js
@@ -39,7 +39,7 @@ export const FormRecebimentoPelaDiretoria = ({stateFormRecebimentoPelaDiretoria,
                             className="form-control"
                             disabled={disabledStatus}
                         >
-                            {tabelaPrestacoes.status && tabelaPrestacoes.status.length > 0 && tabelaPrestacoes.status.filter(element => element.id !== 'DOCS_PENDENTES').map(item => (
+                            {tabelaPrestacoes.status && tabelaPrestacoes.status.length > 0 && tabelaPrestacoes.status.filter(element => element.id !== 'NAO_APRESENTADA').map(item => (
                                 <option key={item.id} value={item.id}>{item.nome}</option>
                             ))}
                         </select>
