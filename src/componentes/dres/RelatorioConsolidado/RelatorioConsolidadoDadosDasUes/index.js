6aa075cfa322e00adfbd6436c16de538297abd4d
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-11-17 10:57:41 -0300

    Criando TopoComBoteoes e passando props

--- src/componentes/dres/RelatorioConsolidado/RelatorioConsolidadoDadosDasUes/index.js
+++ src/componentes/dres/RelatorioConsolidado/RelatorioConsolidadoDadosDasUes/index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import {visoesService} from "../../../../services/visoes.service";
+import {auxGetNomes} from "../auxGetNomes";
+import {TopoComBotoes} from "./TopoComBotoes";
 
 export const RelatorioConsolidadoDadosDasUes = () => {
 
@@ -8,11 +10,37 @@ export const RelatorioConsolidadoDadosDasUes = () => {
 
     const dre_uuid = visoesService.getItemUsuarioLogado('associacao_selecionada.uuid');
 
+    const [periodoNome, setPeriodoNome] = useState('');
+    const [contaNome, setContaNome] = useState('');
+
+    useEffect(()=>{
+        carregaNomePeriodo();
+        carregaNomeConta();
+    });
+
+    const carregaNomePeriodo = async () => {
+        if (periodo_uuid){
+            let periodo_nome = await auxGetNomes.nomePeriodo(periodo_uuid);
+            setPeriodoNome(periodo_nome);
+        }
+    };
+
+    const carregaNomeConta = async () => {
+        let conta_nome = await auxGetNomes.nomeConta(conta_uuid);
+        setContaNome(conta_nome);
+    };
+
+
     return (
         <>
             <div className="col-12 container-visualizacao-da-ata mb-5">
                 <div className="col-12 mt-5">
-                    <h1>RelatorioConsolidadoDadosDasUes</h1>
+                    <TopoComBotoes
+                        periodoNome={periodoNome}
+                        contaNome={contaNome}
+                        periodo_uuid={periodo_uuid}
+                        conta_uuid={conta_uuid}
+                    />
                 </div>
             </div>
         </>
