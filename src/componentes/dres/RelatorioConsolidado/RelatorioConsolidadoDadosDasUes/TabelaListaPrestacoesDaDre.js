1482bf083afaf7b791c2fb300e2c3a981c28047c
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-11-18 09:50:28 -0300

    Populando e estilizando a tabela com valores reais

--- src/componentes/dres/RelatorioConsolidado/RelatorioConsolidadoDadosDasUes/TabelaListaPrestacoesDaDre.js
+++ src/componentes/dres/RelatorioConsolidado/RelatorioConsolidadoDadosDasUes/TabelaListaPrestacoesDaDre.js
@@ -26,9 +26,9 @@ import { Column } from 'primereact/column';
     const reprogramadoTemplate = (rowData) =>{
          return (
              <React.Fragment>
-                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_custeio)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_capital)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_livre)}</span></p>
+                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.saldo_reprogramado_periodo_anterior_capital)}</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.saldo_reprogramado_periodo_anterior_custeio)}</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.saldo_reprogramado_periodo_anterior_livre)}</span></p>
              </React.Fragment>
          )
      };
@@ -36,9 +36,9 @@ import { Column } from 'primereact/column';
     const repasseTemplate = (rowData) =>{
          return (
              <React.Fragment>
-                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_custeio)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_capital)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_livre)}</span></p>
+                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.repasses_no_periodo_capital)}</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.repasses_no_periodo_custeio)}</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.repasses_no_periodo_livre)}</span></p>
              </React.Fragment>
          )
      };
@@ -46,9 +46,9 @@ import { Column } from 'primereact/column';
      const rendimentoTemplate = (rowData) =>{
          return (
              <React.Fragment>
-                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_custeio)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_capital)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_livre)}</span></p>
+                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.receitas_rendimento_no_periodo_capital)}</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.receitas_rendimento_no_periodo_custeio)}</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.receitas_rendimento_no_periodo_livre)}</span></p>
              </React.Fragment>
          )
      };
@@ -56,9 +56,9 @@ import { Column } from 'primereact/column';
      const devolucaoPtrfTemplate = (rowData) =>{
          return (
              <React.Fragment>
-                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_custeio)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_capital)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_livre)}</span></p>
+                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.receitas_devolucao_no_periodo_capital)}</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.receitas_devolucao_no_periodo_custeio)}</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.receitas_devolucao_no_periodo_livre)}</span></p>
              </React.Fragment>
          )
      };
@@ -66,8 +66,8 @@ import { Column } from 'primereact/column';
      const demaisCreditosTemplate = (rowData) =>{
          return (
              <React.Fragment>
-                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_custeio)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_capital)}</span></p>
+                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_capital)}</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_custeio)}</span></p>
                  <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_livre)}</span></p>
              </React.Fragment>
          )
@@ -76,37 +76,78 @@ import { Column } from 'primereact/column';
      const despesaRealizadaTemplate = (rowData) =>{
          return (
              <React.Fragment>
-                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_custeio)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_capital)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_livre)}</span></p>
+                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.despesas_no_periodo_capital)}</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.despesas_no_periodo_custeio)}</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">-</span></p>
              </React.Fragment>
          )
      };
      const saldoTemplate = (rowData) =>{
          return (
              <React.Fragment>
-                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_custeio)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_capital)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_livre)}</span></p>
+                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.saldo_reprogramado_proximo_periodo_capital)}</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.saldo_reprogramado_proximo_periodo_custeio)}</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.saldo_reprogramado_proximo_periodo_livre)}</span></p>
              </React.Fragment>
          )
      };
      const devolucaoTesouroTemplate = (rowData) =>{
          return (
              <React.Fragment>
-                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_custeio)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_capital)}</span></p>
-                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.demais_creditos_no_periodo_livre)}</span></p>
+                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">-</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">-</span></p>
+                 <p className='mb-0 border-top'><span className="p-2 py-2 px-2 conteudo-celulas-lista-dres">{valorTemplate(rowData.valores.devolucoes_ao_tesouro_no_periodo_total)}</span></p>
              </React.Fragment>
          )
      };
 
      const statusTemplate = (rowData) =>{
          return (
-             <React.Fragment>
-                 <p className='mb-0 p-0'><span className="py-2 px-2 conteudo-celulas-lista-dres">{rowData.status_prestacao_contas}</span></p>
-             </React.Fragment>
+             <div>
+                 {rowData['status_prestacao_contas'] ? <p className='mb-0 p-0'><span className={`py-2 px-2 conteudo-celulas-lista-dres span-status-${rowData['status_prestacao_contas']}`}><strong>{exibeLabelStatus(rowData['status_prestacao_contas']).texto_col_tabela}</strong></span></p> : ''}
+             </div>
          )
+     };
+
+     const exibeLabelStatus = (status_converter) => {
+
+         if (status_converter === 'NAO_RECEBIDA') {
+             return {
+                 texto_col_tabela: 'Não recebida',
+             }
+         } else if (status_converter === 'RECEBIDA') {
+             return {
+                 texto_col_tabela: 'Recebida',
+             }
+         } else if (status_converter === 'DEVOLVIDA') {
+             return {
+                 texto_col_tabela: 'Devolvida para acerto',
+             }
+         } else if (status_converter === 'EM_ANALISE') {
+             return {
+                 texto_col_tabela: 'Em análise',
+             }
+         } else if (status_converter === 'APROVADA') {
+             return {
+                 texto_col_tabela: 'Aprovada',
+             }
+         } else if (status_converter === 'APROVADA_RESSALVA') {
+             return {
+                 texto_col_tabela: 'Aprovada com ressalva',
+             }
+         } else if (status_converter === 'REPROVADA') {
+             return {
+                 texto_col_tabela: 'Reprovada',
+             }
+         } else if (status_converter === 'NAO_APRESENTADA') {
+             return {
+                 texto_col_tabela: 'Não apresentada',
+             }
+         } else {
+             return {
+                 texto_col_tabela: 'SEM STATUS',
+             }
+         }
      };
 
   return(
