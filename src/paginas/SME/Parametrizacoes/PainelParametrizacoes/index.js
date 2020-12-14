f75c1b4b9699a02d5347f9cb421de998e1b1bd75
Alessandro Fernandes <alcfernandes@yahoo.com>
2020-12-14 09:03:14 -0300

    feat(29770): Painel de Parâmetros
    Painel de seleção de parâmetros do sistema.

--- src/paginas/SME/Parametrizacoes/PainelParametrizacoes/index.js
+++ src/paginas/SME/Parametrizacoes/PainelParametrizacoes/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {PaginasContainer} from '../../../PaginasContainer'
+import {PainelParametrizacoes} from '../../../../componentes/sme/PainelParametrizacoes'
 
 export const PainelParametrizacoesPage = () => {
     return (
         <PaginasContainer>
             <h1 className="titulo-itens-painel mt-5">Painel de Parametrizações</h1>
             <div className="page-content-inner">
-                <h1>Teste</h1>
-                {/*<DreDashboard/>*/}
+                <PainelParametrizacoes/>
             </div>
         </PaginasContainer>
     )
