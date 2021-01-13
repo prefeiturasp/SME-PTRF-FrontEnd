c8654819840f5b5fdf7ced6ac3c7d962c3c18d7a
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2021-01-13 13:47:50 -0300

    Criando e dando comportamento ao componente Filtros

---
+++ src/componentes/sme/Parametrizacoees/Estrutura/AcoesDasAssociacoes/Filtros.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+export const Filtros = ({stateFiltros, handleChangeFiltros, despesasTabelas}) =>{
+
+    return(
+        <>
+            <form>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label htmlFor="filtrar_por_nome_cod_eol">Filtrar por nome ou código EOL</label>
+                        <input
+                            value={stateFiltros.filtrar_por_nome_cod_eol}
+                            onChange={(e) => handleChangeFiltros(e.target.name, e.target.value)}
+                            name='filtrar_por_nome_cod_eol'
+                            id="filtrar_por_nome_cod_eol"
+                            type="text"
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="form-group col-md-3">
+                        <label htmlFor="filtrar_por_acao">Filtrar por ação</label>
+                        <select
+                            value={stateFiltros.filtrar_por_acao}
+                            onChange={(e) => handleChangeFiltros(e.target.name, e.target.value)}
+                            name='filtrar_por_acao'
+                            id="filtrar_por_acao"
+                            className="form-control"
+                        >
+                            <option>Selecione a ação</option>
+                            {despesasTabelas.acoes_associacao && despesasTabelas.acoes_associacao.map(item => (
+                                <option key={item.uuid} value={item.uuid}>{item.nome}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="form-group col-md-3">
+                        <label htmlFor="filtrar_por_status">Filtrar por status</label>
+                        <select
+                            value={stateFiltros.filtrar_por_status}
+                            onChange={(e) => handleChangeFiltros(e.target.name, e.target.value)}
+                            name='filtrar_por_status'
+                            id="filtrar_por_status"
+                            className="form-control"
+                        >
+                            <option>Selecione o status</option>
+                            <option value='ATIVA'>Ativa</option>
+                            <option value='INATIVA'>Inativa</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className="d-flex  justify-content-end pb-3 mt-3">
+                    <button type="button" className="btn btn btn-outline-success mt-2 mr-2">Cancelar</button>
+                    <button type="bu" className="btn btn-success mt-2">Filtrar</button>
+                </div>
+            </form>
+        </>
+    );
+};
--- src/componentes/sme/Parametrizacoees/Estrutura/AcoesDasAssociacoes/index.js
+++ src/componentes/sme/Parametrizacoees/Estrutura/AcoesDasAssociacoes/index.js
@@ -4,11 +4,21 @@ import {getTodasAcoesDasAssociacoes} from "../../../../../services/sme/Parametri
 import '../parametrizacoes-estrutura.scss'
 import {MenuInterno} from "../../../../Globais/MenuInterno";
 import {UrlsMenuInterno} from "../UrlsMenuInterno";
+import {Filtros} from "./Filtros";
+import {getDespesasTabelas} from "../../../../../services/escolas/Despesas.service";
 
 export const AcoesDasAssociacoes = () => {
 
+    const initialStateFiltros = {
+        filtrar_por_nome_cod_eol: "",
+        filtrar_por_acao: "",
+        filtrar_por_status: "",
+    };
+
     const [todasAsAcoes, setTodasAsAcoes] = useState([]);
     const [count, setCount] = useState(0);
+    const [stateFiltros, setStateFiltros] = useState(initialStateFiltros);
+    const [despesasTabelas, setDespesasTabelas] = useState([]);
 
     const carregaTodasAsAcoes = useCallback(async () =>{
         let todas_acoes = await getTodasAcoesDasAssociacoes();
@@ -20,8 +30,24 @@ export const AcoesDasAssociacoes = () => {
         carregaTodasAsAcoes();
     }, [carregaTodasAsAcoes]);
 
+    const carregaTabelasDespesas = useCallback(async () =>{
+        const resp = await getDespesasTabelas();
+        setDespesasTabelas(resp);
+    }, []);
+
+    useEffect(()=>{
+        carregaTabelasDespesas();
+    }, [carregaTabelasDespesas]);
+
     // Quando a state de todasAsAcoes sofrer alteração
     const totalDeAcoes =  useMemo(() => todasAsAcoes.length, [todasAsAcoes]);
+
+    const handleChangeFiltros = (name, value) => {
+        setStateFiltros({
+            ...stateFiltros,
+            [name]: value
+        });
+    };
 
     return (
         <PaginasContainer>
@@ -29,6 +55,11 @@ export const AcoesDasAssociacoes = () => {
             <div className="page-content-inner">
                 <MenuInterno
                     caminhos_menu_interno={UrlsMenuInterno}
+                />
+                <Filtros
+                    stateFiltros={stateFiltros}
+                    handleChangeFiltros={handleChangeFiltros}
+                    despesasTabelas={despesasTabelas}
                 />
                 <p>Exibindo <span className='total-acoes'>{totalDeAcoes}</span> ações de associações</p>
                 {/*<button onClick={()=>setCount(prevState => prevState+1)}>Botão Sem Use Calback</button>*/}
