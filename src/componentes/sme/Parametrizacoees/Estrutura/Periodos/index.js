3c56c93928db3fae8eec4dd8d0fad2d0bb2d02a4
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2021-01-18 14:18:10 -0300

    Criando rota e componente parametrizacoes/estrutura/periodos

---
+++ src/componentes/sme/Parametrizacoees/Estrutura/Periodos/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {PaginasContainer} from "../../../../../paginas/PaginasContainer";
+
+export const Periodos = () =>{
+
+    return(
+        <PaginasContainer>
+            <h1 className="titulo-itens-painel mt-5">Períodos</h1>
+            <div className="page-content-inner">
+            </div>
+        </PaginasContainer>
+    );
+
+};
