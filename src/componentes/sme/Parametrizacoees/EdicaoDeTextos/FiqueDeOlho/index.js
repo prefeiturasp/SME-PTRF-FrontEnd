b0b0b044198813254773c4d165634abc9106d890
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2021-02-06 10:58:02 -0300

    Criando Rota, area Edicao de Textos e componente Fique de Olho

---
+++ src/componentes/sme/Parametrizacoees/EdicaoDeTextos/FiqueDeOlho/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {PaginasContainer} from "../../../../../paginas/PaginasContainer";
+
+export const FiqueDeOlho = ()=>{
+
+    return(
+        <PaginasContainer>
+            <h1 className="titulo-itens-painel mt-5">Textos do Fique de Olho </h1>
+            <div className="page-content-inner">
+            </div>
+        </PaginasContainer>
+    );
+
+};
