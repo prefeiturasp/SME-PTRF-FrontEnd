f7e50672fb3227997b36c902718f662a462c5d17
ollyvergithub <ollyverottoboni@gmail.com>
2021-05-27 12:07:39 -0300

    Incluindo menu interno em gestão de perfis para levar a carga de arquivos quando a visao for SME

---
+++ src/componentes/Globais/GestaoDePerfis/UrlsMenuInterno.js
@@ -0,0 +1,6 @@
+export const UrlsMenuInterno = [
+    {label: "Dados dos perfis", url: "gestao-de-perfis"},
+    {label: "Cargas de arquivo", url: "parametro-arquivos-de-carga", origem:'CARGA_USUARIOS'},
+];
+
+
