2791edee1ff054ba43a48dff2284cce8013ad750
ollyvergithub <ollyverottoboni@gmail.com>
2021-05-28 13:22:23 -0300

    Alterando componente Menu Interno para receber e validar permissão de exibição do link

--- src/componentes/Globais/MenuInterno/index.js
+++ src/componentes/Globais/MenuInterno/index.js
@@ -1,17 +1,18 @@
 import React, {Fragment} from "react";
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import "./menu-interno.scss"
 
 export const MenuInterno = ({caminhos_menu_interno}) => {
     return (
         <>
             <nav className="nav mb-4 mt-2 menu-interno">
-                {caminhos_menu_interno && caminhos_menu_interno.map((item, index)=>{
+                {caminhos_menu_interno && caminhos_menu_interno.map((item, index) => {
                     return (
+                        item.permissao !== false &&
                         <Fragment key={index}>
                             <li className="nav-item">
                                 <NavLink
-                                    to={`/${item.origem ? item.url + '/' + item.origem  + '/' : item.url}`}
+                                    to={`/${item.origem ? item.url + '/' + item.origem + '/' : item.url}`}
                                 >
                                     {item.label}
                                 </NavLink>
