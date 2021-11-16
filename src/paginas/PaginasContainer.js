f92520c5b8a09e7d14d33908eace6da955a9fc59
luke <lucas.srocha@outlook.com>
2021-11-16 15:44:31 -0300

    feat(51989) Mensagem presidente ausente
    Agora se o presidente estiver ausente, será exibido uma mensagem.

--- src/paginas/PaginasContainer.js
+++ src/paginas/PaginasContainer.js
@@ -1,15 +1,15 @@
 import React, {useContext} from "react";
 import {SidebarContext} from "../context/Sidebar";
 import {NotificacaoContext} from "../context/Notificacoes";
-import {BarraDeMensagem} from "../componentes/Globais/BarraMensagem"
 import {useHistory} from "react-router-dom";
 import {notificaDevolucaoPCService} from "../services/NotificacaDevolucaoPC.service";
-import {faExclamationCircle} from "@fortawesome/free-solid-svg-icons";
+import { barraMensagemCustom } from "../componentes/Globais/BarraMensagem";
 
 export const PaginasContainer = ({children}) => {
     const history = useHistory();
     const sidebarStatus = useContext(SidebarContext);
     const notificacaoContext = useContext(NotificacaoContext);
+    const mensagem = `A prestação de contas ${localStorage.getItem("NOTIFICAR_DEVOLUCAO_REFERENCIA")} foi devolvida para acertos pela DRE.`
 
     const onVerAcertos = () => {
         notificacaoContext.setExibeMensagemFixaTemDevolucao(false)
@@ -20,12 +20,7 @@ export const PaginasContainer = ({children}) => {
         <>
             <div className={`page-content  px-5 pt-0 pb-5 ${!sidebarStatus.sideBarStatus ? "active" : ""}`} id="content">
                 { notificacaoContext.exibeMensagemFixaTemDevolucao &&
-                    <BarraDeMensagem
-                        mensagem={`A prestação de contas ${localStorage.getItem("NOTIFICAR_DEVOLUCAO_REFERENCIA")} foi devolvida para acertos pela DRE.`}
-                        textoBotao={'Ver acertos'}
-                        handleClickBotao={onVerAcertos}
-                        icone={faExclamationCircle}
-                    />
+                    barraMensagemCustom.BarraMensagemSucessLaranja(mensagem, "Ver acertos", onVerAcertos, true)
                 }
                 {children}
             </div>
