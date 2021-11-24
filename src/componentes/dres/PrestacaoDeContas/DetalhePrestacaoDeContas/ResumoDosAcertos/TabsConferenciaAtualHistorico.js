45626ffb23bcb8478074dd9358f5c6dfaa620e31
luke <lucas.srocha@outlook.com>
2021-11-24 15:43:29 -0300

    feat(53366) esconder aba "Conferencia atual"
    Agora a aba "Conferencia atual" só é exibida caso a PC esteja em análise

--- src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/ResumoDosAcertos/TabsConferenciaAtualHistorico.js
+++ src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/ResumoDosAcertos/TabsConferenciaAtualHistorico.js
@@ -6,33 +6,35 @@ import {MsgImgCentralizada} from "../../../../Globais/Mensagens/MsgImgCentraliza
 import Img404 from "../../../../../assets/img/img-404.svg";
 import CardsDevolucoesParaAcertoDaDre from "../../../../Globais/CardsDevolucoesParaAcertoDaDre";
 
-const TabsConferenciaAtualHistorico = ({dataLimiteDevolucao, handleChangeDataLimiteDevolucao, prestacao_conta_uuid, analiseAtualUuid, exibeMsg, textoMsg, totalAnalisesDePcDevolvidas, setAnaliseAtualUuidComPCAnaliseAtualUuid, setPrimeiraAnalisePcDevolvida, setAnaliseAtualUuid, editavel}) =>{
+const TabsConferenciaAtualHistorico = ({dataLimiteDevolucao, handleChangeDataLimiteDevolucao, prestacao_conta_uuid, analiseAtualUuid, exibeMsg, textoMsg, totalAnalisesDePcDevolvidas, setAnaliseAtualUuidComPCAnaliseAtualUuid, setPrimeiraAnalisePcDevolvida, setAnaliseAtualUuid, editavel, pcEmAnalise}) =>{
 
     return(
         <>
             <nav>
-                <div className="nav nav-tabs mb-3 tabs-resumo-dos-acertos" id="nav-tab-conferencia-de-lancamentos" role="tablist">
-                    <a
-                        onClick={setAnaliseAtualUuidComPCAnaliseAtualUuid}
-                        className="nav-link btn-escolhe-acao active"
-                        id="nav-conferencia-atual-tab" data-toggle="tab"
-                        href="#nav-conferencia-atual"
-                        role="tab"
-                        aria-controls="nav-conferencia-atual"
-                        aria-selected="true"
-                    >
-                        Conferência atual
-                    </a>
+                <div className="nav nav-tabs mb-3 tabs-resumo-dos-acertos" id="nav-tab-conferencia-de-lancamentos" role="tablist">      
+                    { pcEmAnalise &&
+                        <a
+                            onClick={setAnaliseAtualUuidComPCAnaliseAtualUuid}
+                            className="nav-link btn-escolhe-acao active"
+                            id="nav-conferencia-atual-tab" data-toggle="tab"
+                            href="#nav-conferencia-atual"
+                            role="tab"
+                            aria-controls="nav-conferencia-atual"
+                            aria-selected="true"
+                        >
+                            Conferência atual
+                        </a>
+                    }
                     {totalAnalisesDePcDevolvidas > 0 &&
                         <a
                             onClick={setPrimeiraAnalisePcDevolvida}
-                            className="nav-link btn-escolhe-acao"
+                            className={`nav-link btn-escolhe-acao ${pcEmAnalise === false ? 'active' : ''}`}
                             id="nav-historico-tab"
                             data-toggle="tab"
                             href="#nav-historico"
                             role="tab"
                             aria-controls="nav-historico"
-                            aria-selected="false"
+                            aria-selected={pcEmAnalise === false ? 'true' : 'false'}
                         >
                             Histórico
                         </a>
@@ -40,7 +42,7 @@ const TabsConferenciaAtualHistorico = ({dataLimiteDevolucao, handleChangeDataLim
                 </div>
             </nav>
             <div className="tab-content" id="nav-tabContent">
-                <div className="tab-pane fade show active" id="nav-conferencia-atual" role="tabpanel" aria-labelledby="nav-conferencia-atual-tab">
+                <div className={`tab-pane fade ${pcEmAnalise ? 'show active' : ''}`} id="nav-conferencia-atual" role="tabpanel" aria-labelledby="nav-conferencia-atual-tab">
                     {exibeMsg ? (
                             <MsgImgCentralizada
                                 texto={textoMsg}
@@ -67,7 +69,7 @@ const TabsConferenciaAtualHistorico = ({dataLimiteDevolucao, handleChangeDataLim
                         </>
                     }
                 </div>
-                <div className="tab-pane fade" id="nav-historico" role="tabpanel" aria-labelledby="nav-historico-tab">
+                <div className={`tab-pane fade ${pcEmAnalise === false ? 'show active' : ''}`} id="nav-historico" role="tabpanel" aria-labelledby="nav-historico-tab">
                     <>
                         <CardsDevolucoesParaAcertoDaDre
                             prestacao_conta_uuid={prestacao_conta_uuid}
