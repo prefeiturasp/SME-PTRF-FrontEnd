4785d925569811fee3e33987a127a5ad5b053698
Alessandro Fernandes <alcfernandes@yahoo.com>
2021-12-23 07:06:21 -0300

    feat(40174): Altera exibição motivo não regularidade se apenas leitura
    Corrige a exibição de análise de regularidade na aba histórico para
    apenas exibir o campo motivo de não regularidade sem permitir
    edição.
    
    Também corrige a exibição do nome da Associação para não exibir
    "undefined" enquanto carrega os dados.

--- src/componentes/dres/RegularidadeAssociacoes/AnalisesRegularidade/NomeAssociacaoBotaoVoltar.js
+++ src/componentes/dres/RegularidadeAssociacoes/AnalisesRegularidade/NomeAssociacaoBotaoVoltar.js
@@ -2,12 +2,14 @@ import React from "react";
 
 export const NomeAssociacaoBotaoVoltar = ({nomeAssociacao}) => {
     const path =  `/regularidade-associacoes`;
-
     return (
         <>
             <div className="d-flex bd-highlight">
                 <div className="p-2 flex-grow-1 bd-highlight">
-                    <h1 className="titulo-itens-painel mt-5">{nomeAssociacao}</h1>
+                    { nomeAssociacao  &&
+                        <h1 className="titulo-itens-painel mt-5">{nomeAssociacao}</h1>
+                    }
+
                 </div>
                 <div className="p-2 bd-highlight mt-5">
                     <button onClick={() => window.location.assign(path)} className="btn btn btn-outline-success">Voltar</button>
