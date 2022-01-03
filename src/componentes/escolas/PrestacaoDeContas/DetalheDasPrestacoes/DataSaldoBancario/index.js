10b3f67eb0a1b3a5f5c8593e16ec35aeb0102ff1
Alessandro Fernandes <alcfernandes@yahoo.com>
2022-01-03 14:25:35 -0300

    feat(56679): Altera conciliação para exibir data atu comprovante
    Altera o formulário de saldo bancário na conciliação para exibir a data e
    hora da última atualização do comprovante de saldo bancário.

--- src/componentes/escolas/PrestacaoDeContas/DetalheDasPrestacoes/DataSaldoBancario/index.js
+++ src/componentes/escolas/PrestacaoDeContas/DetalheDasPrestacoes/DataSaldoBancario/index.js
@@ -15,7 +15,8 @@ const DataSaldoBancario = ({
     nomeComprovanteExtrato, exibeBtnDownload, msgErroExtensaoArquivo,
     changeUploadExtrato, reiniciaUploadExtrato, downloadComprovanteExtrato, salvarExtratoBancario,
     btnSalvarExtratoBancarioDisable, setBtnSalvarExtratoBancarioDisable, classBtnSalvarExtratoBancario,
-    setClassBtnSalvarExtratoBancario, checkSalvarExtratoBancario, setCheckSalvarExtratoBancario, erroDataSaldo
+    setClassBtnSalvarExtratoBancario, checkSalvarExtratoBancario, setCheckSalvarExtratoBancario, erroDataSaldo,
+    dataAtualizacaoComprovanteExtrato
 }) => {
 
     const handleOnClick = () => {
@@ -111,6 +112,11 @@ const DataSaldoBancario = ({
                                                                     icon={faPaperclip}/>
                                                                 </span>{nomeComprovanteExtrato}
                                                             </p>
+                                                            { exibeBtnDownload &&
+                                                                <p>
+                                                                    <span>Atualizado em: {dataAtualizacaoComprovanteExtrato}</span>
+                                                                </p>
+                                                            }
                                                         </div>
 
                                                         <div className="col-lg-4 mt-2 text-right">
