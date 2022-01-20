cdae35ba315e33fcba9d78e1e84c592b6a68874c
Alessandro Fernandes <alcfernandes@yahoo.com>
2022-01-20 08:29:16 -0300

    feat(52133): Cria previas de atas de apresentação e retificação
    Faz as alterações necessárias para o tratamento de prévias de atas de
    apresentação e retificação.

--- src/componentes/escolas/GeracaoDaAta/VisualizacaoDaAta/TopoComBotoes/index.js
+++ src/componentes/escolas/GeracaoDaAta/VisualizacaoDaAta/TopoComBotoes/index.js
@@ -6,7 +6,19 @@ export const TopoComBotoes = ({dadosAta, prestacaoDeContasDetalhe, handleClickEd
     return(
         <div className="row">
             <div className='col-12 col-md-5 mt-2'>
-                <p className='titulo-visualizacao-da-ata'>{dadosAta.tipo_ata === 'RETIFICACAO' ? 'Visualização da ata de retificação' : 'Visualização da ata'}</p>
+
+                {/*Para as atas de apresentação (Não retificação)*/}
+                {/*Se não tem Prestação de Conta trata-se de uma prévia*/}
+                {dadosAta && dadosAta.tipo_ata !== 'RETIFICACAO' &&
+                <p className='titulo-visualizacao-da-ata'>{dadosAta.prestacao_conta ? 'Visualização da ata' : 'Visualização da prévia da ata'}</p>
+                }
+
+                {/*Para as atas de retificação*/}
+                {/*Se o status da PC é DEVOLVIDA trata-se de uma prévia*/}
+                {dadosAta && dadosAta.tipo_ata === 'RETIFICACAO' && prestacaoDeContasDetalhe && prestacaoDeContasDetalhe.status &&
+                <p className='titulo-visualizacao-da-ata'>{prestacaoDeContasDetalhe.status !== 'DEVOLVIDA' ? 'Visualização da ata de retificação' : 'Visualização da prévia da ata de retificação'}</p>
+                }
+
             </div>
 
             <div className='col-12 col-md-7 text-right'>
@@ -25,7 +37,19 @@ export const TopoComBotoes = ({dadosAta, prestacaoDeContasDetalhe, handleClickEd
                         : null
                     )
                 }
+
+                {/*Para as atas de apresentação (Não retificação)*/}
+                {/*Se não tem Prestação de Conta trata-se de uma prévia e não exibe o selecionar e copiar*/}
+                {dadosAta && dadosAta.tipo_ata !== 'RETIFICACAO' && dadosAta.prestacao_conta &&
                 <button onClick={handleClickCopiarAta} type="button" className="btn btn-outline-success mr-2 mt-2"><strong>Selecionar e copiar</strong></button>
+                }
+
+                {/*Para as atas de retificação*/}
+                {/*Se o status da PC é DEVOLVIDA trata-se de uma prévia e não exibe o selecionar e copiar*/}
+                {dadosAta && dadosAta.tipo_ata === 'RETIFICACAO' && prestacaoDeContasDetalhe && prestacaoDeContasDetalhe.status && prestacaoDeContasDetalhe.status !== 'DEVOLVIDA' &&
+                <button onClick={handleClickCopiarAta} type="button" className="btn btn-outline-success mr-2 mt-2"><strong>Selecionar e copiar</strong></button>
+                }
+
                 <button onClick={handleClickFecharAta} type="button" className="btn btn-outline-success mt-2"><strong>Fechar</strong></button>
             </div>
         </div>
