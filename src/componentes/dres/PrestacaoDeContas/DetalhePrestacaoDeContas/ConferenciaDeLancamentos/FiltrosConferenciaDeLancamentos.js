16fb37fdd2cc55fdc792d294d7db55c45fd32235
linikerj <linikerenem@gmail.com>
2022-07-06 15:09:09 -0300

    feat(66783) Finaliza persistencia em datas, e altera campos de período para range picker

--- src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/ConferenciaDeLancamentos/FiltrosConferenciaDeLancamentos.js
+++ src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/ConferenciaDeLancamentos/FiltrosConferenciaDeLancamentos.js
@@ -1,5 +1,9 @@
-import React from "react";
-import {DatePickerField} from "../../../../Globais/DatePickerField";
+import React from 'react';
+import {DatePicker, Space} from "antd";
+import locale from 'antd/es/date-picker/locale/pt_BR';
+import moment from "moment";
+import './../../../../sme/ExtracaoDados/extracao-dados.scss'
+
 
 export const FiltrosConferenciaDeLancamentos = ({stateFiltros, tabelasDespesa, handleChangeFiltros, handleSubmitFiltros, limpaFiltros})=>{
 
@@ -7,30 +11,6 @@ export const FiltrosConferenciaDeLancamentos = ({stateFiltros, tabelasDespesa, h
         <>
             <form>
                 <div className="form-row align-items-center">
-                    <div className="form-group col-md-4">
-                        <label htmlFor="data_inicio">Filtrar por intervalo de datas</label>
-                        <div className="row align-items-center">
-                            <div className="col-12 col-md-5 pr-0">
-                                <DatePickerField
-                                    name="filtrar_por_data_inicio"
-                                    id="filtrar_por_data_inicio"
-                                    value={stateFiltros.filtrar_por_data_inicio}
-                                    onChange={handleChangeFiltros}
-                                />
-                            </div>
-                            <div className="col-12 col-md-2 p-0 text-md-center ">
-                                <span>até</span>
-                            </div>
-                            <div className="col-12 col-md-5 pl-0">
-                                <DatePickerField
-                                    name="filtrar_por_data_fim"
-                                    id="filtrar_por_data_fim"
-                                    value={stateFiltros.filtrar_por_data_fim}
-                                    onChange={handleChangeFiltros}
-                                />
-                            </div>
-                        </div>
-                    </div>
                     <div className="form-group col">
                         <label htmlFor="filtrar_por_acao">Filtrar por ação</label>
                         <select
@@ -47,6 +27,20 @@ export const FiltrosConferenciaDeLancamentos = ({stateFiltros, tabelasDespesa, h
                         </select>
                     </div>
                     <div className="form-group col">
+                        <label htmlFor="filtrar_por_nome_fornecedor">Filtrar por fornecedor</label>
+                        <input value={stateFiltros.filtrar_por_nome_fornecedor}
+                               onChange={(e) => handleChangeFiltros(e.target.name, e.target.value)}
+                               name={`filtrar_por_nome_fornecedor`}
+                               id={`filtrar_por_nome_fornecedor`}
+                               type="text"
+                               className="form-control"
+                               placeholder="Escreva a razão social do fornecedor"
+                        />
+                    </div>
+                </div>
+
+                <div className="form-row align-items-center">
+                    <div className="form-group col">
                         <label htmlFor="filtrar_por_lancamento">Filtrar por tipo de lançamento</label>
                         <select
                             value={stateFiltros.filtrar_por_lancamento}
@@ -60,9 +54,6 @@ export const FiltrosConferenciaDeLancamentos = ({stateFiltros, tabelasDespesa, h
                             <option value='GASTOS'>Gastos</option>
                         </select>
                     </div>
-                </div>
-
-                <div className="form-row align-items-center">
                     <div className="form-group col">
                         <label htmlFor="filtrar_por_numero_de_documento">Filtrar por número de documento</label>
                         <input value={stateFiltros.filtrar_por_numero_de_documento}
@@ -89,8 +80,9 @@ export const FiltrosConferenciaDeLancamentos = ({stateFiltros, tabelasDespesa, h
                             )}
                         </select>
                     </div>
-
-                    <div className="form-group col">
+                </div>
+                <div className="form-row align-items-center">
+                    <div className="form-group col-md-3">
                         <label htmlFor="filtrar_por_tipo_de_pagamento">Filtrar por tipo de transação/pagamento</label>
                         <select
                             value={stateFiltros.filtrar_por_tipo_de_pagamento}
@@ -105,7 +97,41 @@ export const FiltrosConferenciaDeLancamentos = ({stateFiltros, tabelasDespesa, h
                             ))}
                         </select>
                     </div>
+                    <div className="form-group col-md-6">
+                    <Space className='extracao-space' direction='vertical'>
+                    <span>Filtrar por período</span>
+                    <DatePicker.RangePicker
+                        locale={locale}
+                        format={'DD/MM/YYYY'}
+                        disabledDate={(date) => (
+                            (date).startOf('day').toDate().valueOf() >
+                            moment().startOf('day').toDate().valueOf()
+                        )}
+                        allowEmpty={[true, true]}
+                        className='form-control pr-3 w-50'
+                        placeholder={['data inicial', 'data final']}
+                        id="data_range"
+                        defaultValue={[
+                            stateFiltros.filtrar_por_data_inicio ? moment(stateFiltros.filtrar_por_data_inicio) : '',
+                            stateFiltros.filtrar_por_data_fim ? moment(stateFiltros.filtrar_por_data_fim): ''
+                        ]}
+                        
+                        onCalendarChange={(date) => {
+                            if(!date){
+                                handleChangeFiltros("filtrar_por_data_inicio", '');
+                                handleChangeFiltros("filtrar_por_data_fim", '');
+                            }
+                            else {
+                                date[0] && handleChangeFiltros("filtrar_por_data_inicio", date[0].format('YYYY-MM-DD'));
+                                date[1] && handleChangeFiltros("filtrar_por_data_fim", date[1].format('YYYY-MM-DD'));
+                            }
+                        }
+                    }
+                    />
+                    </Space>
+                    </div>
                 </div>
+
                 <div className="d-flex justify-content-end">
                     <button onClick={()=>limpaFiltros()} type="button" className="btn btn-success ml-md-2 mt-1">Limpar</button>
                     <button onClick={()=>handleSubmitFiltros()} type="button" className="btn btn-success ml-md-2 mt-1">Filtrar</button>
