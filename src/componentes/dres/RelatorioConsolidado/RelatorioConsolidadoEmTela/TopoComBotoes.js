0b28b2b739218644b85fa627ac824bc811f5e273
Alessandro Fernandes <alcfernandes@yahoo.com>
2022-08-09 11:35:18 -0300

    feat(70107): Remove componentes não usados em RelatorioConsolidadoEmTela
    Remove componentes e chamadas não usadas no componente
    RelatorioConsolidadoEmTela.

--- src/componentes/dres/RelatorioConsolidado/RelatorioConsolidadoEmTela/TopoComBotoes.js
+++ src/componentes/dres/RelatorioConsolidado/RelatorioConsolidadoEmTela/TopoComBotoes.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export const TopoComBotoes = ({periodoNome, contaNome}) =>{
+export const TopoComBotoes = ({periodoNome}) =>{
     return(
         <>
             <div className="d-flex bd-highlight mb-3">
-                <div className="py-2 flex-grow-1 bd-highlight"><h4 className='pl-0'>Período {periodoNome} | Conta {contaNome}</h4></div>
+                <div className="py-2 flex-grow-1 bd-highlight"><h4 className='pl-0'>Período {periodoNome}</h4></div>
                 <div className="py-2 bd-highlight">
                     <button onClick={()=>window.location.assign('/dre-relatorio-consolidado')} className="btn btn-outline-success">Voltar</button>
                 </div>
--- src/componentes/dres/RelatorioConsolidado/RelatorioConsolidadoEmTela/index.js
+++ src/componentes/dres/RelatorioConsolidado/RelatorioConsolidadoEmTela/index.js
@@ -1,28 +1,17 @@
 import React, {useEffect, useState, useCallback} from "react";
 import {useParams} from "react-router-dom";
-import {InfoAssociacoesEmAnalise} from "./InfoAssociacoesEmAnalise";
 import {
-    getItensDashboard,
     getExecucaoFinanceira,
-    getDevolucoesContaPtrf,
     getJustificativa,
     postJustificativa,
     patchJustificativa,
-    getDevolucoesAoTesouro,
-    putCriarEditarDeletarObservacaoDevolucaoContaPtrf,
-    putCriarEditarDeletarObservacaoDevolucaoTesouro,
     getConsolidadoDre
 } from "../../../../services/dres/RelatorioConsolidado.service";
 import {TopoComBotoes} from "./TopoComBotoes";
-import {BoxConsultarDados} from "./BoxConsultarDados";
 import {visoesService} from "../../../../services/visoes.service";
 import {TabelaExecucaoFinanceira} from "./TabelaExecucaoFinanceira";
 import {JustificativaDiferenca} from "./JustificativaDiferenca";
-import {TabelaDevolucoesContaPtrf} from "./TabelaDevolucoesContaPtrf";
-import {TabelaDevolucoesAoTesouro} from "./TabelaDevolucoesAoTesouro";
-import {TabelaExecucaoFisica} from "./TabelaExecucaoFisica";
 import {auxGetNomes} from "../auxGetNomes";
-import {ModalObservacoesRelatorioConsolidadoApuracao} from "../ModalObservacoesRelatorioConsolidadoApuracao";
 import {ModalSalvarJustificativa} from "../ModalSalvarJustificativa";
 import Loading from "../../../../utils/Loading";
 
@@ -44,36 +33,20 @@ export const RelatorioConsolidadoEmTela = () => {
         texto: ''
     };
 
-    const [itensDashboard, setItensDashboard] = useState(false);
-    const [totalEmAnalise, setTotalEmAnalise] = useState(0);
     const [periodoNome, setPeriodoNome] = useState('');
-    const [contaNome, setContaNome] = useState('');
     const [execucaoFinanceira, setExecucaoFinanceira] = useState(false);
     const [justificativaDiferenca, setJustificativaDiferenca] = useState(initJustificativa);
     const [btnSalvarJustificativaDisable, setBtnSalvarJustificativaDisable] = useState(true);
     const [showSalvarJustificativa, setShowSalvarJustificativa] = useState(false);
-    const [devolucoesContaPtrf, setDevolucoesContaPtrf] = useState(false);
-    const [devolucoesAoTesouro, setDevolucoesAoTesouro] = useState(false);
-
-    const [observacao, setObservacao] = useState(false);
-    const [showModalObservacao, setShowModalObservacao] = useState(false);
     const [loading, setLoading] = useState(false);
 
     // Consolidado DRE
     const [consolidadoDre, setConsolidadoDre] = useState(false);
 
     useEffect(() => {
-        carregaItensDashboard();
-    }, []);
-
-    useEffect(() => {
         carregaNomePeriodo();
-        carregaNomeConta();
-        retornaQtdeEmAnalise();
-        carregaDevolucoesContaPtrf();
         carregaJustificativa();
-        carregaDevolucoesAoTesouro();
-    }, [itensDashboard]);
+    }, []);
 
     // Consolidado DRE
     const carregaConsolidadoDre = useCallback(async () => {
@@ -95,23 +68,12 @@ export const RelatorioConsolidadoEmTela = () => {
         carregaConsolidadoDre()
     }, [carregaConsolidadoDre])
 
-    const carregaItensDashboard = async () => {
-        if (periodo_uuid) {
-            let itens = await getItensDashboard(periodo_uuid);
-            setItensDashboard(itens)
-        }
-    };
 
     const carregaNomePeriodo = async () => {
         if (periodo_uuid) {
             let periodo_nome = await auxGetNomes.nomePeriodo(periodo_uuid);
             setPeriodoNome(periodo_nome);
         }
-    };
-
-    const carregaNomeConta = async () => {
-        let conta_nome = await auxGetNomes.nomeConta(conta_uuid);
-        setContaNome(conta_nome);
     };
 
     const carregaExecucaoFinanceira = useCallback( async () => {
@@ -128,23 +90,6 @@ export const RelatorioConsolidadoEmTela = () => {
         carregaExecucaoFinanceira();
     }, [carregaExecucaoFinanceira]);
 
-    const carregaDevolucoesContaPtrf = async () => {
-        try {
-            let devolucoes = await getDevolucoesContaPtrf(dre_uuid, periodo_uuid, conta_uuid);
-            setDevolucoesContaPtrf(devolucoes);
-        } catch (e) {
-            console.log("Erro ao carregar Devolucoes a Conta Ptrf ", e);
-        }
-    };
-
-    const carregaDevolucoesAoTesouro = async () => {
-        try {
-            let devolucoes = await getDevolucoesAoTesouro(dre_uuid, periodo_uuid, conta_uuid);
-            setDevolucoesAoTesouro(devolucoes)
-        } catch (e) {
-            console.log("Erro ao carregar Devolucoes ao Tesouro ", e);
-        }
-    };
 
     const carregaJustificativa = async () => {
         try {
@@ -154,14 +99,6 @@ export const RelatorioConsolidadoEmTela = () => {
             }
         } catch (e) {
             console.log("Erro ao carregar justificativa ", e)
-        }
-    };
-
-
-    const retornaQtdeEmAnalise = () => {
-        if (itensDashboard) {
-            let total = itensDashboard.cards.filter(elemtent => elemtent.status === 'RECEBIDA' || elemtent.status === 'DEVOLVIDA' || elemtent.status === 'EM_ANALISE').reduce((total, valor) => total + valor.quantidade_prestacoes, 0);
-            setTotalEmAnalise(total)
         }
     };
 
@@ -207,74 +144,10 @@ export const RelatorioConsolidadoEmTela = () => {
         }
     };
 
-    const retornaQtdePorStatus = (status) => {
-        let item = itensDashboard.cards.find(element => element.status === status);
-        return item.quantidade_prestacoes;
-    };
-
-    const retornaNaoApresentadas = () => {
-        return itensDashboard.total_associacoes_dre - retornaQtdePorStatus('EM_ANALISE') - retornaQtdePorStatus('APROVADA') - retornaQtdePorStatus('APROVADA_RESSALVA') - retornaQtdePorStatus('REPROVADA');
-    };
-
-    const onHandleClose = () => {
-        setShowModalObservacao(false);
-    };
 
     const onHandleCloseSalvarJustificativa = () => {
         setShowSalvarJustificativa(false);
     }
-
-
-    // Observações
-    // Os métodos onClickObservacao, onChangeObservacao e serviceObservacao, servem tanto para devoluções a conta PTRF quanto devoluções ao tesouro
-    // É passado ao clicar nas respectivas tabelas os parâmetros tipo_devolucao:'devolucao_conta', tipo_devolucao:'devolucao_tesouro' e operacao:'salvar' e operacao:'deletar'
-
-    const onClickObservacao = (devolucao) => {
-        setShowModalObservacao(true);
-        setObservacao(devolucao)
-    };
-
-    const onChangeObservacao = (valor) => {
-        setObservacao({
-            ...observacao,
-            observacao: valor,
-        })
-    };
-
-    const serviceObservacao = async (operacao) => {
-        setShowModalObservacao(false);
-        setLoading(true);
-        let payload;
-
-        if (operacao.operacao === 'salvar') {
-            payload = {
-                observacao: observacao.observacao,
-            };
-        } else if (operacao.operacao === 'deletar') {
-            payload = {
-                observacao: '',
-            };
-        }
-
-        if (observacao.tipo_devolucao === 'devolucao_conta') {
-            try {
-                await putCriarEditarDeletarObservacaoDevolucaoContaPtrf(dre_uuid, periodo_uuid, conta_uuid, observacao.tipo_uuid, payload);
-                await carregaDevolucoesContaPtrf();
-                console.log("Operação de ", operacao.operacao, " Observação devolução a conta PTRF realizada com sucesso")
-            } catch (e) {
-                console.log("Erro ao ", operacao.operacao, "observação devolução a conta PTRF", e)
-            }
-        } else if (observacao.tipo_devolucao === 'devolucao_tesouro') {
-            try {
-                await putCriarEditarDeletarObservacaoDevolucaoTesouro(dre_uuid, periodo_uuid, conta_uuid, observacao.tipo_uuid, payload);
-                await carregaDevolucoesAoTesouro();
-                console.log("Operação de ", operacao.operacao, " Observação devolução ao tesouro realizada com sucesso")
-            } catch (e) {
-                console.log("Erro ao ", operacao.operacao, "observação devolução ao Tesouro ", e)
-            }
-        }
-        setLoading(false);
-    };
 
     const publicado = () => {
         if(!consolidadoDre){
@@ -306,19 +179,9 @@ export const RelatorioConsolidadoEmTela = () => {
                     ) :
                     <>
                         <div className="col-12 mt-5">
-                            <h1>Em tela...</h1>
+                            <h1>Novo</h1>
                             <TopoComBotoes
                                 periodoNome={periodoNome}
-                                contaNome={contaNome}
-                            />
-                            <InfoAssociacoesEmAnalise
-                                totalEmAnalise={totalEmAnalise}
-                                periodoUuid={periodo_uuid}
-                            />
-                            <BoxConsultarDados
-                                periodo_uuid={periodo_uuid}
-                                conta_uuid={conta_uuid}
-                                jaPublicado={jaPublicado}
                             />
                             <TabelaExecucaoFinanceira
                                 execucaoFinanceira={execucaoFinanceira}
@@ -335,33 +198,7 @@ export const RelatorioConsolidadoEmTela = () => {
                                 setBtnSalvarJustificativaDisable={setBtnSalvarJustificativaDisable}
                                 jaPublicado={jaPublicado}
                             />
-                            <TabelaDevolucoesContaPtrf
-                                devolucoesContaPtrf={devolucoesContaPtrf}
-                                valorTemplate={valorTemplate}
-                                onClickObservacao={onClickObservacao}
-                            />
-                            <TabelaDevolucoesAoTesouro
-                                devolucoesAoTesouro={devolucoesAoTesouro}
-                                valorTemplate={valorTemplate}
-                                onClickObservacao={onClickObservacao}
-                            />
-                            <TabelaExecucaoFisica
-                                itensDashboard={itensDashboard}
-                                retornaQtdePorStatus={retornaQtdePorStatus}
-                                retornaNaoApresentadas={retornaNaoApresentadas}
-                            />
                         </div>
-                        <section>
-                            <ModalObservacoesRelatorioConsolidadoApuracao
-                                show={showModalObservacao}
-                                handleClose={onHandleClose}
-                                observacao={observacao}
-                                onChangeObservacao={onChangeObservacao}
-                                serviceObservacao={serviceObservacao}
-                                titulo="Observação sobre devolução"
-                            />
-                        </section>
-
                         <section>
                             <ModalSalvarJustificativa
                                 show={showSalvarJustificativa}
