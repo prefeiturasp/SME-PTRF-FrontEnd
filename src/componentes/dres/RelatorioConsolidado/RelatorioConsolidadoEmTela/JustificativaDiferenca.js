6f5cda01c550f986c1c78b0f9bde44b631118210
Alessandro Fernandes <alcfernandes@yahoo.com>
2022-08-11 11:17:38 -0300

    feat(70107): Implementa justificativa por conta em página única
    Implementa os campos de digitação de justificativas de diferença por
    conta na página única do relatório em tela.

--- src/componentes/dres/RelatorioConsolidado/RelatorioConsolidadoEmTela/JustificativaDiferenca.js
+++ src/componentes/dres/RelatorioConsolidado/RelatorioConsolidadoEmTela/JustificativaDiferenca.js
@@ -2,7 +2,7 @@ import React from "react";
 import {visoesService} from "../../../../services/visoes.service";
 
 
-export const JustificativaDiferenca = ({comparaValores, justificativaDiferenca, setJustificativaDiferenca, onChangeJustificativaDiferenca, onSubmitJustificativaDiferenca, btnSalvarJustificativaDisable, setBtnSalvarJustificativaDisable, jaPublicado}) => {
+export const JustificativaDiferenca = ({execucaoFinanceira, comparaValores, justificativaDiferenca, setJustificativaDiferenca, onChangeJustificativaDiferenca, onSubmitJustificativaDiferenca, btnSalvarJustificativaDisable, setBtnSalvarJustificativaDisable, jaPublicado}) => {
     
     const onClickBtnLimpar = () => {
         setJustificativaDiferenca({
@@ -11,14 +11,20 @@ export const JustificativaDiferenca = ({comparaValores, justificativaDiferenca, 
         })
         setBtnSalvarJustificativaDisable(false);
     }
-
     return(
         <>
-            {comparaValores() &&
+            {comparaValores(execucaoFinanceira.valores) &&
                 <>
                     <p className='texto-aviso-associacoes-em-analise'><strong>Justificativa da diferença entre o valor previsto pela SME e o transferido pela DRE no período</strong></p>
                     <div className="form-group">
-                        <textarea disabled={!visoesService.getPermissoes(['change_relatorio_consolidado_dre'])} onChange={(e)=>onChangeJustificativaDiferenca(e.target.value)} className="form-control" id="justificativaDiferenca" rows="3" value={justificativaDiferenca.texto} placeholder='Escreva aqui a justificativa para essa diferença.'>
+                        <textarea
+                            disabled={!visoesService.getPermissoes(['change_relatorio_consolidado_dre'])}
+                            onChange={(e)=>onChangeJustificativaDiferenca(e.target.value, execucaoFinanceira.tipo_conta)}
+                            className="form-control" id="justificativaDiferenca"
+                            rows="3"
+                            value={justificativaDiferenca.texto}
+                            placeholder='Escreva aqui a justificativa para essa diferença.'
+                        >
                         </textarea>
                     </div>
                     <div className="d-flex  justify-content-end pb-3">
--- src/componentes/dres/RelatorioConsolidado/RelatorioConsolidadoEmTela/index.js
+++ src/componentes/dres/RelatorioConsolidado/RelatorioConsolidadoEmTela/index.js
@@ -2,7 +2,6 @@ import React, {useEffect, useState, useCallback} from "react";
 import {useParams} from "react-router-dom";
 import {
     getExecucaoFinanceira,
-    getJustificativa,
     postJustificativa,
     patchJustificativa,
     getConsolidadoDre
@@ -35,16 +34,20 @@ export const RelatorioConsolidadoEmTela = () => {
 
     const [periodoNome, setPeriodoNome] = useState('');
     const [execucaoFinanceira, setExecucaoFinanceira] = useState(false);
-    const [justificativaDiferenca, setJustificativaDiferenca] = useState(initJustificativa);
-    const [btnSalvarJustificativaDisable, setBtnSalvarJustificativaDisable] = useState(true);
+    const [justificativaDiferencaCheque, setJustificativaDiferencaCheque] = useState(initJustificativa);
+    const [justificativaDiferencaCartao, setJustificativaDiferencaCartao] = useState(initJustificativa);
+    const [btnSalvarJustificativaDisableCheque, setBtnSalvarJustificativaDisableCheque] = useState(true);
+    const [btnSalvarJustificativaDisableCartao, setBtnSalvarJustificativaDisableCartao] = useState(true);
     const [showSalvarJustificativa, setShowSalvarJustificativa] = useState(false);
     const [loading, setLoading] = useState(false);
     const [ePrevia, setEPrevia] = useState(true);
     const [consolidadoDre, setConsolidadoDre] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
         carregaNomePeriodo();
-        carregaJustificativa();
+        carregaExecucaoFinanceira();
+        setLoading(false);
     }, []);
 
 
@@ -58,7 +61,6 @@ export const RelatorioConsolidadoEmTela = () => {
         if (dre_uuid && periodo_uuid){
             try {
                 let consolidado_dre = await getConsolidadoDre(dre_uuid, periodo_uuid)
-                console.log('Consolidado DRE:', consolidado_dre)
                 if (consolidado_dre && consolidado_dre.length > 0){
                     setConsolidadoDre(consolidado_dre[0])
                 }else {
@@ -82,31 +84,37 @@ export const RelatorioConsolidadoEmTela = () => {
         }
     };
 
-    const carregaExecucaoFinanceira = useCallback( async () => {
+
+    const carregaExecucaoFinanceira = async () => {
         try {
             let execucao = await getExecucaoFinanceira(dre_uuid, periodo_uuid,  consolidado_dre_uuid !== 'null' ? consolidado_dre_uuid : '');
-            console.log("Execução Financeira:", execucao)
+            console.log('Execução:', execucao)
             setExecucaoFinanceira(execucao);
+            carregaJustificativas(execucao)
         } catch (e) {
             console.log("Erro ao carregar execução financeira ", e)
         }
-    }, [dre_uuid, periodo_uuid, consolidado_dre_uuid]);
-
-    useEffect(() => {
-        carregaExecucaoFinanceira();
-    }, [carregaExecucaoFinanceira]);
-
-
-    const carregaJustificativa = async () => {
-        try {
-            let justificativa = await getJustificativa(dre_uuid, periodo_uuid, conta_uuid);
-            if (justificativa && justificativa.length > 0) {
-                setJustificativaDiferenca(justificativa[0])
-            }
-        } catch (e) {
-            console.log("Erro ao carregar justificativa ", e)
-        }
     };
+
+    const carregaJustificativas = (execucao) => {
+        if (execucao) {
+            execucao.por_tipo_de_conta.map((conta) => {
+                const justificativa = {
+                    uuid: conta.justificativa_uuid,
+                    dre: dre_uuid,
+                    periodo: periodo_uuid,
+                    tipo_conta: conta.tipo_conta_uuid,
+                    texto: conta.justificativa_texto,
+                }
+                if (conta.tipo_conta === 'Cheque'){
+                    setJustificativaDiferencaCheque(justificativa)
+                }
+                else if (conta.tipo_conta === 'Cartão'){
+                    setJustificativaDiferencaCartao(justificativa)
+                }
+            })
+        }
+    }
 
     const valorTemplate = (valor) => {
         let valor_formatado = Number(valor).toLocaleString('pt-BR', {
@@ -126,27 +134,39 @@ export const RelatorioConsolidadoEmTela = () => {
         }
     };
 
-    const onChangeJustificativaDiferenca = (justificativa_texto) => {
-        setBtnSalvarJustificativaDisable(false);
-        setJustificativaDiferenca({
-            ...justificativaDiferenca,
-            texto: justificativa_texto
-        })
+    const onChangeJustificativaDiferenca = (justificativa_texto, tipo_conta) => {
+        if (tipo_conta === 'Cheque'){
+            setBtnSalvarJustificativaDisableCheque(false)
+            setJustificativaDiferencaCheque({...justificativaDiferencaCheque, texto:justificativa_texto})
+        }
+        else if (tipo_conta === 'Cartão'){
+            setBtnSalvarJustificativaDisableCartao(false)
+            setJustificativaDiferencaCartao({...justificativaDiferencaCartao, texto:justificativa_texto})
+        }
+
     };
 
-    const onSubmitJustificativaDiferenca = async () => {
+    const onSubmitJustificativaDiferencaCheque = async () => {
+        await atualizaJustificativaDiferenca(justificativaDiferencaCheque)
+        setBtnSalvarJustificativaDisableCheque(true)
+    };
+
+    const onSubmitJustificativaDiferencaCartao = async () => {
+        await atualizaJustificativaDiferenca(justificativaDiferencaCartao)
+        setBtnSalvarJustificativaDisableCartao(true)
+    };
+
+    const atualizaJustificativaDiferenca = async (justificativaDiferenca) => {
         if (justificativaDiferenca && justificativaDiferenca.uuid) {
             let payload = {
                 texto: justificativaDiferenca.texto
             };
             await patchJustificativa(justificativaDiferenca.uuid, payload)
             setShowSalvarJustificativa(true);
-            setBtnSalvarJustificativaDisable(true);
         } else {
             delete justificativaDiferenca.uuid;
             await postJustificativa(justificativaDiferenca)
             setShowSalvarJustificativa(true);
-            setBtnSalvarJustificativaDisable(true);
         }
     };
 
@@ -174,26 +194,28 @@ export const RelatorioConsolidadoEmTela = () => {
                                 periodoNome={periodoNome}
                                 ePrevia={ePrevia}
                             />
-                            {execucaoFinanceira && execucaoFinanceira.por_tipo_de_conta.map((valoresConta) => {
-                                return <TabelaExecucaoFinanceira
-                                    key={valoresConta.tipo_conta}
-                                    execucaoFinanceira={valoresConta.valores}
-                                    valorTemplate={valorTemplate}
-                                    comparaValores={comparaValores}
-                                    tipoConta={valoresConta.tipo_conta}
-                                />
+                            {execucaoFinanceira && execucaoFinanceira.por_tipo_de_conta.map((execucaoFinanceiraConta) => {
+                                return <div key={execucaoFinanceiraConta.tipo_conta}>
+                                    <TabelaExecucaoFinanceira
+                                        execucaoFinanceira={execucaoFinanceiraConta.valores}
+                                        valorTemplate={valorTemplate}
+                                        comparaValores={comparaValores}
+                                        tipoConta={execucaoFinanceiraConta.tipo_conta}
+                                    />
+                                    <JustificativaDiferenca
+                                        execucaoFinanceira={execucaoFinanceiraConta}
+                                        comparaValores={comparaValores}
+                                        justificativaDiferenca={execucaoFinanceiraConta.tipo_conta === 'Cheque' ? justificativaDiferencaCheque : justificativaDiferencaCartao}
+                                        setJustificativaDiferenca={execucaoFinanceiraConta.tipo_conta === 'Cheque' ? setJustificativaDiferencaCheque : setJustificativaDiferencaCartao}
+                                        onChangeJustificativaDiferenca={onChangeJustificativaDiferenca}
+                                        onSubmitJustificativaDiferenca={execucaoFinanceiraConta.tipo_conta === 'Cheque' ? onSubmitJustificativaDiferencaCheque : onSubmitJustificativaDiferencaCartao}
+                                        btnSalvarJustificativaDisable={execucaoFinanceiraConta.tipo_conta === 'Cheque' ? btnSalvarJustificativaDisableCheque : btnSalvarJustificativaDisableCartao}
+                                        setBtnSalvarJustificativaDisable={execucaoFinanceiraConta.tipo_conta === 'Cheque' ? setBtnSalvarJustificativaDisableCheque : setBtnSalvarJustificativaDisableCartao}
+                                        jaPublicado={jaPublicado}
+                                    />
+                                </div>
                             })}
 
-                            <JustificativaDiferenca
-                                comparaValores={comparaValores}
-                                justificativaDiferenca={justificativaDiferenca}
-                                setJustificativaDiferenca={setJustificativaDiferenca}
-                                onChangeJustificativaDiferenca={onChangeJustificativaDiferenca}
-                                onSubmitJustificativaDiferenca={onSubmitJustificativaDiferenca}
-                                btnSalvarJustificativaDisable={btnSalvarJustificativaDisable}
-                                setBtnSalvarJustificativaDisable={setBtnSalvarJustificativaDisable}
-                                jaPublicado={jaPublicado}
-                            />
                         </div>
                         <section>
                             <ModalSalvarJustificativa
