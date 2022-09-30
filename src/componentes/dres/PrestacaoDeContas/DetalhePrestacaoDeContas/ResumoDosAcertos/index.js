c4ba4dab4a8c3c6a66f4da73b7a256a5878b427b
luke <lucas.srocha@outlook.com>
2022-09-30 15:48:08 -0300

    feat(73288): PDF apos acerto final
    Agora a DRE pode realizar o download da versão final do relatorio apos acertos

--- src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/ResumoDosAcertos/index.js
+++ src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/ResumoDosAcertos/index.js
@@ -327,6 +327,7 @@ export const ResumoDosAcertos = () => {
                                 setPrimeiraAnalisePcDevolvida={setPrimeiraAnalisePcDevolvida} // Para TabsConferenciaAtualHistorico
                                 editavel={editavel}
                                 pcEmAnalise={pcEmAnalise}
+                                prestacaoDeContas={prestacaoDeContas}
                             />
                         ) :
                             <Loading
