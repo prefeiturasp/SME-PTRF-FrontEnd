5774567f7376c8e0b034c51c3910ca074f99a36e
linikerj <linikerenem@gmail.com>
2022-10-19 15:37:28 -0300

    feat(76333) Adiciona ponto ao invés de virgula em devolução ao tesouro e ajusta texto dos extratos bancários.

--- src/componentes/Globais/ExibeAcertosEmLancamentosEDocumentosPorConta/TabelaAcertosEmExtratosBancarios.js
+++ src/componentes/Globais/ExibeAcertosEmLancamentosEDocumentosPorConta/TabelaAcertosEmExtratosBancarios.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import moment from "moment";
+import {Link} from "react-router-dom";
+
 
 const TabelaAcertosEmExtratosBancarios = ({extratosBancariosAjustes}) => {
 
@@ -19,13 +21,13 @@ const TabelaAcertosEmExtratosBancarios = ({extratosBancariosAjustes}) => {
                 <div className="row mt-2">
                     {extratosBancariosAjustes.data_extrato &&
                     <div className="col-lg-4">
-                        <strong><p className="text-saldo-reprogramado">Data Extrato</p></strong>
+                        <strong><p className="text-saldo-reprogramado">Ajuste da data do extrato</p></strong>
                         <p>{moment(extratosBancariosAjustes.data_extrato).format('DD/MM/YYYY')}</p>
                     </div>
                     }
                     {extratosBancariosAjustes.saldo_extrato &&
                     <div className="col-lg-4">
-                        <strong><p className="text-saldo-reprogramado">Saldo Extrato</p></strong>
+                        <strong><p className="text-saldo-reprogramado">Ajuste no saldo do extrato</p></strong>
                         <p>{formataValor(extratosBancariosAjustes.saldo_extrato)}</p>
                     </div>
                     }
@@ -33,6 +35,12 @@ const TabelaAcertosEmExtratosBancarios = ({extratosBancariosAjustes}) => {
             ):
                 <p className='text-center fonte-18 mt-4'><strong>Não existem ajustes para serem exibidos</strong></p>
             }
+        <Link
+            to='/detalhe-das-prestacoes'
+            className="btn btn-outline-success"
+        >
+            Ir para conciliação bancária
+        </Link>
         </>
     )
 }
