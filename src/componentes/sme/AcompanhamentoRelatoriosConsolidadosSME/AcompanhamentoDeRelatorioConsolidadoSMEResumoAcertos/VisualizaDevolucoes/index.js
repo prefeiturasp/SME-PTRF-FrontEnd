00bb865761794066bc6ffffce88ab783c09127a9
linikerj <linikerenem@gmail.com>
2022-11-14 04:56:34 -0300

    feat(74211) Adiciona função para lidar com números ordinais

--- src/componentes/sme/AcompanhamentoRelatoriosConsolidadosSME/AcompanhamentoDeRelatorioConsolidadoSMEResumoAcertos/VisualizaDevolucoes/index.js
+++ src/componentes/sme/AcompanhamentoRelatoriosConsolidadosSME/AcompanhamentoDeRelatorioConsolidadoSMEResumoAcertos/VisualizaDevolucoes/index.js
@@ -1,33 +1,28 @@
 import React from 'react';
+import { Ordinais } from '../../../../../utils/NumeroPorExtenso'
 import './styles.scss'
 
-const mockData = [{
-    "data_devolucao": "2022-11-13",
-	"data_limite": "2022-11-15",
-	"data_retorno_analise": "2022-11-13",
-},
-{
-    "data_devolucao": "2023-11-13",
-	"data_limite": "2023-11-15",
-	"data_retorno_analise": "2023-11-13",
-},
-{
+const mockData = Array.from({length: 9}, () => ({
     "data_devolucao": "2024-11-13",
 	"data_limite": "2024-11-15",
 	"data_retorno_analise": "2024-11-13",
-},
-]
+}))
 
 export const VisualizaDevolucoes = ({}) => {
     return (
-        <div>
-            <span>
-                Visualize as devoluções pelas datas
-            </span>
-            {}
-            <select name="visual" id="visual">
-                <option value="" disabled></option>
-            </select>
+        <div className='visualizacao-container d-flex mt-5' >
+            <div className='col-md-3'>
+                <span>
+                    Visualize as devoluções pelas datas : 
+                </span>
+            </div>
+            <div className='col-md-6'>
+                <select name="escolhe-data-devolucao" id="escolhe-data-devolucao" className='form-control'>
+                    {mockData.map( (item, index) => {
+                        return <option key={index} value="">{Ordinais(index)} devolução {new Intl.DateTimeFormat('pt-BR', {year: 'numeric', month: '2-digit', day: '2-digit'},).format(new Date(item.data_devolucao))}</option>
+                    })}
+                </select>
+            </div>
         </div>
     )
     
