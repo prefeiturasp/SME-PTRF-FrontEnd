3565bb5877a9ce14120c30ca80d8cad3d6abcfd8
linikerj <linikerenem@gmail.com>
2022-11-17 11:01:49 -0300

    feat(74211) Adiciona css na pagina

--- src/componentes/sme/AcompanhamentoRelatoriosConsolidadosSME/AcompanhamentoDeRelatorioConsolidadoSMEResumoAcertos/TabsConferencia/index.js
+++ src/componentes/sme/AcompanhamentoRelatoriosConsolidadosSME/AcompanhamentoDeRelatorioConsolidadoSMEResumoAcertos/TabsConferencia/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.scss'
 
 export const TabsConferencia = ({relatorioConsolidado}) => {
-    return <div className="nav nav-tabs mb-3 tabs-resumo-dos-acertos" id="nav-tab-conferencia-de-lancamentos" role="tablist">
+    return <div className="nav nav-tabs mb-3 tabs-resumo-dos-acertos mt-3" id="nav-tab-conferencia-de-lancamentos" role="tablist">
         {
         relatorioConsolidado ?.status_sme === 'EM_ANALISE' ? <a
             className={`nav-link btn-escolhe-acao active`}
