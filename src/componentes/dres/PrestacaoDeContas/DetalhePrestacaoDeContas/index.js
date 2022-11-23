f0e94182de6f9d6a3a4fe86f473aae525e3b426d
linikerj <linikerenem@gmail.com>
2022-11-23 13:56:34 -0300

    feat(79389) Altera chamada da api em referencia da publicação.

--- src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/index.js
+++ src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/index.js
@@ -194,6 +194,7 @@ export const DetalhePrestacaoDeContas = () =>{
 
     useEffect(() => {
         const retornaPeriodo = async () => {
+            console.dir(prestacaoDeContas)
             if (prestacaoDeContas?.periodo_uuid){
                 let periodo = await getPeriodoPorUuid(prestacaoDeContas?.periodo_uuid);
                 setPeriodoReferencia(periodo.referencia)
@@ -202,20 +203,20 @@ export const DetalhePrestacaoDeContas = () =>{
         retornaPeriodo();
     }, [prestacaoDeContas?.periodo_uuid])
 
-    useEffect(() => {
-        const dre_uuid = visoesService.getItemUsuarioLogado('associacao_selecionada.uuid');
-        const carregaConsolidadosDreJaPublicadosProximaPublicacao = async () => {
-                if(prestacaoDeContas?.periodo_uuid){
-                    try {
-                        let consolidados_dre = await getConsolidadosDreJaPublicadosProximaPublicacao(dre_uuid, prestacaoDeContas?.periodo_uuid)
-                        setTituloRelatorio(consolidados_dre.publicacoes_anteriores[consolidados_dre.publicacoes_anteriores.length - 1].titulo_relatorio)
-                    } catch (e) {
-                        console.log("Erro ao buscar Consolidado Dre ", e)
-                    }
-                }
-        }
-        carregaConsolidadosDreJaPublicadosProximaPublicacao()
-    }, [prestacaoDeContas.periodo_uuid])
+    // useEffect(() => {
+    //     const dre_uuid = visoesService.getItemUsuarioLogado('associacao_selecionada.uuid');
+    //     const carregaConsolidadosDreJaPublicadosProximaPublicacao = async () => {
+    //             if(prestacaoDeContas?.periodo_uuid){
+    //                 try {
+    //                     let consolidados_dre = await getConsolidadosDreJaPublicadosProximaPublicacao(dre_uuid, prestacaoDeContas?.periodo_uuid)
+    //                     setTituloRelatorio(consolidados_dre.publicacoes_anteriores[consolidados_dre.publicacoes_anteriores.length - 1].titulo_relatorio)
+    //                 } catch (e) {
+    //                     console.log("Erro ao buscar Consolidado Dre ", e)
+    //                 }
+    //             }
+    //     }
+    //     carregaConsolidadosDreJaPublicadosProximaPublicacao()
+    // }, [prestacaoDeContas.periodo_uuid])
 
 
 
@@ -1213,7 +1214,7 @@ export const DetalhePrestacaoDeContas = () =>{
                     <ModalNaoPodeVoltarParaAnalise
                         show={showNaoPodeVoltarParaAnalise}
                         handleClose={onHandleClose}
-                        texto={`<p>Não é possível reabrir essa PC para análise, pois esta consta na ${tituloRelatorio} do período ${periodoReferencia}.</p>`}
+                        texto={`<p>Não é possível reabrir essa PC para análise, pois esta consta na Publicação ${prestacaoDeContas.referencia_consolidado_dre} do período ${periodoReferencia}.</p>`}
                         primeiroBotaoTexto="Fechar"
                         primeiroBotaoCss="success"
                     />
