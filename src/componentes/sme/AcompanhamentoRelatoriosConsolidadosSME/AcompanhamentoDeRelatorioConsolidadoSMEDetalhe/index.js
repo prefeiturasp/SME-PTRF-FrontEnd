762cdfa8085a4548d6526c2cae9bdbcd9149a8aa
linikerj <linikerenem@gmail.com>
2022-12-20 13:48:20 -0300

    feat(74210) altera visualização

--- src/componentes/sme/AcompanhamentoRelatoriosConsolidadosSME/AcompanhamentoDeRelatorioConsolidadoSMEDetalhe/index.js
+++ src/componentes/sme/AcompanhamentoRelatoriosConsolidadosSME/AcompanhamentoDeRelatorioConsolidadoSMEDetalhe/index.js
@@ -18,6 +18,7 @@ import Loading from "../../../../utils/Loading";
 
 import "./../../../dres/PrestacaoDeContas/prestacao-de-contas.scss"
 import DevolucaoParaAcertos from "./DevolucaoParaAcertos";
+import { BarraDeStatus } from '../../AcompanhamentoPcsSme/BarraDeStatus'
 
 export const AcompanhamentoDeRelatorioConsolidadoSMEDetalhe = () => {
     const params = useParams()
@@ -34,6 +35,7 @@ export const AcompanhamentoDeRelatorioConsolidadoSMEDetalhe = () => {
     const [selectedResponsavel, setSelectedResponsavel] = useState(null);
     const [habilitaVerResumoComentariosNotificados, setHabilitaVerResumoComentariosNotificados] = useState(false);
     const [habilitaVerResumoAcertoEmDocumento, setHabilitaVerResumoAcertoEmDocumento] = useState(false);
+    const [statusRelatorio, setStatusRelatorio] = useState({'cor_idx': 1, 'status_txt': ''});
 
     const getConsolidadoDREUuid = useCallback(async () => {
         let {consolidado_dre_uuid} = params
@@ -258,6 +260,7 @@ export const AcompanhamentoDeRelatorioConsolidadoSMEDetalhe = () => {
             :
                 <>
                     <div className="page-content-inner">
+                        <BarraDeStatus statusRelatorio={statusRelatorio} />
                         <Cabecalho relatorioConsolidado={relatorioConsolidado}/>
                         <BotoesAvancarRetroceder
                             relatorioConsolidado={relatorioConsolidado}
