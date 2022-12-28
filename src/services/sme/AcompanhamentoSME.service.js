fb4a54c966ffe59086353bd44a71f1d7aa5c5788
Lucas Santos Rocha <lucas.srocha@outlook.com>
2022-12-28 15:35:52 -0300

    feat(74212): Gerar previa relatorio sme
    Agora é possivel gerar a previa do relatorio de devolucao de acertos da sme

--- src/services/sme/AcompanhamentoSME.service.js
+++ src/services/sme/AcompanhamentoSME.service.js
@@ -63,3 +63,35 @@ export const downloadDocumentoRelatorio = (documento) => {
 export const getResumoConsolidado = (analise_uuid) => {
     return api.get(`/api/analises-consolidados-dre/${analise_uuid}/`, authHeader)
 }
+
+export const gerarPreviaRelatorioDevolucaoAcertosSme = async (uuid) => {    
+    if(uuid){
+      return (await api.get(`/api/analises-consolidados-dre/previa-relatorio-devolucao-acertos/?analise_consolidado_uuid=${uuid}`,authHeader)).data
+    }
+  }
+
+export const verificarStatusGeracaoDevolucaoAcertosSme = async (uuid) => {
+    return (await api.get(`/api/analises-consolidados-dre/status-info_relatorio_devolucao_acertos/?analise_consolidado_uuid=${uuid}`,authHeader)).data
+}
+
+export const downloadDocumentPdfDevolucaoAcertos = async (analise_atual_uuid) => {
+    return api
+        .get(`/api/analises-consolidados-dre/download-documento-pdf_devolucao_acertos/?analise_consolidado_uuid=${analise_atual_uuid}`, {
+            responseType: 'blob',
+            timeout: 30000,
+            headers: {
+                'Authorization': `JWT ${localStorage.getItem(TOKEN_ALIAS)}`,
+                'Content-Type': 'application/json',
+            }
+        })
+        .then((response) => {
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', `relatorio_dos_acertos.pdf`);
+            document.body.appendChild(link);
+            link.click();
+        }).catch(error => {
+            return error.response;
+        });
+  }
