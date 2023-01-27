30912254a1ef801d56de3035bf36e6985322022f
ollyvergithub <ollyverottoboni@gmail.com>
2023-01-27 09:37:03 -0300

    fix(78971) - Implementa monitoramento de PC

---
+++ src/componentes/Globais/Cabecalho/ModalNotificaErroConcluirPC.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {ModalBootstrap} from "../ModalBootstrap";
+
+export const ModalNotificaErroConcluirPC = (props) =>{
+    return (
+        <ModalBootstrap
+            show={props.show}
+            onHide={props.handleClose}
+            titulo={props.titulo}
+            bodyText={props.texto}
+            primeiroBotaoOnclick={props.handleClose}
+            primeiroBotaoTexto="Fechar"
+            primeiroBotaoCss="outline-success"
+            segundoBotaoOnclick={props.irParaConcluirPc}
+            segundoBotaoCss="success"
+            segundoBotaoTexto="Ir para Geração e Fechar"
+        />
+    )
+};
