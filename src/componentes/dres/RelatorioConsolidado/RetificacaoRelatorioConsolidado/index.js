45e3e43e454dd7b5abcc7d020e89bbb492bb3567
Matheus <matheus.diori@amcom.com.br>
2023-02-06 17:30:34 -0300

    feat(83168): Ajusta as mensagens de sucesso ao adicionar PCs em uma retificação

--- src/componentes/dres/RelatorioConsolidado/RetificacaoRelatorioConsolidado/index.js
+++ src/componentes/dres/RelatorioConsolidado/RetificacaoRelatorioConsolidado/index.js
@@ -580,7 +580,7 @@ const RetificacaoRelatorioConsolidado = () => {
 
         if(ehEdicaoRetificacao){
             await updateRetificarPcs(relatorio_consolidado_uuid, payload);
-            toastCustom.ToastCustomSuccess('Retificar', 'A retificação da publicação foi criada com sucesso.')
+            toastCustom.ToastCustomSuccess('Sucesso!', 'As PCs selecionadas foram adicionadas para retificação.')
 
             setShowModal(false);
             setQuantidadeSelecionada(0);
@@ -594,7 +594,7 @@ const RetificacaoRelatorioConsolidado = () => {
         }
         else{
             await postRetificarPcs(relatorio_consolidado_uuid, payload)
-            toastCustom.ToastCustomSuccess('Retificar', 'A retificação da publicação foi criada com sucesso.')
+            toastCustom.ToastCustomSuccess('Sucesso!', 'A retificação da publicação foi criada com sucesso.')
             onClickVoltar();
         }    
     }
