40bf58c2efcb1205ff153f529833a8e470352376
Matheus de Castro Diori <44413101+mdiori@users.noreply.github.com>
2023-03-14 11:11:16 -0300

    feat(86738): Adiciona card de exportação de materiais e serviços (#1551)
    Co-authored-by: Matheus <matheus.diori@amcom.com.br>

--- src/componentes/sme/ExtracaoDados/Cards.js
+++ src/componentes/sme/ExtracaoDados/Cards.js
@@ -1,14 +1,20 @@
 export const cards = [
-    {
-        titulo: 'Créditos das Unidades Educacionais no período',
-        descricao: 'Arquivo com os créditos informados por todas as unidades educacionais no período. Arquivos: Principal e Motivos de estorno',
-        tags: ['CSV'],
-        endpoint: '/api/exportacoes-dados/creditos/'
-    },
     // { Para a criação de um novo card e feita a inserção de dados via objeto
     //     titulo: '',
     //     descricao: '',
     //     tags: [''],
     //     action: () => <CardButton > Exportar Dados </CardButton>
     // }
+    {
+        titulo: 'Créditos das Unidades Educacionais no período',
+        descricao: 'Arquivo com os créditos informados por todas as unidades educacionais no período. Arquivos: creditos_principal.csv e creditos_motivos_estorno.csv.',
+        tags: ['CSV'],
+        endpoint: '/api/exportacoes-dados/creditos/'
+    },
+    {
+        titulo: 'Especificações de materiais e serviços',
+        descricao: 'Arquivo com as especificações de materiais e serviços para cadastro de despesas nas unidades educacionais. Arquivos: especificacoes_materiais_servicos.csv e tipos_de_custeio.csv.',
+        tags: ['CSV'],
+        endpoint: '/api/exportacoes-dados/materiais-e-servicos/'
+    }
 ]
