f39bfd8de95cedcf482c6dde32b66156ec732dc4
Lucas Santos Rocha <67693972+Lucas-Santos-Rocha-dev@users.noreply.github.com>
2023-03-23 09:01:39 -0300

    Feature/88259 acompanhamento pc (#1572)
    * feat(85993): Altera a cor e o texto dos botões da modal (#1560)
    
    Co-authored-by: Matheus <matheus.diori@amcom.com.br>
    
    * feat(88025): Detalhar acertos, exibe nova categoria (#1562)
    
    Agora é possivel cadastrar tipos de acerto de documento da categoria edição informação
    
    * feat(88261): Adicionado loading botão gerar (#1566)
    
    Adicionado componente BlocoPublicacaoParcial para unificar o
    request getExecucaoFinanceira dos componentes PreviaDocumentoRetificado
    e DemonstrativoDaExecucaoFisicoFinanceira.
    
    Co-authored-by: Matheus <matheus.diori@amcom.com.br>
    
    * Hotfix 6.0.2 (#1569)
    
    * fix(84852): Remove edição dev.tesouro da ata de retificação
    
    Agora não é mais exibido o modal que dava acesso ao usuário editar os
    registros de devolução ao tesouro a partir da ata de retificação.
    
    * docs(6.0.1): Atualiza versão
    
    * docs(6.0.1): Atualiza hotfixes.md
    
    * Update hotfix601 fix85660 (#1509)
    
    * fix(78971) - Implementa monitoramento de PC
    
    * fix(85660): Altera texto modal erro ao concluir PC
    
    * docs(6.0.1): Atualiza hotfixes.md
    
    ---------
    
    Co-authored-by: ollyvergithub <ollyverottoboni@gmail.com>
    
    * Update hotfixes.md
    
    Corrige texto
    
    * feat(84513) Corrige tempo de resposta em pesquisa associação (#1510)
    
    Co-authored-by: Matheus <matheus.diori@amcom.com.br>
    
    * Merge bug 85204 na branch hotfix 6.0.1 (#1513)
    
    * fix(85204): Conciliacao reabertura seletiva
    
    Agora despesas criadas a partir da reabertura seletiva são automaticamente conciliadas
    
    * fix(85204): Merge em hotfix 6.0.1
    
    * docs(6.0.1): Atualiza hotfixes.md
    
    * Hotfix/6.0.2 (#1567)
    
    * fix(85715): Corrige loading relatório consolidado
    
    * fix(85715): Altera modo de setar o estado de acordo com retorno API EM_PROCESSAMENTO
    
    * docs(6.0.2): Atualiza versão e hotfixes.md
    
    * fix(88561): Acertos dados bancários não exibem corretamente (#1565)
    
    Agora as informações das contas são exibidas corretamente de acordo com a conta selecionada.
    
    ---------
    
    Co-authored-by: ollyvergithub <ollyverottoboni@gmail.com>
    Co-authored-by: Lucas Santos Rocha <67693972+Lucas-Santos-Rocha-dev@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ollyver Ottoboni <ollyverottoboni@gmail.com>
    Co-authored-by: Matheus de Castro Diori <44413101+mdiori@users.noreply.github.com>
    Co-authored-by: Matheus <matheus.diori@amcom.com.br>
    Co-authored-by: Lucas Santos Rocha <67693972+Lucas-Santos-Rocha-dev@users.noreply.github.com>
    
    * feat(88259): Acompanhamento de PC
    
    Agora a mensagem de status utiliza o retorno do endpoint
    
    ---------
    
    Co-authored-by: Ollyver Ottoboni <ollyverottoboni@gmail.com>
    Co-authored-by: Matheus de Castro Diori <44413101+mdiori@users.noreply.github.com>
    Co-authored-by: Matheus <matheus.diori@amcom.com.br>
    Co-authored-by: Alessandro Cunha Fernandes <alessandro.c.fernandes@gmail.com>

--- src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/Cabecalho.js
+++ src/componentes/dres/PrestacaoDeContas/DetalhePrestacaoDeContas/Cabecalho.js
@@ -24,23 +24,16 @@ const Cabecalho = ({prestacaoDeContas, exibeSalvar, metodoSalvarAnalise, btnSalv
     }, [prestacaoDeContas])
 
     const verificaPublicacao = useCallback( () => {
-    if (prestacaoDeContas && prestacaoDeContas.publicada && !prestacaoDeContas.em_retificacao){
-        setPcPublicada(true)
-        let textoPublicacao = `Essa PC consta da Publicação ${prestacaoDeContas.referencia_consolidado_dre}`
-        setPublicacaoTexto(textoPublicacao)
-    }
+        if (prestacaoDeContas && prestacaoDeContas.publicada && !prestacaoDeContas.em_retificacao){
+            setPcPublicada(true)
+            let textoPublicacao = prestacaoDeContas.referencia_consolidado_dre
+            setPublicacaoTexto(textoPublicacao)
+        }
     }, [prestacaoDeContas])
 
     const verificaRetificacao = useCallback( () => {
         if (prestacaoDeContas && prestacaoDeContas.em_retificacao && prestacaoDeContas.status){
-            let textoRetificacao = ""
-            if(prestacaoDeContas.status === "RECEBIDA" || prestacaoDeContas.status === "EM_ANALISE") {
-                textoRetificacao = `Essa PC consta da Publicação ${prestacaoDeContas.referencia_consolidado_dre_original}`
-            } else if ((prestacaoDeContas.status === "APROVADA" || prestacaoDeContas.status === "REPROVADA" || prestacaoDeContas.status === "APROVADA_RESSALVA") && !prestacaoDeContas.publicada) {
-                textoRetificacao = `Essa PC constará da ${prestacaoDeContas.referencia_consolidado_dre}`
-            } else if (prestacaoDeContas.publicada){
-                textoRetificacao = `Essa PC consta da ${prestacaoDeContas.referencia_consolidado_dre}`
-            }
+            let textoRetificacao = prestacaoDeContas.referencia_consolidado_dre
             setRetificacaoTexto(textoRetificacao);
         }
     }, [prestacaoDeContas])
