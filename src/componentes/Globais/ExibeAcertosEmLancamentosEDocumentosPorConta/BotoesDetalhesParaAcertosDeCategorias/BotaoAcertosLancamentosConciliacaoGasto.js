6fee29a71dd8b7ff030293a5091acfe7a763bcb7
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-03-28 13:44:59 -0300

    fix(89873): Link indevido de reversão ação (des)conciliação
    O link que reverte a realização da conciliação ou desconciliação não é
    mais exibido em uma situação de apenas leitura.

--- src/componentes/Globais/ExibeAcertosEmLancamentosEDocumentosPorConta/BotoesDetalhesParaAcertosDeCategorias/BotaoAcertosLancamentosConciliacaoGasto.js
+++ src/componentes/Globais/ExibeAcertosEmLancamentosEDocumentosPorConta/BotoesDetalhesParaAcertosDeCategorias/BotaoAcertosLancamentosConciliacaoGasto.js
@@ -4,42 +4,6 @@ import {faCheckCircle} from "@fortawesome/free-solid-svg-icons";
 import {RetornaSeTemPermissaoEdicaoAjustesLancamentos} from "../RetornaSeTemPermissaoEdicaoAjustesLancamentos";
 import {botoesAcertosLancamentosService as BtnService} from "./botoesAcertosLancamentosService.service";
 
-// const BotaoAcertosLancamentosConciliacaoGasto = ({carregaAcertosLancamentos, conta, analise_lancamento, prestacaoDeContas, analisePermiteEdicao}) => {
-//
-//     const TEMPERMISSAO = RetornaSeTemPermissaoEdicaoAjustesLancamentos(prestacaoDeContas, analisePermiteEdicao)
-//
-//     return (
-//         <>
-//             {analise_lancamento && !analise_lancamento.conciliacao_atualizada ? (
-//
-//                 <button
-//                     onClick={()=>BtnService.marcarGastoComoConciliado(analise_lancamento, prestacaoDeContas, carregaAcertosLancamentos, conta)}
-//                     className='btn btn-link clique-aqui-atualizada font-weight-bold'
-//                 >
-//                     { TEMPERMISSAO && 'Clique para conciliar'}
-//                 </button>
-//
-//             ):(
-//                 <>
-//                     TEMPERMISSAO ? (
-//                         <>
-//                             <button
-//                                 onClick={() => BtnService.marcarGastoComoDesconciliado(analise_lancamento, carregaAcertosLancamentos, conta)}
-//                                 className='btn btn-link link-green text-center'
-//                             >
-//                                 <FontAwesomeIcon
-//                                     style={{fontSize: '16px', color: '#00585E'}}
-//                                     icon={faCheckCircle}
-//                                 />
-//                                 <strong> Gasto atualizado. <span
-//                                     className='clique-aqui-atualizada'>{TEMPERMISSAO && 'Clique para desconciliar'}</span></strong>
-//                             </button>
-//                         </>
-//                     ) : (<strong>teste</strong>)
-//                 <>)
-//         </>
-//     )
-// }
 const BotaoAcertosLancamentosConciliacaoGasto = ({carregaAcertosLancamentos, conta, analise_lancamento, prestacaoDeContas, analisePermiteEdicao}) => {
 
     const TEMPERMISSAO = RetornaSeTemPermissaoEdicaoAjustesLancamentos(prestacaoDeContas, analisePermiteEdicao)
