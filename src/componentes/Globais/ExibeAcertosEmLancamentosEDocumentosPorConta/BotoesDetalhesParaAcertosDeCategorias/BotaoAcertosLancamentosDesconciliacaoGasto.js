2d5f7aa326831438e9f10c72ae7492634ab104a1
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-03-28 13:30:18 -0300

    fix(89873): Link indevido de reversão ação (des)conciliação
    O link que reverte a realização da conciliação ou desconciliação não é
    mais exibido em uma situação de apenas leitura.

--- src/componentes/Globais/ExibeAcertosEmLancamentosEDocumentosPorConta/BotoesDetalhesParaAcertosDeCategorias/BotaoAcertosLancamentosDesconciliacaoGasto.js
+++ src/componentes/Globais/ExibeAcertosEmLancamentosEDocumentosPorConta/BotoesDetalhesParaAcertosDeCategorias/BotaoAcertosLancamentosDesconciliacaoGasto.js
@@ -4,7 +4,13 @@ import {faCheckCircle} from "@fortawesome/free-solid-svg-icons";
 import {RetornaSeTemPermissaoEdicaoAjustesLancamentos} from "../RetornaSeTemPermissaoEdicaoAjustesLancamentos";
 import {botoesAcertosLancamentosService as BtnService} from "./botoesAcertosLancamentosService.service";
 
-const BotaoAcertosLancamentosDesconciliacaoGasto = ({carregaAcertosLancamentos, conta, analise_lancamento, prestacaoDeContas, analisePermiteEdicao}) => {
+const BotaoAcertosLancamentosDesconciliacaoGasto = ({
+                                                        carregaAcertosLancamentos,
+                                                        conta,
+                                                        analise_lancamento,
+                                                        prestacaoDeContas,
+                                                        analisePermiteEdicao
+                                                    }) => {
 
     const TEMPERMISSAO = RetornaSeTemPermissaoEdicaoAjustesLancamentos(prestacaoDeContas, analisePermiteEdicao)
 
@@ -13,26 +19,39 @@ const BotaoAcertosLancamentosDesconciliacaoGasto = ({carregaAcertosLancamentos, 
             {analise_lancamento && !analise_lancamento.conciliacao_atualizada ? (
 
                 <button
-                    onClick={()=>BtnService.marcarGastoComoDesconciliado(analise_lancamento, carregaAcertosLancamentos, conta)}
+                    onClick={() => BtnService.marcarGastoComoDesconciliado(analise_lancamento, carregaAcertosLancamentos, conta)}
                     className='btn btn-link clique-aqui-atualizada font-weight-bold'
                 >
-                    { TEMPERMISSAO && 'Clique para desconciliar'}
+                    {TEMPERMISSAO && 'Clique para desconciliar'}
                 </button>
 
-            ):
-                <>
-                    <button
-                        onClick={()=>BtnService.marcarGastoComoConciliado(analise_lancamento, prestacaoDeContas, carregaAcertosLancamentos, conta)}
-                        className='btn btn-link link-green text-center'
-                    >
-                        <FontAwesomeIcon
-                            style={{fontSize: '16px', color: '#00585E'}}
-                            icon={faCheckCircle}
-                        />
-                        <strong> Gasto atualizado. <span className='clique-aqui-atualizada'>{ TEMPERMISSAO && 'Clique para conciliar'}</span></strong>
-                    </button>
-                </>
-            }
+            ) : (
+                TEMPERMISSAO ? (
+                    <>
+                        <button
+                            onClick={() => BtnService.marcarGastoComoConciliado(analise_lancamento, prestacaoDeContas, carregaAcertosLancamentos, conta)}
+                            className='btn btn-link link-green text-center'
+                        >
+                            <FontAwesomeIcon
+                                style={{fontSize: '16px', color: '#00585E'}}
+                                icon={faCheckCircle}
+                            />
+                            <strong> Gasto atualizado. <span
+                                className='clique-aqui-atualizada'>{TEMPERMISSAO && 'Clique para conciliar'}</span></strong>
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <span>
+                            <FontAwesomeIcon
+                                style={{fontSize: '16px', color: '#00585E'}}
+                                icon={faCheckCircle}
+                            />
+                            <strong> Gasto atualizado.</strong>
+                        </span>
+                    </>
+                )
+            )}
         </>
     )
 }
