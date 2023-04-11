13b7cfe398ef57aef82e4f024647892081dc332d
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-04-11 08:28:12 -0300

    feat(89377): Exibe ícone de considerados corretos automaticamente
    Na análise de lançamentos de PC, caso haja solicitações de ajuste em
    lançamentos que foram consideradas corretas automaticamente na cópia da
    análise anterior, é exibido um ícone na coluna "conferido" indicando
    essa condição.

--- src/hooks/dres/PrestacaoDeContas/ConferenciaDeLancamentos/useConferidoTemplate.js
+++ src/hooks/dres/PrestacaoDeContas/ConferenciaDeLancamentos/useConferidoTemplate.js
@@ -1,7 +1,12 @@
 import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCheckCircle} from "@fortawesome/free-solid-svg-icons";
+import {faCheckCircle, faClipboardCheck} from "@fortawesome/free-solid-svg-icons";
+import IconeConferidoAutomaticamente from "../../../../assets/img/icone-conferido-automaticame.svg";
 
+const iconeConferidoAutomaticamente = <img
+    src={IconeConferidoAutomaticamente}
+    alt="Conferido automaticamente"
+/>
 function useConferidoTemplate (){
 
     function retornaConferidoTemplate (rowData=null, column=null) {
@@ -12,6 +17,7 @@ function useConferidoTemplate (){
                         style={{marginRight: "3px", color: '#297805'}}
                         icon={faCheckCircle}
                     />
+                    {rowData[column.field]['houve_considerados_corretos_automaticamente'] === true && iconeConferidoAutomaticamente}
                 </div>
             )
         } else if (rowData[column.field] && rowData[column.field]['resultado'] && rowData[column.field]['resultado'] === 'AJUSTE') {
@@ -21,6 +27,7 @@ function useConferidoTemplate (){
                         style={{marginRight: "3px", color: '#B40C02'}}
                         icon={faCheckCircle}
                     />
+                    {rowData[column.field]['houve_considerados_corretos_automaticamente'] === true && iconeConferidoAutomaticamente}
                 </div>
             )
         } else {
