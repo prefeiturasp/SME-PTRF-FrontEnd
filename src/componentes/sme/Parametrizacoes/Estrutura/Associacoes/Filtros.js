6cbfaee19885059c913d4af6b1fd3d7e9ff72a35
Lucas Santos Rocha <lucas.srocha@outlook.com>
2023-05-15 09:53:05 -0300

    feat(92325): Exibir destaque associacao encerrada em parametrizacoes
    Agora é exibido um destaque na lista de associacoes quando uma associação está encerrada

--- src/componentes/sme/Parametrizacoes/Estrutura/Associacoes/Filtros.js
+++ src/componentes/sme/Parametrizacoes/Estrutura/Associacoes/Filtros.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { Select } from 'antd';
 
-export const Filtros = ({stateFiltros, handleChangeFiltros, handleSubmitFiltros, limpaFiltros, tabelaAssociacoes}) =>{
+
+export const Filtros = ({stateFiltros, handleChangeFiltros, handleSubmitFiltros, limpaFiltros, tabelaAssociacoes, handleOnChangeMultipleSelectStatus}) =>{
+    const { Option } = Select;
+
     return(
         <>
             <form>
@@ -48,6 +52,28 @@ export const Filtros = ({stateFiltros, handleChangeFiltros, handleSubmitFiltros,
                         </select>
                     </div>
                 </div>
+
+                <div className="form-row">
+                    <div className="form-group col-md-3">
+                        <label htmlFor="filtrar_por_informacoes">Filtrar por informações</label>
+                        <Select
+                            mode="multiple"
+                            allowClear
+                            style={{ width: '100%' }}
+                            placeholder="Selecione o status"
+                            name="filtrar_por_informacao"
+                            id="filtrar_por_informacao"
+                            value={stateFiltros.filtrar_por_informacao}
+                            onChange={handleOnChangeMultipleSelectStatus}
+                            className='multiselect-lista-valores-reprogramados'
+                        >
+                            {tabelaAssociacoes && tabelaAssociacoes.filtro_informacoes && tabelaAssociacoes.filtro_informacoes.length > 0 && tabelaAssociacoes.filtro_informacoes.map(item => (
+                                <Option key={item.id} value={item.id}>{item.nome}</Option>
+                            ))}
+                        </Select>
+                    </div>
+                </div>
+
                 <div className="d-flex  justify-content-end mt-n2">
                     <button onClick={()=>limpaFiltros()} type="button" className="btn btn btn-outline-success mt-2 mr-2">Limpar</button>
                     <button onClick={handleSubmitFiltros} type="button" className="btn btn-success mt-2">Filtrar</button>
