666b613da8d02a2e860fb6ad46285aabb518c99d
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-07-10 15:00:24 -0300

    feat(85375): Instala o ReactQueryDevtools
    O Devtools será exibido caso esteja presente a variável de
    ambiente REACT_APP_REACT_QUERY_DEV_TOOLS="true" no .env

--- src/index.js
+++ src/index.js
@@ -12,11 +12,19 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import {store} from "./store";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 if (process.env.NODE_ENV === "production") {
     const SENTRY_URL = "SENTRY_URL_REPLACE_ME";
     Sentry.init({dsn: SENTRY_URL});
 }
+
+let REACT_QUERY_DEV_TOOLS = "REACT_QUERY_DEV_TOOLS_REPLACE_ME";
+
+if (process.env.REACT_APP_NODE_ENV === "local") {
+  REACT_QUERY_DEV_TOOLS = process.env.REACT_APP_REACT_QUERY_DEV_TOOLS;
+}
+
 
 const queryClient = new QueryClient();
 
@@ -36,7 +44,8 @@ ReactDOM.render(
                     </DespesaContextProvider>
                 </CentralDeDownloadContextProvider>
             </NotificacaoContextProvider>
-        </QueryClientProvider>
+            {REACT_QUERY_DEV_TOOLS === "true" && <ReactQueryDevtools initialIsOpen={false} />}
+            </QueryClientProvider>
     </Provider>
 
     , document.getElementById("root"));
