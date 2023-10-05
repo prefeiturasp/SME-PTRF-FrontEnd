3087101902295061a4743e5d159ad5d4ef129b38
ollyvergithub <ollyverottoboni@gmail.com>
2023-10-05 11:31:22 -0300

    feat(99165): Implementa mandatos anteriores em dados da associação

--- src/componentes/escolas/MembrosDaAssociacao/index.js
+++ src/componentes/escolas/MembrosDaAssociacao/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useCallback, useState} from "react";
 import {UrlsMenuInterno} from "../Associacao/UrlsMenuInterno";
 import {MenuInterno} from "../../Globais/MenuInterno";
 import {PaginaMandatoVigente} from "./pages/PaginaMandatoVigente";
@@ -8,6 +8,16 @@ import {MembrosDaAssociacaoProvider} from "./context/MembrosDaAssociacao";
 import "./membros-da-associacao.scss"
 
 export const MembrosDaAssociacao = () => {
+
+    const [isActiveMandatoVigente, setIsActiveMandatoVigente] = useState(true)
+    const [isActiveMandatosAnteriores, setIsActiveMandatosAnteriores] = useState(false)
+
+    // Faz o controle do carregamento dos componentes, evitando conflito na exibição das Composições,
+    const isActive = useCallback(()=>{
+        setIsActiveMandatoVigente(prevState => !prevState)
+        setIsActiveMandatosAnteriores(prevState => !prevState)
+    }, [])
+
     return (
         <MembrosDaAssociacaoProvider>
             <MenuInterno
@@ -15,17 +25,47 @@ export const MembrosDaAssociacao = () => {
             />
             <ExportaDadosDaAsssociacao/>
             <nav>
-                <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                    <button className="nav-link active" id="nav-mandato-vigente-tab" data-toggle="tab" data-target="#nav-mandato-vigente" type="button" role="tab" aria-controls="nav-mandato-vigente" aria-selected="true">Mandato vigente</button>
-                    <button className="nav-link" id="nav-mandatos-anteriores-tab" data-toggle="tab" data-target="#nav-mandatos-anteriores" type="button" role="tab" aria-controls="nav-mandatos-anteriores" aria-selected="false">Mandatos anteriores</button>
+                <div className="nav nav-tabs nav-mandatos" id="nav-tab" role="tablist">
+                    <button
+                        disabled={isActiveMandatoVigente}
+                        onClick={isActive}
+                        className={`nav-link ${isActiveMandatoVigente && 'active'}`}
+                        id="nav-mandato-vigente-tab"
+                        data-toggle="tab"
+                        data-target="#nav-mandato-vigente"
+                        type="button"
+                        role="tab"
+                        aria-controls="nav-mandato-vigente"
+                        aria-selected="true"
+                    >
+                        Mandato vigente
+                    </button>
+                    <button
+                        disabled={isActiveMandatosAnteriores}
+                        onClick={isActive}
+                        className={`nav-link ${isActiveMandatosAnteriores && 'active'}`}
+                        id="nav-mandatos-anteriores-tab"
+                        data-toggle="tab"
+                        data-target="#nav-mandatos-anteriores"
+                        type="button"
+                        role="tab"
+                        aria-controls="nav-mandatos-anteriores"
+                        aria-selected="false"
+                    >
+                        Mandatos anteriores
+                    </button>
                 </div>
             </nav>
-            <div className="tab-content" id="nav-tabContent">
+            <div className="tab-membros-associacao tab-content" id="nav-tabContent">
                 <div className="tab-pane fade show active" id="nav-mandato-vigente" role="tabpanel" aria-labelledby="nav-mandato-vigente-tab">
-                    <PaginaMandatoVigente/>
+                    {isActiveMandatoVigente &&
+                        <PaginaMandatoVigente/>
+                    }
                 </div>
                 <div className="tab-pane fade" id="nav-mandatos-anteriores" role="tabpanel" aria-labelledby="nav-mandatos-anteriores-tab">
-                    <PaginaMandatosAnteriores/>
+                    {isActiveMandatosAnteriores &&
+                        <PaginaMandatosAnteriores/>
+                    }
                 </div>
             </div>
         </MembrosDaAssociacaoProvider>
