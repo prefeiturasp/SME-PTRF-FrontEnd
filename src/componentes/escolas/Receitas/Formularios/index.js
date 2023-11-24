9d2fee092aa6028788a10d8d2536e180766adf69
Rayane Santos <rayanemsantos.contato@gmail.com>
2023-11-24 11:21:27 -0300

    fix(108891): Ajusta regra para retornar conta encerrada no fluxo de analise dre

--- src/componentes/escolas/Receitas/Formularios/index.js
+++ src/componentes/escolas/Receitas/Formularios/index.js
@@ -912,13 +912,7 @@ export const ReceitaForm = () => {
 
 
     const retornaTiposDeContas = (values) => {
-        if (tabelas.contas_associacao !== undefined && tabelas.contas_associacao.length > 0  && values.tipo_receita && e_repasse(values) && Object.keys(repasse).length !== 0) {
-            let conta_associacao = tabelas.contas_associacao.find(conta => (repasse.conta_associacao.nome.includes(conta.nome)));
-            setreadOnlyContaAssociacaoReceita(true);
-            return (
-                <option key={conta_associacao.uuid} value={conta_associacao.uuid}>{conta_associacao.nome}</option>
-            )
-        } else if (tabelas.contas_associacao !== undefined && tabelas.contas_associacao.length > 0  && values.tipo_receita) {
+        if (tabelas.contas_associacao !== undefined && tabelas.contas_associacao.length > 0  && values.tipo_receita) {
             const tipoReceita = tabelas.tipos_receita.find(element => element.id === Number(values.tipo_receita));
 
             // Lista dos nomes dos tipos de conta que são aceitos pelo tipo de receita selecionado.
@@ -963,7 +957,7 @@ export const ReceitaForm = () => {
 
             let dataDoCredito = moment(values.data).format('DD/MM/YYYY')
             const contasFiltradasExcluindoContasComEncerramentoAprovado = contasFiltradasPelaDataInicialEPeloTipo.filter((elemento) => {
-                return !( elemento.status === STATUS_CONTA_ASSOCIACAO.INATIVA && retornaSeDataDoCreditoEhPosteriorDataEncerramento(dataDoCredito, moment(elemento.solicitacao_encerramento.data_de_encerramento_na_agencia).format('DD/MM/YYYY')) )
+                return !(!origemAnaliseLancamento() && elemento.status === STATUS_CONTA_ASSOCIACAO.INATIVA && retornaSeDataDoCreditoEhPosteriorDataEncerramento(dataDoCredito, moment(elemento.solicitacao_encerramento.data_de_encerramento_na_agencia).format('DD/MM/YYYY')) )
             })
 
             if(!contasFiltradasExcluindoContasComEncerramentoAprovado.length && moment(values.data, 'YYYY-MM-DD').isValid()) {
