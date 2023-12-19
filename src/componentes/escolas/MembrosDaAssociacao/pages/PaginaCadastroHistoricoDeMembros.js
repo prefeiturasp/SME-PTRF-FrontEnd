7fb903e410acf0bf73216b312f5139406127d3c2
Rayane Santos <rayanemsantos.contato@gmail.com>
2023-12-19 18:31:54 -0300

    feat(105420): Implementa checagem mudança data fim ocupação para sugerir incluir novo membro

--- src/componentes/escolas/MembrosDaAssociacao/pages/PaginaCadastroHistoricoDeMembros.js
+++ src/componentes/escolas/MembrosDaAssociacao/pages/PaginaCadastroHistoricoDeMembros.js
@@ -1,6 +1,9 @@
 import React, {useCallback, useEffect, useState} from "react";
+import {useDispatch} from "react-redux";
 import {PaginasContainer} from "../../../../paginas/PaginasContainer";
 import {useLocation, useParams} from "react-router-dom";
+// Preparação para react-router-dom-v6
+import {useNavigate} from "react-router-dom-v5-compat";
 import {FormCadastro} from "../components/FormCadastro";
 import {usePostCargoDaComposicao} from "../hooks/usePostCargoDaComposicao";
 import {useGetCargosDiretoriaExecutiva} from "../hooks/useGetCargosDiretoriaExecutiva";
@@ -10,12 +13,43 @@ import {getStatusPresidenteAssociacao} from "../../../../services/escolas/Associ
 
 import {ASSOCIACAO_UUID} from "../../../../services/auth.service";
 import {usePatchStatusPresidente} from "../hooks/usePatchStatusPresidente";
+import { ModalConfirm } from "../../../Globais/Modal/ModalConfirm";
 
+const CargoComposicaoModel = {
+    "id": null,
+    "uuid": null,
+    "ocupante_do_cargo": {
+        "id": null,
+        "uuid": null,
+        "nome": null,
+        "codigo_identificacao": null,
+        "cargo_educacao": null,
+        "representacao": "",
+        "representacao_label": "",
+        "email": null,
+        "cpf_responsavel": null,
+        "telefone": null,
+        "cep": null,
+        "bairro": null,
+        "endereco": null
+    },
+    "cargo_associacao": null,
+    "cargo_associacao_label": null,
+    "data_inicio_no_cargo": null,
+    "data_fim_no_cargo": null,
+    "eh_composicao_vigente": true,
+    "substituto": null,
+    "substituido": null,
+    "ocupante_editavel": true,
+    "data_final_editavel": false
+}
 export const PaginaCadastroHistoricoDeMembros = () => {
-
+    const dispatch = useDispatch();
     let uuid_associacao = localStorage.getItem(ASSOCIACAO_UUID);
     let {composicaoUuid} = useParams();
     const { state } = useLocation();
+    const navigate = useNavigate();
+
     const {mutationPostCargoDaComposicao} = usePostCargoDaComposicao()
     const {mutationPutCargoDaComposicao} = usePutCargoDaComposicao()
     const {mutationPatchStatusPresidenteAssociacao} = usePatchStatusPresidente()
@@ -55,6 +89,29 @@ export const PaginaCadastroHistoricoDeMembros = () => {
         carregaCargosDaDiretoriaExecutiva().then()
     }, [carregaCargosDaDiretoriaExecutiva])
 
+    const perguntarIncluirNovoMembro = async (payload, cargoVazio) => {
+        ModalConfirm({
+            dispatch,
+            title: 'Importante',
+            message: 'Deseja incluir um novo membro para o cargo?',
+            cancelText: 'Não incluir',
+            confirmText: 'Incluir novo membro',
+            dataQa: 'modal-sugerir-incluir-novo-membro',
+            onConfirm: async () => {
+                const mutationResp = await mutationPutCargoDaComposicao.mutateAsync({uuidCargoComposicao: state.cargo.uuid, payload: payload});
+                if (mutationResp.data.composicao_vigente) {
+                    navigate(`/cadastro-historico-de-membros/${mutationResp.data.composicao_vigente}`, {
+                        state: {cargo: cargoVazio}
+                    });
+                }                        
+            }, 
+            onCancel: async () => {
+                await mutationPutCargoDaComposicao.mutateAsync({uuidCargoComposicao: state.cargo.uuid, payload: payload});
+                navigate("/membros-da-associacao");
+            }               
+        })
+    };
+
 
     const onSubmitForm = async (values) => {
         let payload = {
@@ -81,7 +138,15 @@ export const PaginaCadastroHistoricoDeMembros = () => {
         if (!state.cargo.uuid){
             mutationPostCargoDaComposicao.mutate({payload: payload})
         }else {
-            mutationPutCargoDaComposicao.mutate({uuidCargoComposicao: state.cargo.uuid, payload: payload})
+            if(values.data_fim_no_cargo !== state.cargo.data_fim_no_cargo){
+                let cargoVazio = {...CargoComposicaoModel};  
+                cargoVazio.cargo_associacao = values.cargo_associacao
+                cargoVazio.cargo_associacao_label = values.cargo_associacao_label;
+                perguntarIncluirNovoMembro(payload, cargoVazio);
+            } else {
+                await mutationPutCargoDaComposicao.mutateAsync({uuidCargoComposicao: state.cargo.uuid, payload: payload});
+                navigate("/membros-da-associacao");
+            }
         }
 
         let payloadStatusPresidente;
