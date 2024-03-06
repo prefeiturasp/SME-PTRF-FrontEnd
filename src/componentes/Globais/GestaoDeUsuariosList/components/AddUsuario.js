b3d62c6101e5fe81f29bbeaf88f689d9f6d5d999
Lucas Santos Rocha <lucas.srocha@outlook.com>
2024-03-06 07:40:00 -0300

    fix(116606): Corrige exibição grupo acesso
    Agora ao entrar em adicionar usuário não ira mais pegar o cash de outro usuario

--- src/componentes/Globais/GestaoDeUsuariosList/components/AddUsuario.js
+++ src/componentes/Globais/GestaoDeUsuariosList/components/AddUsuario.js
@@ -2,11 +2,20 @@ import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPlus} from "@fortawesome/free-solid-svg-icons";
 import {useHistory} from "react-router-dom";
+import {useQueryClient} from '@tanstack/react-query';
+
 export const AddUsuario = () => {
     let history = useHistory();
+    const queryClient = useQueryClient();
+    
+    const removeQuery = () => {
+        queryClient.removeQueries('grupos-disponiveis-acesso-usuario')
+    }
+
     return (
         <button onClick={() => {
             history.push('/gestao-de-usuarios-form')
+            removeQuery()
         }
         } type="button" className="btn btn-success mt-2">
             <FontAwesomeIcon
