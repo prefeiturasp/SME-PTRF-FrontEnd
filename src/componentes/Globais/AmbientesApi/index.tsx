02f73816bdf65bc17c1f9d4c2ceb79d66cfcdc5f
ollyvergithub <ollyverottoboni@gmail.com>
2024-04-19 12:33:22 -0300

    feat(113097): Converte componentes Ambientes e AmbientesApi em Typescript

--- src/componentes/Globais/AmbientesApi/index.tsx
+++ src/componentes/Globais/AmbientesApi/index.tsx
@@ -1,22 +1,22 @@
 import React, {useCallback, useEffect, useState} from "react";
 
-export const AmbientesApi = () =>{
+export const AmbientesApi: React.FC = () => {
 
-    let url = window.location.href
-    let prefixo_url = url.split('/')[2].split('.', 1)[0]
+    const url: string = window.location.href
+    const prefixo_url: string = url.split('/')[2].split('.', 1)[0]
 
-    const [ambienteApi, setAmbienteApi] = useState('')
+    const [ambienteApi, setAmbienteApi] = useState<string>('')
 
-    const carregaAmbienteApi = useCallback( ()=>{
-        let API_URL = "API_URL_REPLACE_ME";
-
-        if (process.env.REACT_APP_NODE_ENV === "local") {
-            API_URL = process.env.REACT_APP_API_URL;
+    const carregaAmbienteApi = useCallback(()=>{
+        let API_URL: string = "API_URL_REPLACE_ME"
+        if (process.env.REACT_APP_NODE_ENV === "local"){
+            API_URL = process.env.REACT_APP_API_URL || "";
         }
 
         if (API_URL.split('/')[2].split('.', 1)[0] !== prefixo_url){
             setAmbienteApi(API_URL.split('/')[2].split('.', 1)[0])
         }
+
     }, [prefixo_url])
 
     useEffect(()=>{
@@ -29,6 +29,5 @@ export const AmbientesApi = () =>{
                 <span>{`API: ${ambienteApi}`}</span>
             }
         </>
-
     )
 }
