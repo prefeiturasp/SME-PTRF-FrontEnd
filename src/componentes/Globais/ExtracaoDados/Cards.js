4e7536fa3417225d34a90ea09f84d53075e26dd2
Alessandro Fernandes <alcfernandes@yahoo.com>
2024-05-07 06:49:33 -0300

    feat(118388): Add extração de despesas em DRE
    Implementa a extração de despesas também para a
    visão de DRE.

--- src/componentes/Globais/ExtracaoDados/Cards.js
+++ src/componentes/Globais/ExtracaoDados/Cards.js
@@ -73,7 +73,7 @@ export const cards = [
         descricao: 'Arquivo com informações de despesas por período e unidade.',
         tags: ['CSV'],
         endpoint: '/api/exportacoes-dados/documentos-despesas/',
-        visao: ['SME']
+        visao: ['DRE', 'SME']
     },
     {
         titulo: 'Dados das contas',
