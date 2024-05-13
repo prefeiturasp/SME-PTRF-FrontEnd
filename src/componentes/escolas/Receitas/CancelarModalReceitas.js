a6928bcef9ec9558555334d6e3b3ca728f453f03
Matheus <matheus.diori@amcom.com.br>
2024-05-13 16:17:04 -0300

    feat(120457): Altera modal cancelar estorno

--- src/componentes/escolas/Receitas/CancelarModalReceitas.js
+++ src/componentes/escolas/Receitas/CancelarModalReceitas.js
@@ -1,17 +1,35 @@
 import React from "react";
-import {ModalBootstrap} from "../../Globais/ModalBootstrap";
+import { Modal } from 'antd';
+import IconeAvisoVermelho from "../../../assets/img/icone-modal-aviso-vermelho.svg"
 
 export const CancelarModalReceitas = (propriedades) =>{
     return (
-        <ModalBootstrap
-            show={propriedades.show}
-            onHide={propriedades.handleClose}
-            titulo={propriedades.titulo}
-            bodyText=""
-            primeiroBotaoOnclick={propriedades.onCancelarTrue}
-            primeiroBotaoTexto="OK"
-            segundoBotaoOnclick={propriedades.handleClose}
-            segundoBotaoTexto="Fechar"
-        />
+        <Modal 
+            open={propriedades.show}
+            onOk={propriedades.onCancelarTrue}
+            okText="OK"
+            okButtonProps={{className: "btn-base-vermelho"}}
+            onCancel={propriedades.handleClose}
+            cancelText="Fechar"
+            cancelButtonProps={{className: "btn-base-verde-outline"}}
+            wrapClassName={'modal-ant-design'}
+        >
+            <div className="row">
+                <div className="col-md-auto col-lg-12">
+                    <div className="text-center">
+                        <img src={IconeAvisoVermelho} alt="" className="img-fluid"/>
+                    </div>
+                </div>
+
+                <div className="col-md-auto col-lg-12 mt-3">
+                    <div className="text-center">
+                        <p className="title-modal-antdesign-aviso">{propriedades.titulo}</p>
+                    </div>
+                    <div className="text-center mt-2">
+                        <p className="body-text-modal-antdesign-aviso">{propriedades.bodyText}</p>
+                    </div>
+                </div>
+            </div>
+        </Modal>
     )
 };
