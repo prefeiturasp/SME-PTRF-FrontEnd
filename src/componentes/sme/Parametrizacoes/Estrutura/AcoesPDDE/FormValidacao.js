1ec95efb768c2331eca333814d7d6d6eaad202dc
Vitor Campos <vitor.campos@spassu.com.br>
2025-03-25 10:37:40 -0300

    feature(126749): Ajustes no layout e lógica de exibição de mensagens de sucesso e erro na criação e/ou edição de ação e categoria

--- src/componentes/sme/Parametrizacoes/Estrutura/AcoesPDDE/FormValidacao.js
+++ src/componentes/sme/Parametrizacoes/Estrutura/AcoesPDDE/FormValidacao.js
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 export const FormAcoesPDDEValidacao = yup.object().shape({
       nome: yup.string().required("Nome é obrigatório"),
-      categoria: yup.string().test('test-name', 'Categoria é obrigatória',
+      categoria: yup.string().test('test-name', 'Programa é obrigatório',
         function (value) {
           return !(value === undefined || value === null || value === "");
       }),
