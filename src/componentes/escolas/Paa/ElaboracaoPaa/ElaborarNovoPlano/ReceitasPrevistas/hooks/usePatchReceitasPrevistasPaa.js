90ce1e128d07425efa1a9c11ee691b40e6bed75f
Rayane Santos <rayanemsantos.contato@gmail.com>
2025-03-26 18:23:03 -0300

    feat(124771): Atualiza tabela no post/patch

--- src/componentes/escolas/Paa/ElaboracaoPaa/ElaborarNovoPlano/ReceitasPrevistas/hooks/usePatchReceitasPrevistasPaa.js
+++ src/componentes/escolas/Paa/ElaboracaoPaa/ElaborarNovoPlano/ReceitasPrevistas/hooks/usePatchReceitasPrevistasPaa.js
@@ -1,12 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { patchReceitasPrevistasPaa } from "../../../../../../../services/escolas/Paa.service";
 import { toastCustom } from "../../../../../../Globais/ToastCustom";
 
 export const usePatchReceitasPrevistasPaa = (onClose) => {
+  const queryClient = useQueryClient();
+
   const mutationPatch = useMutation({
     mutationFn: ({ uuid, payload }) => patchReceitasPrevistasPaa(uuid, payload),
     onSuccess: (data) => {
       toastCustom.ToastCustomSuccess("Recurso editado com sucesso.");
+      queryClient.invalidateQueries(["acoes-associacao"]);
       onClose && onClose();
     },
     onError: (e) => {
--- src/componentes/escolas/Paa/ElaboracaoPaa/ElaborarNovoPlano/ReceitasPrevistas/hooks/usePostReceitasPrevistasPaa.js
+++ src/componentes/escolas/Paa/ElaboracaoPaa/ElaborarNovoPlano/ReceitasPrevistas/hooks/usePostReceitasPrevistasPaa.js
@@ -1,12 +1,14 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { postReceitasPrevistasPaa } from "../../../../../../../services/escolas/Paa.service";
 import { toastCustom } from "../../../../../../Globais/ToastCustom";
 
 export const usePostReceitasPrevistasPaa = (onClose) => {
+  const queryClient = useQueryClient();
   const mutationPost = useMutation({
     mutationFn: ({ payload }) => postReceitasPrevistasPaa(payload),
     onSuccess: (data) => {
       toastCustom.ToastCustomSuccess("Recurso criado com sucesso.");
+      queryClient.invalidateQueries(["acoes-associacao"]);
       onClose && onClose();
     },
     onError: (e) => {
