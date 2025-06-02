882a85cc09740df0a6789d35d97956c72bf44c82
Agostinho <jr.jardimmatos@gmail.com>
2025-06-02 09:19:04 -0300

    test(128363): Cobertura de testes frontend

---
+++ src/componentes/Globais/CardsDevolucoesParaAcertoDaDre/__tests__/index.test.js
@@ -0,0 +1,142 @@
+import { screen, render, waitFor, fireEvent } from '@testing-library/react'
+import { getAnalisesDePcDevolvidas } from '../../../../services/dres/PrestacaoDeContas.service'
+import CardsDevolucoesParaAcertoDaDre from '../index'
+import { mantemEstadoAnaliseDre } from "../../../../services/mantemEstadoAnaliseDre.service";
+
+import {exibeDataPT_BR} from "../../../../utils/ValidacoesAdicionaisFormularios";
+
+require("ordinal-pt-br");
+
+jest.mock("../../../../utils/ValidacoesAdicionaisFormularios", ()=>({
+    exibeDataPT_BR: jest.fn()
+}));
+
+jest.mock('../../../../services/mantemEstadoAnaliseDre.service', ()=> ({
+    mantemEstadoAnaliseDre: {
+        getAnaliseDreUsuarioLogado: jest.fn(),
+        limpaAnaliseDreUsuarioLogado: jest.fn(),
+        setAnaliseDrePorUsuario: jest.fn()
+    }
+}));
+
+jest.mock('../../../../services/dres/PrestacaoDeContas.service', ()=> ({
+    getAnalisesDePcDevolvidas: jest.fn()
+}))
+
+const mockData = {
+    prestacao_conta_uuid: '1234', 
+    analiseAtualUuid: false, 
+    setAnaliseAtualUuid: jest.fn(), 
+    setPermitirTriggerOnclick: null,
+    devolucao_atual: {
+        uuid: '987654'
+    },
+    setDevolucaoAtualSelecionada: null
+}
+
+const mockAnalises = [
+    {
+        devolucao_prestacao_conta: {
+            data: '2025-01-01'
+        }
+    },
+    {
+        devolucao_prestacao_conta: {
+            data: '2025-01-01'
+        }
+    },
+    {
+        devolucao_prestacao_conta: {
+            data: '2025-01-01'
+        }
+    },
+    {
+        devolucao_prestacao_conta: {
+            data: '2025-01-01'
+        }
+    },
+    {
+        devolucao_prestacao_conta: {
+            data: '2025-01-01'
+        }
+    },
+    {
+        devolucao_prestacao_conta: {
+            data: '2025-01-01'
+        }
+    },
+    {
+        devolucao_prestacao_conta: {
+            data: '2025-01-01'
+        }
+    },
+    {
+        devolucao_prestacao_conta: {
+            data: '2025-01-01'
+        }
+    },
+    {
+        devolucao_prestacao_conta: {
+            data: '2025-01-01'
+        }
+    },
+    {
+        devolucao_prestacao_conta: {
+            data: '2025-01-01'
+        }
+    },
+]
+
+const mockUsuarioLogado = {
+    analise_pc_uuid: '123456789',
+    
+}
+
+describe('CardsDevolucoesParaAcertoDaDre', ()=>{
+
+    test('Renderiza componente', async ()=>{
+        await waitFor(()=>{
+            getAnalisesDePcDevolvidas.mockReturnValue(mockAnalises)
+            mantemEstadoAnaliseDre.getAnaliseDreUsuarioLogado.mockReturnValue(mockUsuarioLogado)
+
+            render(<CardsDevolucoesParaAcertoDaDre {...mockData}/>)
+        })
+        await waitFor(()=>{
+            const campo = screen.getByTestId('select-filtro-ajuste')
+            expect(campo).toBeInTheDocument()
+            expect(getAnalisesDePcDevolvidas).toHaveBeenCalled()
+            expect(mantemEstadoAnaliseDre.getAnaliseDreUsuarioLogado).toHaveBeenCalled()
+        })
+    })
+
+    test('Renderiza componente', async ()=>{
+        await waitFor(() => {
+            getAnalisesDePcDevolvidas.mockReturnValue([])
+            mantemEstadoAnaliseDre.getAnaliseDreUsuarioLogado.mockReturnValue(mockUsuarioLogado)
+
+            render(<CardsDevolucoesParaAcertoDaDre {...mockData}/>)
+        })
+        await waitFor(() => {
+            const campo = screen.getByTestId('select-filtro-ajuste')
+            expect(campo).toBeInTheDocument()
+            expect(getAnalisesDePcDevolvidas).toHaveBeenCalled()
+            expect(mantemEstadoAnaliseDre.getAnaliseDreUsuarioLogado).toHaveBeenCalled()
+        })
+    })
+
+    test('Renderiza componente', async ()=>{
+        await waitFor(() => {
+            getAnalisesDePcDevolvidas.mockReturnValue(mockAnalises)
+            mantemEstadoAnaliseDre.getAnaliseDreUsuarioLogado.mockReturnValue(mockUsuarioLogado)
+
+            render(<CardsDevolucoesParaAcertoDaDre {...mockData}/>)
+        })
+        await waitFor(() => {
+            const campo = screen.getByTestId('select-filtro-ajuste')
+            console.log(campo.innerHTML)
+            expect(campo).toBeInTheDocument()
+            fireEvent.change(campo, {target: {value: '5678', selectedIndex: 0}})
+            
+        })
+    })
+})
--- src/componentes/Globais/CentralDeNotificacoes/__fixtures__/mockData.js
+++ src/componentes/Globais/CentralDeNotificacoes/__fixtures__/mockData.js
@@ -1,18 +1,130 @@
 export const mockNotificacoes = [
-    {infos: [{uuid: '0001', tipo: 'tipo 1', lido: true}]},
-    {infos: [{uuid: '0002', tipo: 'tipo 2', lido: false}]},
-    {infos: [{uuid: '0003', tipo: 'tipo 3', lido: true}]},
-    {infos: [{uuid: '0004', tipo: 'tipo 4', lido: true}]},
-    {infos: [{uuid: '0005', tipo: 'tipo 5', lido: true}]},
-    {infos: [{uuid: '0006', tipo: 'tipo 6', lido: true}]},
-    {infos: [{uuid: '0007', tipo: 'tipo 7', lido: true}]},
-    {infos: [{uuid: '0008', tipo: 'tipo 8', lido: true}]},
-    {infos: [{uuid: '0009', tipo: 'tipo 9', lido: true}]},
-    {infos: [{uuid: '00010', tipo: 'tipo 10', lido: true}]},
-    {infos: [{uuid: '00011', tipo: 'tipo 11', lido: true}]},
+    {
+        data: "Segunda 09 Dez. 2024",
+        infos: [
+            {
+                uuid: "5f7892eb-9d0a-4c81-871a-37eca17863fe",
+                titulo: "Ajustes necessários na PC | Prazo: 09/12/2024",
+                descricao: "A DRE solicitou alguns ajustes em sua prestação de contas do período 2023.2. O seu prazo para envio das mudanças é 09/12/2024",
+                lido: false,
+                hora: "15:25",
+                tipo: "Alerta",
+                remetente: "DRE",
+                categoria: "Devolução de PC para ajustes",
+                unidade: "cd6572b3-03cb-41c4-9cf3-39dcdad09543",
+                periodo: null
+            },
+            {
+                uuid: "02a25c89-2d95-42a6-9d24-827257d9f51b",
+                titulo: "Ajustes necessários na PC | Prazo: 10/12/2024",
+                descricao: "A DRE solicitou alguns ajustes em sua prestação de contas do período 2023.2. O seu prazo para envio das mudanças é 10/12/2024",
+                lido: true,
+                hora: "15:13",
+                tipo: "Alerta",
+                remetente: "DRE",
+                categoria: "Devolução de PC para ajustes",
+                unidade: "cd6572b3-03cb-41c4-9cf3-39dcdad09543",
+                periodo: null
+            }
+        ]
+    }
 ]
 
 export const mockTiposNotificacoes = [
-    {id: 1, nome: 'Tipo 1'},
-    {id: 2, nome: 'Tipo 2'}
+    {
+        "id": "ALERTA",
+        "nome": "Alerta"
+    },
+    {
+        "id": "INFORMACAO",
+        "nome": "Informação"
+    },
+    {
+        "id": "URGENTE",
+        "nome": "Urgente"
+    },
+    {
+        "id": "AVISO",
+        "nome": "Aviso"
+    }
 ]
+
+export const mockRemetentes = [
+    {
+        "id": "SISTEMA",
+        "nome": "Sistema"
+    },
+    {
+        "id": "DRE",
+        "nome": "DRE"
+    },
+    {
+        "id": "ASSOCIACAO",
+        "nome": "Associação"
+    },
+    {
+        "id": "SME",
+        "nome": "SME"
+    }
+]
+
+export const mockCategorias = [
+    {
+        "id": "COMENTARIO_PC",
+        "nome": "Comentário na prestação de contas"
+    },
+    {
+        "id": "ELABORACAO_PC",
+        "nome": "Elaboração de PC"
+    },
+    {
+        "id": "ANALISE_PC",
+        "nome": "Análise de PC"
+    },
+    {
+        "id": "DEVOLUCAO_PC",
+        "nome": "Devolução de PC para ajustes"
+    },
+    {
+        "id": "APROVACAO_PC",
+        "nome": "Aprovação de PC"
+    },
+    {
+        "id": "APROVACAO_RESSALVAS_PC",
+        "nome": "Aprovação de PC com ressalvas"
+    },
+    {
+        "id": "REPROVACAO_PC",
+        "nome": "Reprovação de PC"
+    },
+    {
+        "id": "ERRO_AO_CONCLUIR_PC",
+        "nome": "Erro ao concluir PC"
+    },
+    {
+        "id": "DEVOLUCAO_CONSOLIDADO",
+        "nome": "Devolução de relatório consolidado"
+    },
+    {
+        "id": "COMENTARIO_CONSOLIDADO_DRE",
+        "nome": "Comentário no relatório consolidado"
+    },
+    {
+        "id": "ENCERRAMENTO_CONTA_BANCARIA",
+        "nome": "Encerramento de Conta Bancária"
+    },
+    {
+        "id": "GERACAO_ATA",
+        "nome": "Geração de Ata"
+    },
+    {
+        "id": "CONCLUSAO_PC",
+        "nome": "Conclusão da PC"
+    }
+]
+
+export const mockTabelaNotificacoes = {
+    "tipos_notificacao": mockTiposNotificacoes,
+    "remetentes": mockRemetentes,
+    "categorias": mockCategorias
+}
