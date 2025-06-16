1dec8936b7b16577294e61df2fc4e30f6bdd3a5b
Rayane Santos <rayanemsantos.contato@gmail.com>
2025-06-16 19:16:02 -0300

    feat(127457): Implementa testes classificar bem e atualiza outros de situação patrimonial

---
+++ src/componentes/escolas/SituacaoPatrimonial/FormularioBemProduzido/__tests__/ClassificarBem/index.test.js
@@ -0,0 +1,263 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { ClassificarBem } from "../../ClassificarBem/index";
+
+const mockUseNavigate = jest.fn();
+const mockCadastrarBem = jest.fn();
+
+jest.mock("react-router-dom-v5-compat", () => ({
+  ...jest.requireActual("react-router-dom-v5-compat"),
+  useNavigate: () => mockUseNavigate,
+}));
+
+describe("ClassificarBem", () => {
+  beforeEach(() => {
+    window.matchMedia = jest.fn().mockImplementation((query) => ({
+      matches: false,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    }));
+  });
+
+  it("Deve chamar cadastrarBens ao clicar em Cadastrar bem", async () => {
+    render(
+      <MemoryRouter>
+        <ClassificarBem
+          items={[
+            {
+              num_processo_incorporacao: "1111111111111111",
+              especificacao_do_bem: "uuid-fake",
+              quantidade: 1,
+              valor_individual: 1000,
+            },
+          ]}
+          cadastrarBens={mockCadastrarBem}
+          salvarRascunhoClassificarBens={jest.fn()}
+          setBemProduzidoItems={jest.fn()}
+          setHabilitaCadastrarBem={jest.fn()}
+          habilitaCadastrarBem={true}
+          total={1000}
+        />
+      </MemoryRouter>
+    );
+
+    const buttonCadastrarBem = screen.getByRole("button", {
+      name: "Cadastrar bem",
+    });
+
+    fireEvent.click(buttonCadastrarBem);
+
+    await waitFor(() => {
+      expect(mockCadastrarBem).toHaveBeenCalled();
+    });
+  });
+
+  it("Deve desabilitar botão de cadastrar bem se valores inválidos", async () => {
+    render(
+      <MemoryRouter>
+        <ClassificarBem
+          items={[
+            {
+              num_processo_incorporacao: "",
+              especificacao_do_bem: "",
+              quantidade: "",
+              valor_individual: "",
+            },
+          ]}
+          cadastrarBens={jest.fn()}
+          salvarRascunhoClassificarBens={jest.fn()}
+          setBemProduzidoItems={jest.fn()}
+          setHabilitaCadastrarBem={jest.fn()}
+          habilitaCadastrarBem={false}
+          total={1000}
+        />
+      </MemoryRouter>
+    );
+
+    const buttonCadastrarBem = screen.getByRole("button", {
+      name: "Cadastrar bem",
+    });
+
+    expect(buttonCadastrarBem).toBeDisabled();
+  });
+
+  it("Deve adicionar formulário na tela quando clicar em adicionar item", async () => {
+    render(
+      <MemoryRouter>
+        <ClassificarBem
+          items={[
+            {
+              num_processo_incorporacao: "",
+              especificacao_do_bem: "",
+              quantidade: "",
+              valor_individual: "",
+            },
+          ]}
+          cadastrarBens={jest.fn()}
+          salvarRascunhoClassificarBens={jest.fn()}
+          setBemProduzidoItems={jest.fn()}
+          setHabilitaCadastrarBem={jest.fn()}
+          habilitaCadastrarBem={false}
+          total={1000}
+        />
+      </MemoryRouter>
+    );
+
+    const buttonAdicionarItem = screen.getByRole("button", {
+      name: "plus Adicionar item",
+    });
+    fireEvent.click(buttonAdicionarItem);
+
+    expect(screen.queryByText(/Item 2/i)).toBeInTheDocument();
+  });
+
+  it("Deve remover formulário da tela quando clicar em remover item", async () => {
+    render(
+      <MemoryRouter>
+        <ClassificarBem
+          items={[
+            {
+              num_processo_incorporacao: "",
+              especificacao_do_bem: "",
+              quantidade: "",
+              valor_individual: "",
+            },
+          ]}
+          cadastrarBens={jest.fn()}
+          salvarRascunhoClassificarBens={jest.fn()}
+          setBemProduzidoItems={jest.fn()}
+          setHabilitaCadastrarBem={jest.fn()}
+          habilitaCadastrarBem={false}
+          total={1000}
+        />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("Item 1")).toBeInTheDocument();
+
+    const buttonRemoverItem = screen.getByRole("button", {
+      name: "close-circle Remover item",
+    });
+    fireEvent.click(buttonRemoverItem);
+
+    expect(screen.queryByText(/Item 1/i)).not.toBeInTheDocument();
+  });
+
+  it("Deve mostrar erro de validação quando quantidade e/ou valor utilizado igual a zero", async () => {
+    render(
+      <MemoryRouter>
+        <ClassificarBem
+          items={[
+            {
+              num_processo_incorporacao: "",
+              especificacao_do_bem: "",
+              quantidade: "",
+              valor_individual: "",
+            },
+          ]}
+          cadastrarBens={jest.fn()}
+          salvarRascunhoClassificarBens={jest.fn()}
+          setBemProduzidoItems={jest.fn()}
+          setHabilitaCadastrarBem={jest.fn()}
+          habilitaCadastrarBem={false}
+          total={1000}
+        />
+      </MemoryRouter>
+    );
+
+    const inputQnt = screen.getByRole("spinbutton", {
+      name: /Quantidade/i,
+    });
+    userEvent.type(inputQnt, "0");
+
+    await waitFor(() => {
+      expect(
+        screen.getByText("O valor deve ser maior que 0")
+      ).toBeInTheDocument();
+    });
+
+    const inputValor = screen.getByRole("spinbutton", {
+      name: /Valor Individual/i,
+    });
+    userEvent.type(inputValor, "0");
+
+    await waitFor(() => {
+      expect(
+        screen.getByText("O valor deve ser maior que 0")
+      ).toBeInTheDocument();
+    });
+  });
+
+  it("Deve formatar o número de processo de incorporação ao digitar", async () => {
+    render(
+      <MemoryRouter>
+        <ClassificarBem
+          items={[
+            {
+              num_processo_incorporacao: "",
+              especificacao_do_bem: "",
+              quantidade: "",
+              valor_individual: "",
+            },
+          ]}
+          cadastrarBens={jest.fn()}
+          salvarRascunhoClassificarBens={jest.fn()}
+          setBemProduzidoItems={jest.fn()}
+          setHabilitaCadastrarBem={jest.fn()}
+          habilitaCadastrarBem={false}
+          total={1000}
+        />
+      </MemoryRouter>
+    );
+
+    const input = screen.getByRole("spinbutton", {
+      name: /Número do processo de incorporação/i,
+    });
+
+    userEvent.type(input, "1111111111111111");
+
+    await waitFor(() => {
+      expect(input).toHaveValue("1111.1111/1111111-1");
+    });
+  });
+
+  it("Deve mostrar o valor total dos bens produzidos", async () => {
+    render(
+      <MemoryRouter>
+        <ClassificarBem
+          items={[]}
+          cadastrarBens={jest.fn()}
+          salvarRascunhoClassificarBens={jest.fn()}
+          setBemProduzidoItems={jest.fn()}
+          setHabilitaCadastrarBem={jest.fn()}
+          habilitaCadastrarBem={false}
+          total={1000}
+        />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("R$ 1.000,00")).toBeInTheDocument();
+  });
+
+  it("Deve voltar para a página de listagem ao clicar no botão cancelar", async () => {
+    render(
+      <MemoryRouter>
+        <ClassificarBem
+          items={[]}
+          cadastrarBens={jest.fn()}
+          salvarRascunhoClassificarBens={jest.fn()}
+          setBemProduzidoItems={jest.fn()}
+          setHabilitaCadastrarBem={jest.fn()}
+          habilitaCadastrarBem={false}
+          total={0}
+        />
+      </MemoryRouter>
+    );
+
+    const buttonCancelar = screen.getByRole("button", { name: "Cancelar" });
+    fireEvent.click(buttonCancelar);
+
+    expect(mockUseNavigate).toHaveBeenCalledWith("/lista-situacao-patrimonial");
+  });
+});
--- src/componentes/escolas/SituacaoPatrimonial/FormularioBemProduzido/__tests__/index.test.js
+++ src/componentes/escolas/SituacaoPatrimonial/FormularioBemProduzido/__tests__/index.test.js
@@ -13,11 +13,16 @@ import { useParams } from "react-router-dom";
 import { combineReducers, createStore } from "redux";
 import { usePostBemProduzido } from "../hooks/usePostBemProduzido";
 import { usePatchBemProduzido } from "../hooks/usePatchBemProduzido";
+import { usePatchBemProduzidoItemsRascunho } from "../ClassificarBem/hooks/usePatchBemProduzidoItemsRascunho";
+import { usePatchBemProduzidoItems } from "../ClassificarBem/hooks/usePatchBemProduzidoItems";
 
 const mockUseNavigate = jest.fn();
+const mockSearchParams = new URLSearchParams("?step=1");
+const mockSetSearchParams = jest.fn();
 const mockMutatePostAsync = jest.fn();
 const mockMutatePatchAsync = jest.fn();
-const mockInformarValores = jest.fn();
+const mockMutationPatchBemProduzidoItemsRascunhoAsync = jest.fn();
+const mockMutationPatchBemProduzidoItemsAsync = jest.fn();
 
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
@@ -27,6 +32,7 @@ jest.mock("react-router-dom", () => ({
 jest.mock("react-router-dom-v5-compat", () => ({
   ...jest.requireActual("react-router-dom-v5-compat"),
   useNavigate: () => mockUseNavigate,
+  useSearchParams: () => [mockSearchParams, mockSetSearchParams],
 }));
 
 jest.mock("../../../../Globais/UI", () => ({
@@ -53,9 +59,20 @@ jest.mock("../VincularDespesas", () => ({
 jest.mock("../InformarValores", () => ({
   InformarValores: ({ salvarRascunhoInformarValores }) => (
     <div data-testid="informar-valores">
-      <button onClick={() => salvarRascunhoInformarValores({ campo: "valor" })}>
+      <button onClick={() => salvarRascunhoInformarValores()}>
         Mock salvar rascunho em informar valores
       </button>
+    </div>
+  ),
+}));
+
+jest.mock("../ClassificarBem", () => ({
+  ClassificarBem: ({ salvarRascunhoClassificarBens, cadastrarBens }) => (
+    <div data-testid="informar-valores">
+      <button onClick={() => salvarRascunhoClassificarBens()}>
+        Mock salvar rascunho em classificar bem
+      </button>
+      <button onClick={() => cadastrarBens()}>Mock cadastrar bem</button>
     </div>
   ),
 }));
@@ -63,6 +80,8 @@ jest.mock("../InformarValores", () => ({
 jest.mock("../hooks/useGetBemProduzido");
 jest.mock("../hooks/usePostBemProduzido");
 jest.mock("../hooks/usePatchBemProduzido");
+jest.mock("../ClassificarBem/hooks/usePatchBemProduzidoItemsRascunho");
+jest.mock("../ClassificarBem/hooks/usePatchBemProduzidoItems");
 
 const rootReducer = combineReducers({});
 const mockStore = createStore(rootReducer);
@@ -95,6 +114,19 @@ describe("Componente FormularioBemProduzido", () => {
     usePatchBemProduzido.mockReturnValue({
       mutationPatch: { mutateAsync: mockMutatePatchAsync, isLoading: false },
     });
+    usePatchBemProduzidoItemsRascunho.mockReturnValue({
+      mutationPatch: {
+        mutateAsync: mockMutationPatchBemProduzidoItemsRascunhoAsync,
+        isLoading: false,
+      },
+    });
+    usePatchBemProduzidoItems.mockReturnValue({
+      mutationPatch: {
+        mutateAsync: mockMutationPatchBemProduzidoItemsAsync,
+        isLoading: false,
+      },
+    });
+
     useParams.mockReturnValue({ uuid: undefined });
   });
 
@@ -104,44 +136,7 @@ describe("Componente FormularioBemProduzido", () => {
     expect(screen.getByText("Informar valores")).toBeInTheDocument();
   });
 
-  // it("deve renderizar o componente Steps com o passo atual", () => {
-  //   renderComponent();
-
-  //   expect(screen.getByTestId("steps")).toHaveTextContent("Passo atual: 1");
-  // });
-
-  // it("deve renderizar o componente VincularDespesas quando o step é 1", () => {
-  //   renderComponent();
-
-  //   expect(screen.getByTestId("vincular-despesas")).toHaveTextContent("UUID:");
-  // });
-
-  it("deve definir step como 2 quando data tem despesas", () => {
-    useParams.mockReturnValue({ uuid: "uuid-bem-produzido-1234" });
-    useGetBemProduzido.mockReturnValue({ data: mockBemProduzido });
-
-    renderComponent();
-
-    expect(screen.getByTestId("steps")).toHaveTextContent("Passo atual: 2");
-  });
-
-  it("deve voltar para o passo 1 ao clicar em Selecionar despesas", async () => {
-    useParams.mockReturnValue({ uuid: "uuid-bem-produzido-1234" });
-    useGetBemProduzido.mockReturnValue({ data: mockBemProduzido });
-
-    renderComponent();
-
-    const buttonSelecionarDespesas = screen.getByRole("button", {
-      name: /Selecionar despesas/,
-    });
-    fireEvent.click(buttonSelecionarDespesas);
-
-    await waitFor(() => {
-      expect(screen.getByTestId("steps")).toHaveTextContent("Passo atual: 1");
-    });
-  });
-
-  it("deve chamar mutationPost e navegar para listagem ao clicar em Salvar rascunho", async () => {
+  it("deve chamar a função salvarRascunhoInformarValores e navegar para listagem ao clicar em Salvar rascunho", async () => {
     renderComponent();
 
     const buttonSalvarRascunho = screen.getByRole("button", {
@@ -157,7 +152,24 @@ describe("Componente FormularioBemProduzido", () => {
     });
   });
 
+  it("deve chamar a função salvarDespesas e navegar para edição ao clicar em Informar Valores", async () => {
+    renderComponent();
+
+    const buttonInformarValores = screen.getByRole("button", {
+      name: /Informar valores/,
+    });
+    fireEvent.click(buttonInformarValores);
+
+    await waitFor(() => {
+      expect(buttonInformarValores).toBeInTheDocument();
+      expect(mockMutatePostAsync).toHaveBeenCalled();
+      expect(mockUseNavigate).toHaveBeenCalled();
+    });
+  });
+
   it("deve chamar mutationPatch e navegar para listagem ao clicar em Salvar rascunho na etapa de informar valores", async () => {
+    mockSearchParams.set("step", "2");
+
     useParams.mockReturnValue({ uuid: "uuid-bem-produzido-1234" });
     useGetBemProduzido.mockReturnValue({ data: mockBemProduzido });
 
@@ -176,17 +188,68 @@ describe("Componente FormularioBemProduzido", () => {
     });
   });
 
-  it("deve chamar mutationPost e navegar para edição ao clicar em Informar Valores", async () => {
+  it("deve chamar a função informarValores ao clicar no botão 'Classificar o bem'", async () => {
+    useParams.mockReturnValue({ uuid: "uuid-bem-produzido-1234" });
+    mockSearchParams.set("step", "2");
+
+    useGetBemProduzido.mockReturnValue({ data: mockBemProduzido });
+
     renderComponent();
 
-    const buttonInformarValores = screen.getByRole("button", {
-      name: /Informar valores/,
+    const buttonClassificarBem = screen.getByRole("button", {
+      name: /Classificar o bem/,
     });
-    fireEvent.click(buttonInformarValores);
+    fireEvent.click(buttonClassificarBem);
 
     await waitFor(() => {
-      expect(mockMutatePostAsync).toHaveBeenCalled();
-      expect(mockUseNavigate).toHaveBeenCalled();
+      expect(mockMutatePatchAsync).toHaveBeenCalled();
+      expect(mockUseNavigate).toHaveBeenCalledWith(
+        "/edicao-bem-produzido/uuid-bem-produzido-1234/?step=3"
+      );
+    });
+  });
+
+  it("deve chamar a função salvarRascunhoClassificarBens ao clicar no botão 'Salvar rascunho' no passo 3", async () => {
+    useParams.mockReturnValue({ uuid: "uuid-bem-produzido-1234" });
+    mockSearchParams.set("step", "3");
+
+    useGetBemProduzido.mockReturnValue({ data: mockBemProduzido });
+
+    renderComponent();
+
+    const buttonSalvarRascunho = screen.getByRole("button", {
+      name: /Mock salvar rascunho em classificar bem/,
+    });
+    fireEvent.click(buttonSalvarRascunho);
+
+    await waitFor(() => {
+      expect(
+        mockMutationPatchBemProduzidoItemsRascunhoAsync
+      ).toHaveBeenCalled();
+      expect(mockUseNavigate).toHaveBeenCalledWith(
+        "/lista-situacao-patrimonial"
+      );
+    });
+  });
+
+  it("deve chamar a função cadastrarBens ao clicar no botão 'Cadastrar bem' no passo 3", async () => {
+    useParams.mockReturnValue({ uuid: "uuid-bem-produzido-1234" });
+    mockSearchParams.set("step", "3");
+
+    useGetBemProduzido.mockReturnValue({ data: mockBemProduzido });
+
+    renderComponent();
+
+    const buttonCadastrarBem = screen.getByRole("button", {
+      name: /Mock cadastrar bem/,
+    });
+    fireEvent.click(buttonCadastrarBem);
+
+    await waitFor(() => {
+      expect(mockMutationPatchBemProduzidoItemsAsync).toHaveBeenCalled();
+      expect(mockUseNavigate).toHaveBeenCalledWith(
+        "/lista-situacao-patrimonial"
+      );
     });
   });
 });
