0e1c780893bb20dcf91d939e744ac81fff50cb03
Matheus Diori <mdiori@hotmail.com>
2025-06-27 13:53:40 -0300

    feature(127458): Adiciona listagem situacao patrimonial

--- src/services/escolas/BensProduzidos.service.js
+++ src/services/escolas/BensProduzidos.service.js
@@ -1,5 +1,6 @@
 import api from "../api";
 import { ASSOCIACAO_UUID, TOKEN_ALIAS } from "../auth.service.js";
+import { addFiltersToQueryString } from "../../utils/Api.js";
 
 const authHeader = () => ({
   headers: {
@@ -46,4 +47,21 @@ export const patchCadastrarBem = async (bem_produzido_uuid, payload) => {
       authHeader()
     )
   ).data;
+};
+
+export const getBemProduzidosComAdquiridos = async (filters, page) => {
+  let queryString = `?associacao_uuid=${localStorage.getItem(
+    ASSOCIACAO_UUID
+  )}&page=${page}`;
+  queryString = addFiltersToQueryString(queryString, filters);
+  return (
+    await api.get(
+      `api/bens-produzidos-e-adquiridos/${queryString}`,
+      authHeader()
+    )
+  ).data;
+};
+
+export const getTodosPeriodosComPC = async (referencia = "") => {
+  return (await api.get(`/api/periodos/?referencia=${referencia}&somente_com_pcs_entregues=true&associacao_uuid=${localStorage.getItem(ASSOCIACAO_UUID)}`, authHeader())).data;
 };
