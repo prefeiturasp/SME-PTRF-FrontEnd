a08ea6c3583b189e8e28197e8475dbfb8719a4b4
Rayane Santos <rayanemsantos.contato@gmail.com>
2025-07-11 18:15:06 -0300

    feat(129068): Atualiza logos prefeitura de sp

---
+++ src/componentes/Globais/UI/LogoSP/index.js
@@ -0,0 +1,15 @@
+import logoSP from "../../../../assets/img/logos/logo_PrefSP_sem_fundo_horizontal.png";
+import logoSPHorizontalMonocromatica from "../../../../assets/img/logos/logo_PrefSP_sem_fundo_horizontal_branco_monocromatico.png";
+
+export const LogoSPHorizontalColorida = () => {
+  return <img src={logoSP} alt="logo prefeitura de são paulo" />;
+};
+
+export const LogoSPHorizontalMonocromatica = () => {
+  return (
+    <img
+      src={logoSPHorizontalMonocromatica}
+      alt="logo prefeitura de são paulo monocromática"
+    />
+  );
+};
