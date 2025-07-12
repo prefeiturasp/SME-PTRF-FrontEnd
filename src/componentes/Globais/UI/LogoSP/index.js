0706067a8ba8e2d2e56906f1488c3385a802bf31
Rayane Santos <rayanemsantos.contato@gmail.com>
2025-07-12 20:21:27 -0300

    feat(129068): Atualiza com imagens svg

--- src/componentes/Globais/UI/LogoSP/index.js
+++ src/componentes/Globais/UI/LogoSP/index.js
@@ -1,5 +1,5 @@
-import logoSP from "../../../../assets/img/logos/logo_PrefSP_sem_fundo_horizontal.png";
-import logoSPHorizontalMonocromatica from "../../../../assets/img/logos/logo_PrefSP_sem_fundo_horizontal_branco_monocromatico.png";
+import logoSP from "../../../../assets/img/logos/logo_PrefSP_sem_fundo_horizontal_colorida.svg";
+import logoSPHorizontalMonocromatica from "../../../../assets/img/logos/logo_PrefSP_sem_fundo_horizontal_branco_monocromatico.svg";
 
 export const LogoSPHorizontalColorida = () => {
   return <img src={logoSP} alt="logo prefeitura de são paulo" />;
