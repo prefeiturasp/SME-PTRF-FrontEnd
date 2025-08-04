2081e6cc47f4ff8b0b2d2da671b15301a87bc205
Rayane Santos <rayanemsantos.contato@gmail.com>
2025-08-04 12:21:19 -0300

    feat(131508): Implementa testes unitários em tabela de transações e topo com botões

--- src/utils/FormataData.js
+++ src/utils/FormataData.js
@@ -1,5 +1,7 @@
 import moment from "moment";
 
-export const formataData = (data, formato = 'DD/MM/YYYY') => {
-    return moment(data).format(formato);
+export const formataData = (data, formato = "DD/MM/YYYY") => {
+  return moment(data).format(formato);
 };
+
+export const formatDateOrDash = (date) => (date ? moment(date).format("DD/MM/YYYY") : "-");
