eb250f7e7eb08f7e5056bc41110cea0714732b15
vferreira90 <vef_90kl@hotmail.com>
2025-08-08 10:50:13 -0300

    hotfix(132102): acerto data do extrato retornando um dia menos

--- src/componentes/Globais/DatePickerField/index.js
+++ src/componentes/Globais/DatePickerField/index.js
@@ -10,11 +10,17 @@ registerLocale("pt", ptBR);
 
 
 export const DatePickerField = ({ dataQa="", name, id, value, className="form-control", onChange, onCalendarOpen, onCalendarClose, disabled, placeholderText, maxDate=null, wrapperClassName=null, minDate=null }) => {
+    const parseDate = (dateString) => {
+        if (!dateString) return null;
+        if (dateString instanceof Date) return dateString;
+        const date = moment(dateString, 'YYYY-MM-DD', true);
+        return date.isValid() ? date.toDate() : null;
+    };
 
     return (
         <DatePicker
             disabled={disabled}
-            selected={(value && new Date(moment(value))) || null}
+            selected={parseDate(value)}
             onChange={val => {
                 onChange(name, val);
             }}
