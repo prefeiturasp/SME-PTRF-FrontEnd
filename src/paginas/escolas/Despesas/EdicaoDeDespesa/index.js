9c39bcc3278863e520c7f67b2f0bf0242dc2b953
Matheus Diori <mdiori@hotmail.com>
2025-09-10 10:46:13 -0300

    feat(133158): Adiciona aba situação patrimonial dre

--- src/paginas/escolas/Despesas/EdicaoDeDespesa/index.js
+++ src/paginas/escolas/Despesas/EdicaoDeDespesa/index.js
@@ -42,6 +42,7 @@ export const EdicaoDeDespesa = ()=>{
     const parametroLocation = useLocation();
     const navigate = useNavigate();
     const veioDeSituacaoPatrimonial = parametroLocation.state?.origem === 'situacao_patrimonial';
+    const visao_selecionada = visoesService.getItemUsuarioLogado('visao_selecionada.nome')
 
     useEffect(() => {
         (async function setValoresIniciais() {
@@ -164,7 +165,7 @@ export const EdicaoDeDespesa = ()=>{
                     {veioDeSituacaoPatrimonial && (
                         <button
                             className="btn btn btn-outline-success mt-2 mr-2"
-                            onClick={() => navigate('/lista-situacao-patrimonial')}
+                            onClick={() => navigate(visao_selecionada === "UE" ? '/lista-situacao-patrimonial' : '/dre-detalhes-associacao')}
                         >
                             Voltar para Situação Patrimonial
                         </button>
