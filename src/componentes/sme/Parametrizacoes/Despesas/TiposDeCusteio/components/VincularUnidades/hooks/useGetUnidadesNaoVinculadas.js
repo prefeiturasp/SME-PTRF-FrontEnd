b8943c9eeaba5ef4014feeac89411bf89509b827
Tarcísio Thallys <tarcisio_thallys@hotmail.com>
2026-03-10 16:01:27 -0300

    feature(144076): tipos de despesas de custeio com vincular todas unidades de uma vez.

--- src/componentes/sme/Parametrizacoes/Despesas/TiposDeCusteio/components/VincularUnidades/hooks/useGetUnidadesNaoVinculadas.js
+++ src/componentes/sme/Parametrizacoes/Despesas/TiposDeCusteio/components/VincularUnidades/hooks/useGetUnidadesNaoVinculadas.js
@@ -1,10 +1,23 @@
 import { getUnidadesNaoVinculadasTipoCusteio } from "../../../../../../../../services/sme/Parametrizacoes.service";
 import { useQuery } from "@tanstack/react-query";
 
-export const useGetUnidadesNaoVinculadas = (uuid, page, nome_ou_codigo = "", dre = "") => {
+export const useGetUnidadesNaoVinculadas = (
+  uuid,
+  page,
+  nome_ou_codigo = "",
+  dre = "",
+  tipo_unidade = "",
+) => {
   const { status, isFetching, isError, data, error, refetch } = useQuery({
     queryKey: ["unidades-nao-vinculadas-tipo-custeio", uuid],
-    queryFn: () => getUnidadesNaoVinculadasTipoCusteio(uuid, nome_ou_codigo, dre, page),
+    queryFn: () =>
+      getUnidadesNaoVinculadasTipoCusteio(
+        uuid,
+        nome_ou_codigo,
+        dre,
+        tipo_unidade,
+        page,
+      ),
     keepPreviousData: false,
     staleTime: 5 * 60 * 1000, // 5 minutos antes de ser considerado "stale"
     cacheTime: 10 * 60 * 1000, // 10 minutos antes de ser removido do cache
