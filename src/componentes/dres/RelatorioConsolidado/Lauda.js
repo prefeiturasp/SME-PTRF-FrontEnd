d50347611463b8c954ac7c0fd9f29f91d0fd8ced
Matheus Diori <mdiori@hotmail.com>
2026-05-15 09:52:52 -0300

    feat(147067): Adiciona lauda PDF

--- src/componentes/dres/RelatorioConsolidado/Lauda.js
+++ src/componentes/dres/RelatorioConsolidado/Lauda.js
@@ -19,19 +19,12 @@ const Lauda = ({consolidadoDre}) => {
 
     const downloadLauda = async (lauda) => {
         let nome_dre = "";
-        let tipo_conta = "";
-        let filename = "";
 
         if (consolidadoDre && consolidadoDre.dre_nome) {
             nome_dre = formataNomeDRE(consolidadoDre.dre_nome).toLowerCase();
         }
 
-        if (lauda && lauda.tipo_conta) {
-            tipo_conta = lauda.tipo_conta.toLowerCase();
-            filename = `Lauda_${nome_dre}_${tipo_conta}.docx.txt`;
-        } else {
-            filename = `Lauda_${nome_dre}.docx.txt`
-        }
+        const filename = `Lauda_${nome_dre}.pdf`;
 
         await getDownloadLauda(lauda.uuid, filename);
     };
@@ -57,7 +50,7 @@ const Lauda = ({consolidadoDre}) => {
                                         <div className="col-12 col-md-8">
                                             <div className='mt-2 mb-3'>
                                                 <p className='fonte-14 mb-1'>
-                                                    <strong>Lauda {lauda && lauda.tipo_conta ? "- Conta " + lauda.tipo_conta : ""}</strong>
+                                                    <strong>Lauda</strong>
                                                 </p>
                                                 <p className={`fonte-12 mb-0`}>
                                                     <span>{lauda.mensagem_sem_movimentacao}</span>
@@ -70,7 +63,7 @@ const Lauda = ({consolidadoDre}) => {
                                     <div className="col-12 col-md-8">
                                         <div className='mt-2 mb-3'>
                                             <p className='fonte-14 mb-1'>
-                                                <strong>Lauda {lauda && lauda.tipo_conta ? "- Conta " + lauda.tipo_conta : ""}</strong>
+                                                <strong>Lauda</strong>
                                             </p>
                                             <p className={`fonte-12 mb-0 ${retornaClasseMensagem(lauda.status)}`}>
                                                 <span>{lauda.status_geracao_arquivo}</span>
