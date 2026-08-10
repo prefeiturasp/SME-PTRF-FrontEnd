07b512b2fb240d53209b1b8ee69019f320f338f0
GabrielPSantana <gabrieldevti@gmail.com>
2026-08-10 09:33:35 -0300

    feature(152458): Melhorias na visualização da Ata do PAA e ajustes em funcionalidades relacionadas

--- src/componentes/escolas/GeracaoDaAta/VisualizacaoDaAta/VisualizacaoAtaPaa/AtividadesEstatutarias/index.jsx
+++ src/componentes/escolas/GeracaoDaAta/VisualizacaoDaAta/VisualizacaoAtaPaa/AtividadesEstatutarias/index.jsx
@@ -13,74 +13,49 @@ export const AtividadesEstatutarias = memo(({
     const atividadesComAlteracao = useMemo(() => {
         return atividades ? atividades.filter(act => act.alteracao) : [];
     }, [atividades]);
-    
+
     const naoEstaCarregando = !isLoadingAtividades && !isLoading;
 
-    return (  
-        naoEstaCarregando && atividades && atividades.length > 0 && !paaRetificacao ? (
-         <div className="col-12 mt-4">
-           <h4 className="mb-3" style={{ fontWeight: "bold", color: "#42474A" }}>
-             <span className="mr-3">Atividades Estatutárias </span>{paaRetificacao && !isLoading && <TagRetificacao />}
-           </h4>
-           <table className="table table-bordered" style={{ width: "100%" }}>
-             <thead style={{ backgroundColor: "#dadada" }}>
-               <tr>
-                 <th style={{ width: "20%" }}>Tipo de Atividades</th>
-                 <th style={{ width: "15%" }}>Data</th>
-                 <th style={{ width: "45%" }}>Atividades Estatutárias Previstas</th>
-                 <th style={{ width: "20%" }}>Mês/Ano</th>
-               </tr>
-             </thead>
-              <tbody>
-                {atividades.map((atividade, index) => {
-                  const mesAnoBase =
-                    atividade.isGlobal && !atividade.vinculoUuid
-                      ? atividade.mesLabel || "-"
-                      : formatarMesAno(atividade.data);
-                  return (
-                    <tr key={atividade.uuid || index}>
-                      <td>{atividade.tipoAtividade || "-"}</td>
-                      <td>{formatarData(atividade.data)}</td>
-                      <td>{atividade.descricao || "-"}</td>
-                      <td>{mesAnoBase || "-"}</td>
+    const deveRenderizarPadrao = !paaRetificacao && naoEstaCarregando && atividades && atividades.length > 0;
+    const deveRenderizarComAlteracao = paaRetificacao && naoEstaCarregando && atividadesComAlteracao.length > 0;
+
+    if (!deveRenderizarPadrao && !deveRenderizarComAlteracao) {
+        return null;
+    }
+
+    return (
+        <div className="col-12 mt-4">
+            <h4 className="mb-3" style={{ fontWeight: "bold", color: "#42474A" }}>
+                <span className="mr-3">Atividades Estatutárias </span>{paaRetificacao && !isLoading && <TagRetificacao />}
+            </h4>
+
+            <table className="table table-bordered" style={{ width: "100%" }}>
+                <thead style={{ backgroundColor: "#dadada" }}>
+                    <tr>
+                        <th scope="col" style={{ width: "20%" }}>Tipo de Atividades</th>
+                        <th scope="col" style={{ width: "15%" }}>Data</th>
+                        <th scope="col" style={{ width: "45%" }}>Atividades Estatutárias Previstas</th>
+                        <th scope="col" style={{ width: "20%" }}>Mês/Ano</th>
                     </tr>
-                  );
-                })}
-              </tbody>
-           </table>
-         </div>
-         ) : naoEstaCarregando && atividadesComAlteracao.length > 0 ? (
-         <div className="col-12 mt-4">
-           <h4 className="mb-3" style={{ fontWeight: "bold", color: "#42474A" }}>
-             <span className="mr-3">Atividades Estatutárias </span>{paaRetificacao && !isLoading && <TagRetificacao />}
-           </h4>
-           <table className="table table-bordered" style={{ width: "100%" }}>
-             <thead style={{ backgroundColor: "#dadada" }}>
-               <tr>
-                 <th style={{ width: "20%" }}>Tipo de Atividades</th>
-                 <th style={{ width: "15%" }}>Data</th>
-                 <th style={{ width: "45%" }}>Atividades Estatutárias Previstas</th>
-                 <th style={{ width: "20%" }}>Mês/Ano</th>
-               </tr>
-             </thead>
-              <tbody>
-                {atividadesComAlteracao.map((atividade, index) => {
-                  const mesAnoBase =
-                    atividade.isGlobal && !atividade.vinculoUuid
-                      ? atividade.mesLabel || "-"
-                      : formatarMesAno(atividade.data);
-                  return (
-                    <tr key={atividade.uuid || index}>
-                      <td>{atividade.tipoAtividade || "-"}</td>
-                      <td>{formatarData(atividade.data)}</td>
-                      <td>{atividade.descricao || "-"}</td>
-                      <td>{mesAnoBase || "-"}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-           </table>
-         </div>
-       ) : null
-    )
+                </thead>
+
+                <tbody>
+                    {atividades.map((atividade, index) => {
+                        const mesAnoBase =
+                        atividade.isGlobal && !atividade.vinculoUuid
+                            ? atividade.mesLabel || "-"
+                            : formatarMesAno(atividade.data);
+                        return (
+                            <tr key={atividade.uuid || index}>
+                                <td>{atividade.tipoAtividade || "-"}</td>
+                                <td>{formatarData(atividade.data)}</td>
+                                <td>{atividade.descricao || "-"}</td>
+                                <td>{mesAnoBase || "-"}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
 });
