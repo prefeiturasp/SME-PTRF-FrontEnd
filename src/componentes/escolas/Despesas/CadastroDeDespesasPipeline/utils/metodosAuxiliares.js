2d7839741dfce69bcd7600bbd1741a0f1ab8a23f
GabrielPSantana <gabrieldevti@gmail.com>
2026-09-16 16:42:30 -0300

    fix(155761): implementa limpeza automática dos campos no formulário de cadatro de despesas

--- src/componentes/escolas/Despesas/CadastroDeDespesasPipeline/utils/metodosAuxiliares.js
+++ src/componentes/escolas/Despesas/CadastroDeDespesasPipeline/utils/metodosAuxiliares.js
@@ -194,6 +194,15 @@ const limpaTipoDespesaCusteio = (setFieldValue, index) => {
     setFieldValue(`rateios[${index}].especificacao_material_servico`, "")
 }
 
+const limpaCamposExclusivosAplicacaoRecurso = (setFieldValue, index, aplicacaoRecurso) => {
+    if (aplicacaoRecurso !== 'CAPITAL'){
+        setFieldValue(`rateios[${index}].quantidade_itens_capital`, "")
+        setFieldValue(`rateios[${index}].valor_item_capital`, "")
+        setFieldValue(`rateios[${index}].nao_exibir_em_rel_bens`, false)
+        setFieldValue(`rateios[${index}].numero_processo_incorporacao_capital`, "")
+    }
+}
+
 const setaValoresCusteioCapital = (mais_de_um_tipo_de_despesa = null, values, setFieldValue) =>{
     if (mais_de_um_tipo_de_despesa && mais_de_um_tipo_de_despesa === 'nao'){
         setFieldValue('rateios[0].valor_rateio', calculaValorRecursoAcoes(values));
@@ -476,5 +485,6 @@ export const metodosAuxiliares = {
     bloqueiaCamposDespesaImposto,
     mostraBotaoDeletar,
     documentoTransacaoObrigatorio,
-    limpaTipoDespesaCusteio
+    limpaTipoDespesaCusteio,
+    limpaCamposExclusivosAplicacaoRecurso
 };
